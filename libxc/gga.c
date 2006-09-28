2605e2c17ca47777058f03e441711e5f13090a5b
Miguel Marques <marques@tddft.org>
2006-09-28 11:06:54 +0000

    *) Added Becke 88 to libxc. However, this functional is still not called from octopus. That will come later.
    *) The GGAs are now also tested. The exchange PBE and B88 pass all test 
    without problems. The correlation part of the PBE does not, however. The 
    differences are small (5th digit or so), so you may relax ;) In any 
    case, I believe that the problem is, in fact, the "reference" 
    implementation that has an inconsistency in the constants it uses...

--- libxc/gga.c
+++ libxc/gga.c
@@ -9,7 +9,8 @@ void gga_init(gga_type *p, int functional, int nspin)
   /* sanity check */
   assert(functional == XC_GGA_X_PBE    ||
 	 functional == XC_GGA_C_PBE    ||
-	 functional == XC_GGA_XC_LB);
+	 functional == XC_GGA_XC_LB    ||
+	 functional == XC_GGA_X_B88);
   
   assert(nspin==XC_UNPOLARIZED || nspin==XC_POLARIZED);
   p->nspin = nspin;
@@ -28,6 +29,9 @@ void gga_init(gga_type *p, int functional, int nspin)
     gga_lb_init(p, nspin, 0, 0.0);
     break;
 
+  case(XC_GGA_X_B88):
+    gga_x_b88_init(p);
+    break;
   }
 }
 
@@ -42,15 +46,26 @@ void gga_end(gga_type *p)
   case(XC_GGA_XC_LB) :
     gga_lb_end(p);
     break;
+  case(XC_GGA_X_B88) :
+    gga_x_b88_end(p);
+    break;
   }
 }
 
+/* Some useful formulas:
 
-void gga(gga_type *p, double *rho, double *grho,
-	 double *e, double *dedd, double *dedgd)
+   sigma_st  = grad rho_s . grad rho_t
+   vrho_s    = d e / d rho_s
+   vsigma_st = d n*e / d sigma_st
+
+if nspin == 2
+   rho   = (rho_u, rho_d)
+   sigma = (sigma_uu, sigma_du, sigma_dd)
+*/
+void gga(gga_type *p, double *rho, double *sigma,
+	 double *e, double *vrho, double *vsigma)
 {
   double dens;
-  int i;
 
   assert(p!=NULL);
   
@@ -59,26 +74,35 @@ void gga(gga_type *p, double *rho, double *grho,
   
   if(dens <= MIN_DENS){
     int i;
+
     *e = 0.0;
-    for(i=0; i<  p->nspin; i++) dedd [i] = 0.0;
-    for(i=0; i<3*p->nspin; i++) dedgd[i] = 0.0;
+    for(i=0; i<p->nspin; i++) vrho [i] = 0.0;
+
+    vsigma[0] = 0.0;
+    if(p->nspin == XC_POLARIZED){
+      vsigma[1] = 0.0; vsigma[2] = 0.0;
+    }
     return;
   }
   
   switch(p->func->number){
   case(XC_GGA_X_PBE):
-    gga_x_pbe(p, rho, grho, e, dedd, dedgd);
+    gga_x_pbe(p, rho, sigma, e, vrho, vsigma);
     break;
-
+    
   case(XC_GGA_C_PBE):
-    gga_c_pbe(p, rho, grho, e, dedd, dedgd);
+    gga_c_pbe(p, rho, sigma, e, vrho, vsigma);
     break;
 
-  case(XC_GGA_XC_LB):
+  case(XC_GGA_XC_LB): {
+    int i;
     *e = 0.0;
-    for(i=0; i<3*p->nspin; i++) dedgd[i] = 0.0;
-    gga_lb(p, rho, grho, 0.0, 0.0, 0.0, dedd);
+    for(i=0; i<3*p->nspin; i++) vsigma[i] = 0.0;
+    gga_lb(p, rho, sigma, 0.0, 0.0, 0.0, vrho);
     break;
+  }
+  case(XC_GGA_X_B88):
+    gga_x_b88(p, rho, sigma, e, vrho, vsigma);
   }
 
 }

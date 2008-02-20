aacb4bf8cf3e6181ab96336ae137d3ced9b3ba70
Miguel Marques <marques@tddft.org>
2008-02-20 09:31:06 +0000

    Added the mPW functional. I still have some doubts concerning the constants in the functional (they were taken from http://scsg20.unige.ch/~dulak/files/functionals.pdf), so they will eventually have to be checked.

--- libxc/src/gga_x_pw91.c
+++ libxc/src/gga_x_pw91.c
@@ -21,29 +21,52 @@
 #include "util.h"
 
 #define XC_GGA_X_PW91         109 /* Perdew & Wang 91 */
+#define XC_GGA_X_mPW91        119 /* Modified form of PW91 by Adamo & Barone */
 
 static inline void 
 func(xc_gga_type *p, double x, double *f, double *dfdx, double *ldfdx)
 {
-  const double x2s = 0.12827824385304220645; /* 1/(2*(6*pi^2)^(1/3)) */
-  const double aa = 0.19645, bb = 7.7956, cc = 0.2743, dd=-0.1508, ff=0.004, alpha=100.0;
+  const double x2s     = 0.12827824385304220645; /* 1/(2*(6*pi^2)^(1/3)) */
+  const double aa[2]   = {0.19645, 0.21516};
+  const double bb      = 7.7956;
+  const double cc[2]   = {0.2743, 0.30042};
+  const double dd[2]   = {-0.1508, -0.17696};
+  const double ff[2]   = {0.004, 0.002285};
+  const double alpha   = 100.0;
+  const double expo[2] = {4.0, 3.75};
+
   double ss, ss2, ss4;
   double f1, f2, f3, f4;
 
+  int func;
+
+  switch(p->info->number){
+  case XC_GGA_X_mPW91:   func = 1; break;
+  default:               func = 0; /* original PW91 */
+  }
+
   ss  = x2s*x;
   ss2 = ss*ss;
-  ss4 = ss2*ss2;
+  ss4 = pow(ss, expo[func]);
 
-  f1 = dd*exp(-alpha*ss2);
-  f2 = aa*asinh(bb*ss);
-  f3 = (cc + f1)*ss2 - ff*ss4;
-  f4 = 1.0 + ss*f2 + ff*ss4;
+  f1 = dd[func]*exp(-alpha*ss2);
+  f2 = aa[func]*asinh(bb*ss);
+  f3 = (cc[func] + f1)*ss2 - ff[func]*ss4;
+  f4 = 1.0 + ss*f2 + ff[func]*ss4;
 
   *f     = 1.0 + f3/f4;
-  *dfdx  = (2.0*ss*(cc + f1*(1.0 - alpha*ss2) - 2.0*ff*ss2)*f4 - 
-	    f3*(f2 + ss*(aa*bb/sqrt(1.0 + bb*bb*ss2) + 4.0*ff*ss2)))/(f4*f4);
-  *dfdx *= x2s;
-  *ldfdx = x2s*x2s*(cc + dd);
+  {
+    double df3, df4;
+
+    df3 = 2.0*ss*(cc[func] + f1*(1.0 - alpha*ss2) - expo[func]/2.0*ff[func]*pow(ss, expo[func]-2.0));
+    df4 = f2 + ss*(aa[func]*bb/sqrt(1.0 + bb*bb*ss2) + expo[func]*ff[func]*pow(ss, expo[func]-2.0));
+    *dfdx  = (df3*f4 - f3*df4)/(f4*f4);
+    *ldfdx = cc[func] + dd[func];
+  }
+
+
+  *dfdx  *= x2s;
+  *ldfdx *= x2s*x2s;
 }
 
 #include "work_gga_x.c"
@@ -53,7 +76,21 @@ const xc_func_info_type func_info_gga_x_pw91 = {
   XC_EXCHANGE,
   "Perdew & Wang 91",
   XC_FAMILY_GGA,
-  "JP Perdew, JA Chevary, SH Vosko, KA Jackson, MR Pederson, and C Fiolhais, Phys. Rev. B 46, 6671 (1992)",
+  "JP Perdew, in Proceedings of the 21st Annual International Symposium on the Electronic Structure of Solids, ed. by P Ziesche and H Eschrig (Akademie Verlag, Berlin, 1991), p. 11.\n"
+  "JP Perdew, JA Chevary, SH Vosko, KA Jackson, MR Pederson, DJ Singh, and C Fiolhais, Phys. Rev. B 46, 6671 (1992)\n"
+  "JP Perdew, JA Chevary, SH Vosko, KA Jackson, MR Pederson, DJ Singh, and C Fiolhais, Phys. Rev. B 48, 4978(E) (1993)",
+  XC_PROVIDES_EXC | XC_PROVIDES_VXC,
+  NULL, NULL, NULL,
+  work_gga_x
+};
+
+
+const xc_func_info_type func_info_gga_x_mpw91 = {
+  XC_GGA_X_mPW91,
+  XC_EXCHANGE,
+  "mPW91 of Adamo & Barone",
+  XC_FAMILY_GGA,
+  "C Adamo and V Barone, J. Chem. Phys. 108, 664 (1998)",
   XC_PROVIDES_EXC | XC_PROVIDES_VXC,
   NULL, NULL, NULL,
   work_gga_x

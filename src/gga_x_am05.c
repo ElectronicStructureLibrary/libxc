3446d34ab9eaa34cf1b22bb6d9a1835abb8a5318
Miguel Marques <marques@tddft.org>
2008-03-31 20:19:39 +0000

    Added a first implementation of AM05 correlation. This is still not working. I again have some doubts concerning the spin-polarized implementation, and I am waiting for the authors to answer mye mail before finishing my implementation.

--- src/gga_x_am05.c
+++ src/gga_x_am05.c
@@ -25,124 +25,17 @@
 
 #define XC_GGA_X_AM05         120 /* Armiento & Mattsson 05 exchange                */
 
-static void
-lambert(FLOAT x, FLOAT *w, FLOAT *dw)
-{
-  *w  = (FLOAT)lambert_w((double)x);
-  if(x != 0.0)
-    *dw = (*w)/(x*(1+(*w)));
-  else
-    *dw = 1.0;
-}
-
-static void /* Eq. (7) */
-zeta_t(FLOAT s, FLOAT *f, FLOAT *df)
-{
-  FLOAT x, w, dw;
-
-  x = POW(s, 1.5)/(2.0*sqrt(6.0));
-  lambert(x, &w, &dw);
-
-  *f  = POW(1.5*w, 2.0/3.0);
-  if(s!=0)
-    *df = POW(1.5, 1.0/6.0)*sqrt(s)*dw/(4.0*POW(w, 1.0/3.0));
-  else
-    *df = POW(6.0, 1.0/3.0)/4.0;
-}
-
-static void /* Eq. (3) */
-zeta_tt(FLOAT s, FLOAT *f, FLOAT *df)
-{
-  FLOAT c;
-  FLOAT z_t, z_t2, dz_t;
-  zeta_t(s, &z_t, &dz_t);
-  z_t2 = z_t*z_t;
-
-  c   = POW(POW(4.0/3.0, 1.0/3.0) * 2.0*M_PI/3.0, 4);
-
-  *f  = POW(c*z_t2 + z_t2*z_t2, 1.0/4.0);
-  *df = POW(c*z_t2 + z_t2*z_t2, -3.0/4.0)*(2.0*c*z_t + 4.0*z_t2*z_t)*dz_t/4.0;
-}
-
-static void /* Eq. (10) */
-n0_t(FLOAT s, FLOAT *f, FLOAT *df)
-{
-  FLOAT z_t, dz_t;
-
-  zeta_t(s, &z_t, &dz_t);
-
-  *f  = POW(z_t, 3.0/2.0)/(3.0*M_PI*M_PI*s*s*s);
-  *df = (*f) * (3.0/2.0*dz_t/z_t - 3.0/s);
-}
-
-static void /* Eq. (3) */
-ex_LDA(FLOAT n, FLOAT *f, FLOAT *df)
-{
-  FLOAT c;
-
-  c = -3.0/(4.0*M_PI)*POW(3.0*M_PI*M_PI, 1.0/3.0);
-
-  *f  = c*POW(n, 1.0/3.0);
-  *df = c*POW(n, -2.0/3.0)/3.0;
-}
-
-static void /* Eq. (3) */
-Fx_b(FLOAT s, FLOAT *f, FLOAT *df)
-{
-  FLOAT n0, dn0, z_t, dz_t, z_tt, dz_tt, ex, dex;
-
-  n0_t(s, &n0, &dn0);
-  zeta_t(s, &z_t, &dz_t);
-  zeta_tt(s, &z_tt, &dz_tt);
-  ex_LDA(n0, &ex, &dex);
-
-  *f  = -1.0/(ex*4.0*z_tt);
-  *df = 4.0*(*f)*(*f) * (dex*dn0*z_tt + ex*dz_tt);
-}
-
-static void /* Eq. (12) */
-XX(FLOAT s, FLOAT *f, FLOAT *df)
-{
-  const FLOAT alpha = 2.804;
-  FLOAT s2, f1;
-
-  s2 = s*s;
-  f1 = 1.0 + alpha*s2;
-
-  *f  = 1.0 - alpha*s2/f1;
-  *df = -2.0*alpha*s/(f1*f1);
-}
-
-static void /* Eq. (6) */
-F_LAA(FLOAT s, FLOAT *f, FLOAT *df)
-{
-  const FLOAT c=0.7168;
-  FLOAT s2, fxb, dfxb, f1, f2, df1, df2;
-
-  s2 = s*s;
-  Fx_b(s, &fxb, &dfxb);
-
-  f1  = c*s2 + 1.0;
-  df1 = 2.0*c*s;
-
-  f2  = c*s2/fxb + 1.0;
-  df2 = c*(2.0*s*fxb - dfxb*s2)/(fxb*fxb);
-
-  *f  = f1/f2;
-  *df = (df1*f2 - f1*df2)/(f2*f2);
-}
-
 static inline void 
 func(const XC(gga_type) *p, FLOAT x, FLOAT *f, FLOAT *dfdx, FLOAT *ldfdx, FLOAT *d2fdx2)
 {
-  const FLOAT am05_c     = 0.7168;
-  const FLOAT am05_alpha = 2.804;
+  const FLOAT am05_c      = 0.7168;
+  const FLOAT am05_alpha  = 2.804;
 
-  const FLOAT x2s        = 0.12827824385304220645; /* 1/(2*(6*pi^2)^(1/3)) */
-  const double z_tt_factor = POW(POW(4.0/3.0, 1.0/3.0) * 2.0*M_PI/3.0, 4);
+  const FLOAT x2s         = 0.12827824385304220645; /* 1/(2*(6*pi^2)^(1/3)) */
+  const FLOAT z_tt_factor = POW(POW(4.0/3.0, 1.0/3.0) * 2.0*M_PI/3.0, 4);
 
-  double ss, ss2, lam_x, ww;
-  double z_t, z_t2, z_tt, fx_b, xx, flaa_1, flaa_2, flaa;
+  FLOAT ss, ss2, lam_x, ww;
+  FLOAT z_t, z_t2, z_tt, fx_b, xx, flaa_1, flaa_2, flaa;
 
   if(x < MIN_GRAD){
     *f    = 1.0;
@@ -174,7 +67,7 @@ func(const XC(gga_type) *p, FLOAT x, FLOAT *f, FLOAT *dfdx, FLOAT *ldfdx, FLOAT 
   *f     = xx + (1.0 - xx)*flaa;
 
   if(dfdx != NULL){
-    double dww, dz_t, dz_tt, dfx_b, dxx, dflaa_1, dflaa_2, dflaa;
+    FLOAT dww, dz_t, dz_tt, dfx_b, dxx, dflaa_1, dflaa_2, dflaa;
 
     dww     = ww/(lam_x*(1.0 + ww));
     dz_t    = POW(1.5, 1.0/6.0)*sqrt(ss)*dww/(4.0*POW(ww, 1.0/3.0));

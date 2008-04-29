d4406dfc6472ed496c3e5a3826c63be8fc644d4a
Miguel Marques <marques@tddft.org>
2008-04-29 11:41:15 +0000

    Secon derivatives of the WC functional

--- src/gga_x_wc.c
+++ src/gga_x_wc.c
@@ -22,25 +22,12 @@
 
 #define XC_GGA_X_WC         118 /* Wu & Cohen */
 
-/* 
-     the variable used in the original PBE paper is s = |grad n|/(2 k_f n)
-     while here we use x = |grad n_s|/n_s^(4/3). Therefore, the value of s
-     we use here is the original x multiplied by 
-
-       1/2^(1/3) * 1/(2*(3 pi^2)^(1/3))
-
-     where the first term comes from using the spin densities, and the second
-     from the constants of k_f = (3 pi^2 n)^(1/3)
-*/
-
-static FLOAT wc_mu;
-static FLOAT wc_c, wc_fac;
+static FLOAT wc_mu, wc_c;
 
 static void
 gga_x_wc_init(void *p_)
 {
   wc_mu  = 0.2195149727645171;
-  wc_fac = 1.0/(2.0*POW(6.0*M_PI*M_PI, 1.0/3.0));
   wc_c   = (146.0/2025.0)*(4.0/9.0) - (73.0/405.0)*(2.0/3.0) + (wc_mu - 10.0/81.0);
 }
 
@@ -49,25 +36,34 @@ func(const XC(gga_type) *p, FLOAT x, FLOAT *f, FLOAT *dfdx, FLOAT *ldfdx, FLOAT 
 {
   const FLOAT kappa = 0.8040;
 
-  FLOAT s, s2, xx, dxx;
-  FLOAT aux1, aux2, dd;
+  FLOAT s, s2;
+  FLOAT aux1, aux2, f0, df0, d2f0, dd;
 
-  s  = wc_fac*x;
+  s  = X2S*x;
   s2 = s*s;
   
   aux1 = wc_mu - 10.0/81.0;
   aux2 = exp(-s2);
 
-  xx = 10.0/81.0*s2 + s2*aux1*aux2 + log(1.0 + wc_c*s2*s2);
-  dd = 1.0/(kappa + xx);
+  f0 = kappa + 10.0/81.0*s2 + s2*aux1*aux2 + log(1.0 + wc_c*s2*s2);
+  *f = 1.0 + kappa*(1.0 - kappa/f0);
 
-  *f = 1.0 + kappa*(1.0 - kappa*dd);
+  if(dfdx==NULL && d2fdx2==NULL) return; /* nothing else to do */
 
-  dxx  = 20.0/81.0*s + 2.0*s*aux1*aux2*(1.0 - s2) + 4.0*wc_c*s*s2/(1.0 + wc_c*s2*s2);
-  dxx *= wc_fac; /* convert d/ds in d/dx */
+  df0 = 20.0/81.0*s + 2.0*s*aux1*aux2*(1.0 - s2) + 4.0*wc_c*s*s2/(1.0 + wc_c*s2*s2);
 
-  *dfdx  = dxx * kappa*kappa * dd*dd;
-  *ldfdx = 10.0/81.0;
+  if(dfdx!=NULL){
+    *dfdx  = X2S*kappa*kappa*df0/(f0*f0);
+    *ldfdx = X2S*X2S*wc_mu;
+  }
+
+  if(d2fdx2==NULL) return; /* nothing else to do */
+
+  dd   = 1.0 + wc_c*s2*s2;
+  d2f0 = 20.0/81.0 + 2.0*aux1*aux2*(1.0 - 5.0*s2 + 2.0*s2*s2)
+    - 4.0*wc_c*s2*(dd - 4.0)/(dd*dd);
+
+  *d2fdx2 = X2S*X2S*kappa*kappa/(f0*f0)*(d2f0 - 2.0*df0*df0/f0);
 }
 
 
@@ -80,7 +76,7 @@ const XC(func_info_type) XC(func_info_gga_x_wc) = {
   "Wu & Cohen",
   XC_FAMILY_GGA,
   "Z Wu and RE Cohen, Phys. Rev. B 73, 235116 (2006)",
-  XC_PROVIDES_EXC | XC_PROVIDES_VXC,
+  XC_PROVIDES_EXC | XC_PROVIDES_VXC | XC_PROVIDES_FXC,
   gga_x_wc_init, 
   NULL, NULL,
   work_gga_x

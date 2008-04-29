c61bb2c75aef9e579eb19ef749022e2dce851926
Miguel Marques <marques@tddft.org>
2008-04-29 13:41:10 +0000

    AM05 (exchange part) gets second derivatives also

--- src/gga_x_am05.c
+++ src/gga_x_am05.c
@@ -33,12 +33,15 @@ func(const XC(gga_type) *p, FLOAT x, FLOAT *f, FLOAT *dfdx, FLOAT *ldfdx, FLOAT 
 
   const FLOAT z_tt_factor = POW(POW(4.0/3.0, 1.0/3.0) * 2.0*M_PI/3.0, 4);
 
-  FLOAT ss, ss2, lam_x, ww;
-  FLOAT z_t, z_t2, z_tt, fx_b, xx, flaa_1, flaa_2, flaa;
+  FLOAT ss, ss2, lam_x, dlam_x, d2lam_x;
+  FLOAT ww, z_t, z_t2, z_tt, fx_b, xx, flaa_1, flaa_2, flaa;
+  FLOAT dww, dz_t, dz_tt, dfx_b, dxx, dflaa_1, dflaa_2, dflaa;
+  FLOAT d2ww, d2z_t, d2z_tt, d2fx_b, d2xx, d2flaa_1, d2flaa_2, d2flaa;;
 
   if(x < MIN_GRAD){
     *f    = 1.0;
-    if(dfdx != NULL) *ldfdx = -am05_alpha*X2S*X2S;
+    if(dfdx   != NULL)  *ldfdx = -am05_alpha*X2S*X2S;
+    if(d2fdx2 != NULL) *d2fdx2 = 0.0;
     return;
   }
 
@@ -51,7 +54,7 @@ func(const XC(gga_type) *p, FLOAT x, FLOAT *f, FLOAT *dfdx, FLOAT *ldfdx, FLOAT 
   z_t    = POW(1.5*ww, 2.0/3.0);
   z_t2   = z_t*z_t;
 
-  /* This is equal to sqrt(t_zeta) * tt_zeta} of the JCP*/
+  /* This is equal to sqrt(t_zeta) * tt_zeta of the JCP*/
   z_tt   = z_t * POW(z_tt_factor + z_t2, 1.0/4.0);
 
   /* note that there is a factor of 2 missing in the JCP */
@@ -65,19 +68,20 @@ func(const XC(gga_type) *p, FLOAT x, FLOAT *f, FLOAT *dfdx, FLOAT *ldfdx, FLOAT 
 
   *f     = xx + (1.0 - xx)*flaa;
 
-  if(dfdx != NULL){
-    FLOAT dww, dz_t, dz_tt, dfx_b, dxx, dflaa_1, dflaa_2, dflaa;
+  if(dfdx==NULL && d2fdx2==NULL) return; /* nothing else to do */
 
-    dww     = ww/(lam_x*(1.0 + ww));
-    dz_t    = POW(1.5, 1.0/6.0)*sqrt(ss)*dww/(4.0*POW(ww, 1.0/3.0));
-    dz_tt   = POW(z_tt_factor + z_t2, -3.0/4.0)*(z_tt_factor + 3.0*z_t2/2.0)*dz_t;
-    dfx_b   = M_PI/3.0*(z_tt - ss*dz_tt)/(z_tt*z_tt);
+  dlam_x  = 1.5*lam_x/ss;
+  dww     = ww/(lam_x*(1.0 + ww))*dlam_x;
+  dz_t    = POW(1.5, 2.0/3.0)*2.0/3.0*POW(ww, -1.0/3.0)*dww;
+  dz_tt   = POW(z_tt_factor + z_t2, -3.0/4.0)*(z_tt_factor + 3.0*z_t2/2.0)*dz_t;
+  dfx_b   = M_PI/3.0*(z_tt - ss*dz_tt)/(z_tt*z_tt);
 
-    dxx     = -2.0*am05_alpha*ss * xx*xx;
-    dflaa_1 = 2.0*am05_c*ss;
-    dflaa_2 = am05_c*(2.0*ss*fx_b - dfx_b*ss2)/(fx_b*fx_b);
-    dflaa   = (dflaa_1*flaa_2 - flaa_1*dflaa_2)/(flaa_2*flaa_2);
+  dxx     = -2.0*am05_alpha*ss * xx*xx;
+  dflaa_1 = 2.0*am05_c*ss;
+  dflaa_2 = am05_c*(2.0*ss*fx_b - dfx_b*ss2)/(fx_b*fx_b);
+  dflaa   = (dflaa_1*flaa_2 - flaa_1*dflaa_2)/(flaa_2*flaa_2);
 
+  if(dfdx!=NULL){
     *dfdx  = dxx*(1.0 - flaa) + dflaa*(1.0 - xx);
     *ldfdx = -am05_alpha; /* -alpha?? */
 
@@ -85,6 +89,26 @@ func(const XC(gga_type) *p, FLOAT x, FLOAT *f, FLOAT *dfdx, FLOAT *ldfdx, FLOAT 
     *ldfdx *= X2S*X2S;
   }
 
+  if(d2fdx2==NULL) return; /* nothing else to do */
+
+  d2lam_x  = 0.5*dlam_x/ss;
+  d2ww     = (dww*lam_x*dlam_x + ww*(1.0 + ww)*lam_x*d2lam_x - ww*(1.0 + ww)*dlam_x*dlam_x) /
+    (lam_x*lam_x*(1+ww)*(1+ww));
+  d2z_t    = POW(1.5, 2.0/3.0)*2.0/3.0*(-POW(ww, -4.0/3.0)*dww*dww/3.0 + POW(ww, -1.0/3.0)*d2ww);
+
+  d2z_tt   = POW(z_tt_factor + z_t2, -7.0/4.0)*
+    ((d2z_t*(z_tt_factor + z_t2) - 3.0/2.0*z_t*dz_t*dz_t)*(z_tt_factor + 3.0*z_t2/2.0) + 
+     (z_tt_factor + z_t2)*3.0*z_t*dz_t*dz_t);
+  d2fx_b   = M_PI/3.0*(-ss*d2z_tt*z_tt - 2.0*dz_tt*z_tt + 2*ss*dz_tt*dz_tt)/(z_tt*z_tt*z_tt);
+
+  d2xx     = 2.0*am05_alpha*(3.0*am05_alpha*ss2 - 1.0) * xx*xx*xx;
+  d2flaa_1 = 2.0*am05_c;
+  d2flaa_2 = am05_c*(2.0*(fx_b*fx_b - 2.0*ss*fx_b*dfx_b + ss2*dfx_b*dfx_b) - ss2*fx_b*d2fx_b)/(fx_b*fx_b*fx_b);
+  d2flaa   = (2.0*flaa_1*dflaa_2*dflaa_2 + flaa_2*flaa_2*d2flaa_1 - flaa_2*(2.0*dflaa_1*dflaa_2 + flaa_1*d2flaa_2))/
+    (flaa_2*flaa_2*flaa_2);
+
+  *d2fdx2  = d2xx*(1.0 - flaa) - 2.0*dxx*dflaa + d2flaa*(1.0 - xx);
+  *d2fdx2 *= X2S*X2S;
 }
 
 #include "work_gga_x.c"
@@ -96,7 +120,7 @@ const XC(func_info_type) XC(func_info_gga_x_am05) = {
   XC_FAMILY_GGA,
   "R Armiento and AE Mattsson, Phys. Rev. B 72, 085108 (2005)\n"
   "AE Mattsson, R Armiento, J Paier, G Kresse, JM Wills, and TR Mattsson, J. Chem. Phys. 128, 084714 (2008).",
-  XC_PROVIDES_EXC | XC_PROVIDES_VXC,
+  XC_PROVIDES_EXC | XC_PROVIDES_VXC | XC_PROVIDES_FXC,
   NULL, NULL, NULL,
   work_gga_x
 };

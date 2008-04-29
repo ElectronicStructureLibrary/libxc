8f036afc62891e8ad4bfeb89c4df731bd1324fd9
Miguel Marques <marques@tddft.org>
2008-04-29 07:56:21 +0000

    Now also PW92 is OK (including the second derivatives)

--- src/lda_c_pw.c
+++ src/lda_c_pw.c
@@ -69,148 +69,105 @@ static void g(int func, int k, FLOAT *rs, FLOAT *f, FLOAT *dfdrs, FLOAT *d2fdrs2
       {10.357,   3.6231,   0.88026, 0.49671}
     }};
   
-  FLOAT q0, q1, q1p, b, aux;
+  FLOAT q0, dq0, q1, dq1, q2;
   
-  b = beta[func][k][0]*rs[0] + beta[func][k][1]*rs[1] + 
+  q0  = -2.0*a[func][k]*(1.0 + alpha[func][k]*rs[1]);
+  q1  =  2.0*a[func][k];
+  q1 *= beta[func][k][0]*rs[0] + beta[func][k][1]*rs[1] + 
     beta[func][k][2]*rs[0]*rs[1] + beta[func][k][3]*rs[2];
-  
-  q0     = -2.0*a[func][k]*(1.0 + alpha[func][k] * rs[1]);
-  q1     =  2.0*a[func][k]*b;
+  q2  = log(1.0 + 1.0/q1);
 
   /* the function */
-  *f = q0*log(1.0 + 1.0/q1);
+  *f = q0*q2;
   
   if(dfdrs==NULL && d2fdrs2==NULL) return; /* nothing else to do */
 
   /* and now the derivative */
-  aux = 1.0/(q1*q1 + q1);
-  q1p = a[func][k]*(beta[func][k][0]/rs[0] + 2.0*beta[func][k][1] + 
+  dq0 = -2.0*a[func][k]*alpha[func][k];
+  dq1 = a[func][k]*(beta[func][k][0]/rs[0] + 2.0*beta[func][k][1] + 
 		    3.0*beta[func][k][2]*rs[0] + 4.0*beta[func][k][3]*rs[1]);
 
   if(dfdrs!=NULL)
-    *dfdrs = -2.0*a[func][k]*alpha[func][k]*log(1.0 + 1.0/q1) - (q0*q1p)*aux;
+    *dfdrs = dq0*q2 - q0*dq1/(q1*(1.0 + q1));
 
   if(d2fdrs2 != NULL){
-    FLOAT q1pp;
+    FLOAT d2q1;
 
-    q1pp = a[func][k]*(-beta[func][k][0]/(2.0*rs[0]*rs[1]) +
+    d2q1 = a[func][k]*(-beta[func][k][0]/(2.0*rs[0]*rs[1]) +
 		       3.0*beta[func][k][2]/(2.0*rs[0]) + 4.0*beta[func][k][3]);
 
-    *d2fdrs2 = aux*(4.0*a[func][k]*alpha[func][k]*q1p - q0*q1pp + q0*q1p*q1p*(2.0*q1 + 1.0)*aux);
+    *d2fdrs2 = 1.0/(q1*(1.0 + q1))*(-2*dq0*dq1 - q0*d2q1 + q0*(2.0*q1 + 1.0)*dq1*dq1/(q1*(1.0 + q1)));
   }
 }
 
 
 /* the functional */
-static void lda_c_pw(const void *p_, const FLOAT *rho, FLOAT *ec, FLOAT *vc, FLOAT *fc)
+static inline void 
+func(const XC(lda_type) *p, FLOAT *rs, FLOAT zeta, 
+     FLOAT *zk, FLOAT *dedrs, FLOAT *dedz, 
+     FLOAT *d2edrs2, FLOAT *d2edrsz, FLOAT *d2edz2)
 {
-  XC(lda_type) *p = (XC(lda_type) *)p_;
+  int func;
 
-  FLOAT dens, zeta;
-  FLOAT rs[3], Dec_Drs, D2ec_Drs2, ec0, *dp;
-  int func = p->info->number - XC_LDA_C_PW;
-  
+  func = p->info->number - XC_LDA_C_PW;
   assert(func==0 || func==1 || func==2);
   
-  XC(rho2dzeta)(p->nspin, rho, &dens, &zeta);
-
-  /* Wigner radius */
-  rs[1] = RS(dens);
-  rs[0] = sqrt(rs[1]);
-  rs[2] = rs[1]*rs[1];
-  
   /* ec(rs, 0) */
-  dp = (fc == NULL) ? NULL : (&D2ec_Drs2);
-  g(func, 0, rs, &ec0, &Dec_Drs, dp);
+  g(func, 0, rs, zk, dedrs, d2edrs2);
   
-  if(p->nspin == XC_UNPOLARIZED){
-    if(ec != NULL)
-      *ec = ec0;
-
-    if(vc != NULL)
-      vc[0] = (*ec) - (rs[1]/3.0)*Dec_Drs;
-    
-    if(fc != NULL){
-      FLOAT Drs = -(4.0*M_PI/9.0)*rs[2]*rs[2];
-      fc[0] = (2.0*Dec_Drs - rs[1]*D2ec_Drs2)*Drs/3.0;
-    }
-
-  }else{
+  if(p->nspin == XC_POLARIZED){
     static FLOAT fz20[3] = {
       1.709921,                           /* PW */
       1.709920934161365617563962776245,   /* PW (modified) */
       1.709921                            /* OB */
     };
 
-    FLOAT fz, fpz, z4, ec1, alphac;
-    FLOAT ectmp, Dec0_Drs, Dec1_Drs, D2ec1_Drs2, Dalphac_Drs, D2alphac_Drs2, Dec_Dz;
+    FLOAT ecp, vcp, fcp, ecf, vcf, fcf, alpha, dalpha, d2alpha;
+    FLOAT z2, z3, z4, fz, dfz, d2fz;
     
-    fz  =  FZETA(zeta);
-    fpz = DFZETA(zeta);
-    z4  = POW(zeta, 4);
+    /* store paramagnetic values */
+    ecp = *zk;
+    if(dedrs   != NULL) vcp = *dedrs;
+    if(d2edrs2 != NULL) fcp = *d2edrs2;   
 
-    /* ec(rs, 1) */
-    dp = (fc == NULL) ? NULL : (&D2ec1_Drs2);
-    g(func, 1, rs, &ec1, &Dec1_Drs, dp);
+    /* get ferromagnetic values */
+    g(func, 1, rs, &ecf, dedrs, d2edrs2);
+    if(dedrs   != NULL) vcf = *dedrs;
+    if(d2edrs2 != NULL) fcf = *d2edrs2;
 
-    /* -alpha_c(rs) */
-    dp = (fc == NULL) ? NULL : (&D2alphac_Drs2);
-    g(func, 2, rs, &alphac, &Dalphac_Drs, dp);
+    /* get alpha_c */
+    g(func, 2, rs, &alpha, dedrs, d2edrs2);
+    alpha = -alpha;
+    if(dedrs   != NULL) dalpha  = -(*dedrs);
+    if(d2edrs2 != NULL) d2alpha = -(*d2edrs2);
 
-    /* what is parametrized is -alpha, so we change signs */
-    alphac = -alphac; Dalphac_Drs = -Dalphac_Drs;
-    if(dp) D2alphac_Drs2 = -D2alphac_Drs2;
-    
-    /* save copies that will be needed later */
-    Dec0_Drs = Dec_Drs;
+    fz  = FZETA(zeta);
+    z2  = zeta*zeta;
+    z3  = zeta*z2;
+    z4  = zeta*z3;
+    *zk = ecp + z4*fz*(ecf - ecp - alpha/fz20[func]) + fz*alpha/fz20[func];
 
-    ectmp   =  ec0 + z4*fz*(ec1 - ec0 - alphac/fz20[func]) + fz*alphac/fz20[func];
+    if(dedrs==NULL && d2edrs2==NULL) return; /* nothing else to do */
 
-    Dec_Drs = Dec0_Drs + z4*fz*(Dec1_Drs - Dec0_Drs - Dalphac_Drs/fz20[func]) + fz*Dalphac_Drs/fz20[func];
-    Dec_Dz  = (4.0*POW(zeta, 3)*fz + z4*fpz)*(ec1 - ec0 - alphac/fz20[func])
-      + fpz*alphac/fz20[func];
-    
-    if(ec != NULL)
-      *ec = ectmp;
-
-    if(vc != NULL){
-      vc[0] = ectmp - (rs[1]/3.0)*Dec_Drs - (zeta - 1.0)*Dec_Dz;
-      vc[1] = ectmp - (rs[1]/3.0)*Dec_Drs - (zeta + 1.0)*Dec_Dz;
-    }    
-
-    if(fc != NULL){
-      FLOAT tmp, fppz, D2ec_Dz2, D2ec_DrsDz;
-      FLOAT Drs = -(4.0*M_PI/9.0)*rs[2]*rs[2];
-
-      fppz = 0.0;
-      if(zeta > -1.0) fppz += POW(1.0 + zeta, -2.0/3.0);
-      if(zeta <  1.0) fppz += POW(1.0 - zeta, -2.0/3.0);
-      fppz *= 4.0/(9.0*FZETAFACTOR);
-
-      D2ec_Drs2  = D2ec_Drs2 + z4*fz*(D2ec1_Drs2 - D2ec_Drs2 - D2alphac_Drs2/fz20[func])
-	+ fz*D2alphac_Drs2/fz20[func];
-
-      D2ec_DrsDz = 4.0*POW(zeta, 3)*fz*(Dec1_Drs - Dec0_Drs - Dalphac_Drs/fz20[func]) + 
-	fpz*(z4*(Dec1_Drs - Dec0_Drs) + (1.0 - z4)*Dalphac_Drs/fz20[func]);
-
-      D2ec_Dz2   = 4.0*POW(zeta, 2)*(ec1 - ec0 - alphac/fz20[func])*(3.0*fz + 2.0*zeta*fpz) + 
-	fppz*(z4*(ec1 - ec0) + (1.0 - z4)*alphac/fz20[func]);
-    
-      tmp = (2.0*Dec_Drs - rs[1]*D2ec_Drs2)*Drs/3.0;
-
-      fc[0] = tmp - D2ec_DrsDz*(zeta - 1.0)*Drs 
-	+ (zeta - 1.0)/dens*(D2ec_DrsDz*rs[1]/3.0 + (zeta - 1.0)*D2ec_Dz2);
-      
-      fc[1] = tmp - D2ec_DrsDz*(zeta - 1.0)*Drs 
-	+ (zeta + 1.0)/dens*(D2ec_DrsDz*rs[1]/3.0 + (zeta - 1.0)*D2ec_Dz2);
-
-      fc[2] = tmp - D2ec_DrsDz*(zeta + 1.0)*Drs 
-	+ (zeta + 1.0)/dens*(D2ec_DrsDz*rs[1]/3.0 + (zeta + 1.0)*D2ec_Dz2);
-
+    dfz = DFZETA(zeta);
+    if(dedrs!=NULL){
+      *dedrs = vcp + z4*fz*(vcf - vcp - dalpha/fz20[func]) + fz*dalpha/fz20[func];
+      *dedz  = (4.0*z3*fz + z4*dfz)*(ecf - ecp - alpha/fz20[func])
+	+ dfz*alpha/fz20[func];
     }
+
+    if(d2edrs2==NULL) return; /* nothing else to do */
+    
+    d2fz = D2FZETA(zeta);
+    *d2edrs2 = fcp + z4*fz*(fcf - fcp - d2alpha/fz20[func]) + fz*d2alpha/fz20[func];
+    *d2edrsz = (4.0*z3*fz + z4*dfz)*(vcf - vcp - dalpha/fz20[func])
+	+ dfz*dalpha/fz20[func];
+    *d2edz2  = (4.0*3.0*z2*fz + 8.0*z3*dfz + z4*d2fz)*(ecf - ecp - alpha/fz20[func])
+	+ d2fz*alpha/fz20[func];
   }
 }
 
+#include "work_lda.c"
 
 const XC(func_info_type) XC(func_info_lda_c_pw) = {
   XC_LDA_C_PW,
@@ -219,9 +176,9 @@ const XC(func_info_type) XC(func_info_lda_c_pw) = {
   XC_FAMILY_LDA,
   "JP Perdew and Y Wang, Phys. Rev. B 45, 13244 (1992)",
   XC_PROVIDES_EXC | XC_PROVIDES_VXC | XC_PROVIDES_FXC,
-  NULL,
-  NULL,
-  lda_c_pw
+  NULL,     /* init */
+  NULL,     /* end  */
+  work_lda, /* lda  */
 };
 
 const XC(func_info_type) XC(func_info_lda_c_pw_mod) = {
@@ -233,9 +190,9 @@ const XC(func_info_type) XC(func_info_lda_c_pw_mod) = {
   "Added extra digits to some constants as in the PBE routine\n"
   "http://dft.rutgers.edu/pubs/PBE.asc",
   XC_PROVIDES_EXC | XC_PROVIDES_VXC | XC_PROVIDES_FXC,
-  NULL,
-  NULL,
-  lda_c_pw
+  NULL,     /* init */
+  NULL,     /* end  */
+  work_lda, /* lda  */
 };
 
 const XC(func_info_type) XC(func_info_lda_c_ob_pw) = {
@@ -247,7 +204,7 @@ const XC(func_info_type) XC(func_info_lda_c_ob_pw) = {
   "G Ortiz and P Ballone, Phys. Rev. B 56, 9970(E) (1997)\n"
   "JP Perdew and Y Wang, Phys. Rev. B 45, 13244 (1992)",
   XC_PROVIDES_EXC | XC_PROVIDES_VXC | XC_PROVIDES_FXC,
-  NULL,
-  NULL,
-  lda_c_pw
+  NULL,     /* init */
+  NULL,     /* end  */
+  work_lda, /* lda  */
 };

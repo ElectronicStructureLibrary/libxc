37184b9bbddeacebc962486bf0713fa8d482597d
Miguel Marques <marques@tddft.org>
2008-04-30 13:18:35 +0000

    Now also FT97 gets second derivatives. This took me almost a day to do :((

--- src/gga_x_ft97.c
+++ src/gga_x_ft97.c
@@ -24,12 +24,15 @@
 #define XC_GGA_X_FT97_B       115 /* Filatov & Thiel 97 (version B) */
 
 static inline void
-func(XC(gga_type) *p, FLOAT x, FLOAT sigma, FLOAT *f, FLOAT *dfdx, FLOAT *ldfdx, FLOAT *vsigma)
+func(const XC(gga_type) *p, FLOAT x, FLOAT sigma, FLOAT *f, FLOAT *dfdx, FLOAT *ldfdx, 
+     FLOAT *vsigma, FLOAT *d2fdx2, FLOAT *v2sigma2, FLOAT *v2sigmax)
 {
   static const FLOAT 
     beta0 = 0.002913644, beta1 = 0.0009474169, beta2 = 6255746.320201; /* beta2 = 2501.149^2 ?? (Eq. (16a) */
 
-  FLOAT x2, beta, dbetadsigma, f1, f2, f3;
+  FLOAT x2, beta, dbetadsigma, d2betadsigma2; 
+  FLOAT f0, f1, f2, df2, d2f2, f3, df3, d2f3;
+  FLOAT df3df2, d2f3df2, d2f3df2x;
   int func;
 
   switch(p->info->number){
@@ -37,13 +40,11 @@ func(XC(gga_type) *p, FLOAT x, FLOAT sigma, FLOAT *f, FLOAT *dfdx, FLOAT *ldfdx,
   default:              func = 0; /*  XC_GGA_X_FT97_A */
   }
 
-  if(func == 0){
+  if(func==0){
     beta = 0.00293;
-    dbetadsigma = 0.0;
   }else{
     f1   = beta2 + sigma;
     beta = beta0 + beta1*sigma/f1;
-    dbetadsigma = beta1*beta2/(f1*f1);
   }
 
   x2 = x*x;
@@ -51,10 +52,38 @@ func(XC(gga_type) *p, FLOAT x, FLOAT sigma, FLOAT *f, FLOAT *dfdx, FLOAT *ldfdx,
   f3 = sqrt(1.0 + 9.0*x2*f2*f2);
   *f = 1.0 + beta/X_FACTOR_C*x2/f3;
  
-  *dfdx = beta/X_FACTOR_C*2.0*x*( f3 - 9.0/2.0*x2/f3*(f2*f2 + 2.0*x2*beta*f2/sqrt(1 + x2*x2)) )/(f3*f3);
-  *ldfdx= beta0/X_FACTOR_C;
+  if(dfdx==NULL && d2fdx2==NULL) return; /* nothing else to do */
 
-  *vsigma = dbetadsigma*x2/(f3*X_FACTOR_C)*(1.0 - 9.0*x2*f2*f2/(f3*f3));
+  f0  = sqrt(1.0 + x2*x2);
+  df2 = beta*2.0*x/f0;
+  df3 = 9.0*x*f2*(f2 + x*df2)/f3;
+
+  dbetadsigma = (func == 0) ? 0.0 : beta1*beta2/(f1*f1);
+
+  if(dfdx!=NULL){
+    *dfdx = beta/X_FACTOR_C*x*(2.0*f3 - x*df3)/(f3*f3);
+    *ldfdx= beta0/X_FACTOR_C;
+
+    df3df2  = 9.0*x2*f2/f3;
+    *vsigma = dbetadsigma*x2/(f3*X_FACTOR_C)*(1.0 - f2*df3df2/f3);
+  }
+
+  if(d2fdx2==NULL) return; /* nothing else to do */
+
+  d2f2 = beta*2.0*(1.0 - x2*x2)/(f0*f0*f0);
+  d2f3 = 9.0*(x2*f3*df2*df2 + f2*f2*(f3 - x*df3) + x*f2*(df2*(4.0*f3 - x*df3) + x*f3*d2f2)) /
+    (f3*f3);
+
+  *d2fdx2 = beta/X_FACTOR_C*(2.0*(f3*f3 - 2.0*x*f3*df3 + x2*df3*df3) - x2*f3*d2f3)/(f3*f3*f3);
+
+  d2betadsigma2 = (func == 0) ? 0.0 : -2.0*dbetadsigma/f1;
+  d2f3df2       = 9.0*x2*(f3 - f2*df3df2)/(f3*f3);
+  *v2sigma2     = d2betadsigma2*x2/(f3*X_FACTOR_C)*(1.0 - f2*df3df2/f3)
+    - dbetadsigma*dbetadsigma*x2*f2/(f3*f3*X_FACTOR_C*beta)*(2.0*df3df2 - 2.0*df3df2*df3df2*f2/f3 + d2f3df2*f2);
+
+  d2f3df2x      = 9.0*x*(2*f2/f3 + x*(df2*f3 - f2*df3)/(f3*f3));
+  *v2sigmax     = dbetadsigma*2.0*x/(f3*X_FACTOR_C)*(1.0 - f2*df3df2/f3)
+    - dbetadsigma/X_FACTOR_C*x2/(f3*f3)*(df3 + (df2*f3 - 2.0*f2*df3)/f3*df3df2 + d2f3df2x*f2);
 }
 
 #define HEADER 2
@@ -66,7 +95,7 @@ const XC(func_info_type) XC(func_info_gga_x_ft97_a) = {
   "Filatov & Thiel 97 (version A)",
   XC_FAMILY_GGA,
   "M Filatov and W Thiel, Mol. Phys 91, 847 (1997)",
-  XC_PROVIDES_EXC | XC_PROVIDES_VXC,
+  XC_PROVIDES_EXC | XC_PROVIDES_VXC | XC_PROVIDES_FXC,
   NULL, NULL, NULL, 
   work_gga_x
 };
@@ -77,7 +106,7 @@ const XC(func_info_type) XC(func_info_gga_x_ft97_b) = {
   "Filatov & Thiel 97 (version B)",
   XC_FAMILY_GGA,
   "M Filatov and W Thiel, Mol. Phys 91, 847 (1997)",
-  XC_PROVIDES_EXC | XC_PROVIDES_VXC,
+  XC_PROVIDES_EXC | XC_PROVIDES_VXC | XC_PROVIDES_FXC,
   NULL, NULL, NULL, 
   work_gga_x
 };

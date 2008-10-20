6636052c864deb65ce37939abdb28bc55ba7519f
Miguel Marques <marques@tddft.org>
2008-10-20 09:39:57 +0000

    Sorry for the mess: it should compile now!

--- libxc/src/xc_f.c
+++ libxc/src/xc_f.c
@@ -142,10 +142,10 @@ void XC_FC_FUNC(f90_lda_kxc, F90_LDA_KXC)
 /* Now come some special initializations */
 
 /* parameter of PRM08 */
-void XC_FC_FUNC(f90_lda_c_prm08_set_params, F90_LDA_C_PRM08_SET_PARAMS)
+void XC_FC_FUNC(f90_lda_c_2d_prm08_set_params, F90_LDA_C_2D_PRM08_SET_PARAMS)
   (void **p, FLOAT *N)
 {
-  XC(lda_c_prm08_set_params)((XC(lda_type) *)(*p), *N);
+  XC(lda_c_2d_prm08_set_params)((XC(lda_type) *)(*p), *N);
 }
 
 /* exchange in the LDA */

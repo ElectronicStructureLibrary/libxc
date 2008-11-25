5fb81529818bbf8d1c4b633982ec6fa00454790c
Miguel Marques <marques@tddft.org>
2008-11-25 10:31:48 +0000

    Rewrote the B97 class of functionals. Now it also includes fxc and some mroe elements of this family. BTW, Xavier, included is the functional of Grimme that you sent me long ago... of course that to use it, one should also add the vdW term (trivial, actually)

--- src/test.c
+++ src/test.c
@@ -60,27 +60,32 @@ void test_lda()
 void test_tpss()
 {
   XC(mgga_type) tpss;
+  XC(gga_type) agga;
   int i;
 
-  XC(mgga_init)(&tpss, XC_MGGA_X_LTA, XC_POLARIZED);
+  //XC(mgga_init)(&tpss, XC_MGGA_X_LTA, XC_POLARIZED);
+  XC(gga_init)(&agga, XC_GGA_XC_B97, XC_UNPOLARIZED);
   
-  for(i=0; i<1; i++){
+  for(i=0; i<1000; i++){
     double rho[2], sigma[3], tau[2];
     double zk, vrho[2], vsigma[3], vtau[2];
     double v2rho2[3], v2rhosigma[6], v2sigma2[6], v2rhotau[4], v2tausigma[6], v2tau2[3];
 
-    rho[0]   = 1.021446e-09; //0.15;
-    rho[1]   = 0.0000000000000000; //0.23;
-    sigma[0] = 1.152303e-16; //0.01 + i/1000.0;
-    sigma[1] = 0.0; //0.11;
-    sigma[2] = 0.0; //0.7;
-    tau[0]   = 3.850065e-08; //0.22;
-    tau[1]   = 0.0; //0.15;
+    rho[0]   = 0.23;
+    rho[1]   = 0.15;
+    sigma[0] = 0.01 + i/1000.0;
+    sigma[1] = 0.11;
+    sigma[2] = 0.7;
+    tau[0]   = 0.22;
+    tau[1]   = 0.15;
 
-    XC(mgga)(&tpss, rho,  sigma,  tau, 
-	     &zk,  vrho, vsigma, vtau, 
-	     NULL, v2rhosigma, v2sigma2, v2rhotau, v2tausigma, v2tau2);
-    printf("%16.10lf\t%16.10lf\t%16.10lf\n", rho[0], (rho[0]+rho[1])*zk, vrho[0]);
+    //XC(mgga)(&agga, rho,  sigma,  tau, 
+    //	     &zk,  vrho, vsigma, vtau, 
+    //	     v2rho2, v2rhosigma, v2sigma2, v2rhotau, v2tausigma, v2tau2);
+    XC(gga)(&agga, rho,  sigma,
+	    &zk,  vrho, vsigma,
+	    v2rho2, v2rhosigma, v2sigma2);
+    printf("%16.10lf\t%16.10lf\t%16.10lf\t%16.10lf\n", sigma[0], (rho[0]+rho[1])*zk, vrho[0], v2rhosigma[0]);
   }
 }
 

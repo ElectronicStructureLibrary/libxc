2c7b94bb28df8c2473689c59f87748c11e3ce243
Miguel Marques <marques@tddft.org>
2008-11-28 15:40:25 +0000

    Cleaned up a bit the hybrid GGAs, and added some more hybrids on the process. Let me see if I don't forget any: XC_HYB_GGA_XC_mPW3PW, XC_HYB_GGA_XC_B1LYP, XC_HYB_GGA_XC_B1PW91, XC_HYB_GGA_XC_mPW1PW, XC_HYB_GGA_XC_mPW3LYP

--- libxc/src/gga_xc_xlyp.c
+++ libxc/src/gga_xc_xlyp.c
@@ -26,68 +26,29 @@
 static void
 gga_xc_xlyp_init(void *p_)
 {
+  const FLOAT cc[3] = {0.722, 0.347, 1.0};
+
   XC(gga_type) *p = (XC(gga_type) *)p_;
-  int i;
 
-  p->lda_aux = (XC(lda_type) *) malloc(sizeof(XC(lda_type)));
-  XC(lda_x_init)(p->lda_aux, p->nspin, 3, XC_NON_RELATIVISTIC);
+  p->mix = (XC(mix_func_type) *) malloc(sizeof(XC(mix_func_type)));
+  XC(mix_func_init)(p->mix, XC_FAMILY_GGA, p->nspin);
 
-  p->gga_aux = (XC(gga_type) **) malloc(3*sizeof(XC(gga_type) *));
-  for(i=0; i<3; i++)
-    p->gga_aux[i] = (XC(gga_type) *) malloc(sizeof(XC(gga_type)));
+  p->mix->lda_n = 1;
+  p->mix->gga_n = 3;
+  XC(mix_func_alloc)(p->mix);
 
-  XC(gga_init)(p->gga_aux[0], XC_GGA_X_B88, p->nspin);
-  XC(gga_init)(p->gga_aux[1], XC_GGA_X_PW91, p->nspin);
-  XC(gga_init)(p->gga_aux[2], XC_GGA_C_LYP, p->nspin);
+  XC(lda_x_init)(&p->mix->lda_mix[0], p->nspin, 3, XC_NON_RELATIVISTIC);
+  p->mix->lda_coef[0] = 1.0 - cc[0] - cc[1];
+
+  XC(gga_init)(&p->mix->gga_mix[0], XC_GGA_X_B88,  p->nspin);
+  p->mix->gga_coef[0] = cc[0];
+
+  XC(gga_init)(&p->mix->gga_mix[1], XC_GGA_X_PW91, p->nspin);
+  p->mix->gga_coef[0] = cc[1];
+
+  XC(gga_init)(&p->mix->gga_mix[2], XC_GGA_C_LYP,  p->nspin);
+  p->mix->gga_coef[0] = cc[2];
 }
-
-static void
-gga_xc_xlyp_end(void *p_)
-{
-  XC(gga_type) *p = (XC(gga_type) *)p_;
-  int i;
-
-  for(i=0; i<3; i++){
-    XC(gga_end)(p->gga_aux[i]);
-    free(p->gga_aux[i]);
-  }
-  free(p->gga_aux);
-}
-
-static void 
-gga_xc_xlyp(void *p_, FLOAT *rho, FLOAT *sigma,
-            FLOAT *e, FLOAT *vrho, FLOAT *vsigma)
-{
-  static FLOAT cc[3] = {0.722, 0.347, 1.0};
-
-  XC(gga_type) *p = p_;
-  FLOAT dd, e1, vrho1[2], vsigma1[3];
-  int ifunc, is, js;
-
-  dd = 1.0 - cc[0] - cc[1];
-
-  XC(lda_vxc)(p->lda_aux, rho, &e1, vrho1);
-  *e = dd*e1;
-  for(is=0; is<p->nspin; is++)
-    vrho[is] = dd*vrho1[is];
-  
-  js = (p->nspin == XC_UNPOLARIZED) ? 1 : 3;
-  for(is=0; is<js; is++)
-    vsigma[is] = 0.0;
-
-  for(ifunc=0; ifunc<3; ifunc++){
-    XC(gga_vxc)(p->gga_aux[ifunc], rho, sigma, &e1, vrho1, vsigma1);
-
-    *e += cc[ifunc]*e1;
-    for(is=0; is<p->nspin; is++)
-      vrho[is] += cc[ifunc]*vrho1[is];
-
-    for(is=0; is<js; is++)
-      vsigma[is] += cc[ifunc]*vsigma1[is];
-  }
- 
-}
-
 
 const XC(func_info_type) XC(func_info_gga_xc_xlyp) = {
   XC_GGA_XC_XLYP,
@@ -97,7 +58,5 @@ const XC(func_info_type) XC(func_info_gga_xc_xlyp) = {
   "X Xu and WA Goddard, III, PNAS 101, 2673 (2004)",
   XC_PROVIDES_EXC | XC_PROVIDES_VXC,
   gga_xc_xlyp_init, 
-  gga_xc_xlyp_end, 
-  NULL,
-  gga_xc_xlyp
+  NULL, NULL, NULL
 };

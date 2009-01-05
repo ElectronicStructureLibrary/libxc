f4650a46bdf5fd75e7712845f1ab8679f87f4500
Miguel Marques <marques@tddft.org>
2009-01-05 10:41:59 +0000

    Added a new form for the exachange enhancement factor that comes from a bayesian best fit procedure

---
+++ libxc/src/gga_x_bayesian.c
@@ -0,0 +1,68 @@
+/*
+ Copyright (C) 2006-2007 M.A.L. Marques
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+  
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+  
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <stdio.h>
+#include <assert.h>
+#include "util.h"
+
+#define XC_GGA_X_BAYESIAN          125 /* Bayesian best fit for the enhancement factor */
+
+static inline void 
+func(const XC(gga_type) *p, FLOAT x, FLOAT *f, FLOAT *dfdx, FLOAT *ldfdx, FLOAT *d2fdx2)
+{
+  static const FLOAT theta[3] = {
+    1.0008, 0.1926, 1.8962
+  };
+
+  FLOAT ss, f0, df0, d2f0;
+
+  ss = X2S*x;
+
+  f0 = ss/(1.0 + ss);
+
+  *f = theta[0] + f0*f0*(theta[1] + f0*f0*theta[2]);
+
+  if(dfdx==NULL && d2fdx2==NULL) return; /* nothing else to do */
+
+  df0 = 1.0/((1.0 + ss)*(1.0 + ss));
+
+  if(dfdx!=NULL){
+    *dfdx  = X2S*f0*(2.0*theta[1] + 4.0*f0*f0*theta[2])*df0;
+    *ldfdx = X2S*X2S*theta[1];
+  }
+
+  if(d2fdx2==NULL) return; /* nothing else to do */
+
+  d2f0 = -2.0*df0/(1.0 + ss);
+  *d2fdx2 = X2S*X2S*
+    (f0*(2.0*theta[1] + 4.0*f0*f0*theta[2])*d2f0 +
+     (2.0*theta[1] + 4.0*3.0*f0*f0*theta[2])*df0*df0);
+}
+
+#include "work_gga_x.c"
+
+const XC(func_info_type) XC(func_info_gga_x_bayesian) = {
+  XC_GGA_X_BAYESIAN,
+  XC_EXCHANGE,
+  "Bayesian best fit for the enhancement factor",
+  XC_FAMILY_GGA,
+  "JJ Mortensen, K Kaasbjerg, SL Frederiksen, JK Nørskov, JP Sethna, and KW Jacobsen, Phys. Rev. Lett. 95, 216401 (2005)",
+  XC_PROVIDES_EXC | XC_PROVIDES_VXC | XC_PROVIDES_FXC,
+  NULL, NULL, NULL,
+  work_gga_x
+};

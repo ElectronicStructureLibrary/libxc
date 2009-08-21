17e67bafe97626a00d21fb5a6e82e9fc2af6a979
Miguel Marques <marques@tddft.org>
2009-08-21 09:21:05 +0000

    Added the soft-Coulomb version of the 1d_csc lda

--- libxc/src/lda_c_1d_csc.c
+++ libxc/src/lda_c_1d_csc.c
@@ -23,16 +23,23 @@
 
 #define XC_LDA_C_1D_CSC          18 /* Casula, Sorella, and Senatore 1D correlation     */
 
+typedef struct{
+  int interaction;  /* 0: exponentially screened; 1: soft-Coulomb */
+  int ii;           /* index in the parameter list */
+
+  FLOAT bb;         /* screening parameter */
+} lda_c_1d_csc_params;
+
 static void 
 lda_c_1d_csc_init(void *p_)
 {
   XC(lda_type) *p = (XC(lda_type) *)p_;
 
   assert(p->params == NULL);
-  p->params = malloc(sizeof(int));
+  p->params = malloc(sizeof(lda_c_1d_csc_params));
 
-  /* default value is 1.0 for no particular reason */
-  XC(lda_c_1d_csc_set_params)(p, 1.0);
+  /* default value is soft-Coulomb with beta=1.0 */
+  XC(lda_c_1d_csc_set_params)(p, 1, 1.0);
 }
 
 static void 
@@ -46,55 +53,67 @@ lda_c_1d_csc_end(void *p_)
 }
 
 void 
-XC(lda_c_1d_csc_set_params)(XC(lda_type) *p, FLOAT bb)
+XC(lda_c_1d_csc_set_params)(XC(lda_type) *p, int interaction, FLOAT bb)
 {
-  int ii;
+  lda_c_1d_csc_params *params = (lda_c_1d_csc_params *)(p->params);
 
-  assert(p->params != NULL);
+  assert(params != NULL);
 
-  if     (bb == 0.1)
-    ii = 0;
-  else if(bb == 0.3)
-    ii = 1;
-  else if(bb == 0.5)
-    ii = 2;
-  else if(bb == 0.75)
-    ii = 3;
-  else if(bb == 1.0)
-    ii = 4;
-  else if(bb == 2.0)
-    ii = 5;
-  else if(bb == 4.0)
-    ii = 6;
-  else{
-    fprintf(stderr, "Invalid value of parameter b = %f in lda_c_1d_csc_set_params", bb);
+  params->interaction = -1;
+  params->ii          = -1;
+
+  if(interaction == 0){
+    if     (bb == 0.1)
+      params->ii = 0;
+    else if(bb == 0.3)
+      params->ii = 1;
+    else if(bb == 0.5)
+      params->ii = 2;
+    else if(bb == 0.75)
+      params->ii = 3;
+    else if(bb == 1.0)
+      params->ii = 4;
+    else if(bb == 2.0)
+      params->ii = 5;
+    else if(bb == 4.0)
+      params->ii = 6;
+  }else if(interaction == 1){
+    if     (bb == 1.0)
+      params->ii = 7 + 0;
+  }
+
+  if(params->ii < 0){
+    fprintf(stderr, "Invalid value of parameters (inter,b) = (%d,%f) in lda_c_1d_csc_set_params", interaction, bb);
     exit(1);
   }
 
-  *((int *)p->params) = ii;
+  params->interaction = interaction;
+  params->bb          = bb;
 }
 
 static inline void
 func(const XC(lda_type) *p, XC(lda_rs_zeta) *r)
 {
   static const struct {
-    FLOAT A, B, C, n, alpha, beta, m;
+    FLOAT A, B, C, D, n, alpha, beta, m;
   } pp[] = {
-    {  4.66,  2.092, 3.735, 1.379, 23.63,  109.9,    1.837},
-    {  9.5,   1.85,  5.64,  0.882,  5.346,   6.69,   3.110},
-    { 16.40,  2.90,  6.235, 0.908,  3.323,   2.23,   3.368},
-    { 22.53,  2.09,  7.363, 0.906,  2.029,   0.394,  4.070},
-    { 32.1,   3.77,  7.576, 0.941,  1.63,    0.198,  4.086},
-    {110.5,   7.90,  8.37,  1.287,  1.399,   0.0481, 4.260},
-    {413.0,  10.8,   7.99,  1.549,  1.308,   0.0120, 4.165}
+    {  4.66,  2.092, 3.735, 0.0, 1.379, 23.63,  109.9,    1.837}, /* exponentially screened interaction */
+    {  9.5,   1.85,  5.64,  0.0, 0.882,  5.346,   6.69,   3.110},
+    { 16.40,  2.90,  6.235, 0.0, 0.908,  3.323,   2.23,   3.368},
+    { 22.53,  2.09,  7.363, 0.0, 0.906,  2.029,   0.394,  4.070},
+    { 32.1,   3.77,  7.576, 0.0, 0.941,  1.63,    0.198,  4.086},
+    {110.5,   7.90,  8.37,  0.0, 1.287,  1.399,   0.0481, 4.260},
+    {413.0,  10.8,   7.99,  0.0, 1.549,  1.308,   0.0120, 4.165},
+
+    {18.40, 7.501, 0.10185, 0.012827, 1.0, 1.511, 0.258, 4.424}, /* soft-Coulomb interaction */
   };
 
   int ii;
-  FLOAT rs_n, rs_m, arg, larg, den;
-  FLOAT darg, dden;
+  FLOAT rs_n, rs_m, arg, larg, den, num;
+  FLOAT darg, dden, dnum;
 
   assert(p->params != NULL);
-  ii = *((int *)p->params);
+  ii = ((lda_c_1d_csc_params *)p->params)->ii;
 
   rs_n = POW(r->rs[1], pp[ii].n);
   rs_m = POW(r->rs[1], pp[ii].m);
@@ -103,16 +122,18 @@ func(const XC(lda_type) *p, XC(lda_rs_zeta) *r)
   larg = LOG(arg);
 
   den  = pp[ii].A + pp[ii].B*rs_n + pp[ii].C*r->rs[2];
+  num  = r->rs[1] + pp[ii].D*r->rs[2];
 
-  r->zk  = -r->rs[1]*larg/den;
+  r->zk  = -num*larg/den;
   r->zk /= 2.0; /* conversion from Ry to Hartree */
 
   if(r->order < 1) return;
 
-  darg = pp[ii].alpha*r->rs[1] + pp[ii].beta*pp[ii].m*rs_m; /* times rs */
-  dden = pp[ii].B*pp[ii].n*rs_n + 2.0*pp[ii].C*r->rs[2];    /* times rs */
+  darg = pp[ii].alpha + pp[ii].beta*pp[ii].m*rs_m/r->rs[1];
+  dden = pp[ii].B*pp[ii].n*rs_n/r->rs[1] + 2.0*pp[ii].C*r->rs[1];
+  dnum = 2.0*pp[ii].D*r->rs[1];
 
-  r->dedrs  = -((larg + darg/arg)*den - dden*larg)/(den*den);
+  r->dedrs  = -((dnum*larg + num*darg/arg)*den - dden*num*larg)/(den*den);
   r->dedrs /= 2.0; /* conversion from Ry to Hartree */
 
   r->dedz   = 0.0; /* apparently the function is spin-unpolarized only */

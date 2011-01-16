97c7eea54eea8b0830a9cd6526e8ce003b3d121c
Xavier Andrade <xavier@tddft.org>
2011-01-16 05:52:40 +0000

    Some optimization for lda_xc_teter93.

--- libxc/src/work_lda.c
+++ libxc/src/work_lda.c
@@ -35,7 +35,16 @@ work_lda(const void *p_, int np, const FLOAT *rho,
 
   XC(lda_rs_zeta) r;
   int is, ip;
-  FLOAT cnst_rs, dens, drs, d2rs, d3rs;
+  FLOAT dens, drs, d2rs, d3rs;
+
+  /* Wigner radius */
+# if   XC_DIMENSIONS == 1
+  const FLOAT cnst_rs = 0.5;
+# elif XC_DIMENSIONS == 2
+  const FLOAT cnst_rs = 0.56418958354775627928; /* = 1.0/sqrt(M_PI) */
+# else /* three dimensions */
+  const FLOAT cnst_rs = 0.6203504908994000866; /* = POW(3.0/(4*M_PI), 1.0/3.0)*/
+# endif
 
   r.order = -1;
   if(zk     != NULL) r.order = 0;
@@ -48,15 +57,6 @@ work_lda(const void *p_, int np, const FLOAT *rho,
     XC(rho2dzeta)(p->nspin, rho, &dens, &r.zeta);
 
     if(dens < MIN_DENS) goto end_ip_loop;
-
-  /* Wigner radius */
-# if   XC_DIMENSIONS == 1
-    cnst_rs = 1.0/2.0;
-# elif XC_DIMENSIONS == 2
-    cnst_rs = 1.0/sqrt(M_PI);
-# else /* three dimensions */
-    cnst_rs = POW(3.0/(4*M_PI), 1.0/3.0);
-# endif
 
     r.rs[1] = cnst_rs*POW(dens, -1.0/XC_DIMENSIONS);
     r.rs[0] = sqrt(r.rs[1]);

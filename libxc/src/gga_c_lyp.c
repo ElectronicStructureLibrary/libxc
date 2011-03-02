d13d0f207fb25fc02f1890956212d4dd02230947
Miguel Marques <marques@tddft.org>
2011-03-02 12:42:51 +0000

    I rewrote lyp to the new framework. Now the file is half the size as before, and it will be easy to get the second derivatives.

--- libxc/src/gga_c_lyp.c
+++ libxc/src/gga_c_lyp.c
@@ -28,7 +28,6 @@ typedef struct{
   FLOAT A, B, c, d;
 } gga_c_lyp_params;
 
-
 void XC(gga_c_lyp_init)(void *p_)
 {
   XC(gga_type) *p = (XC(gga_type) *)p_;
@@ -65,265 +64,131 @@ void XC(gga_c_lyp_set_params_)(XC(gga_type) *p, FLOAT A, FLOAT B, FLOAT c, FLOAT
 }
 
 
-static void 
-my_gga_c_lyp(const void *p_, const FLOAT *rho_, const FLOAT *sigma_,
-	     FLOAT *e, FLOAT *vrho, FLOAT *vsigma)
+static inline void 
+func(const XC(gga_type) *p, int order, FLOAT rs, FLOAT zeta, FLOAT xt, FLOAT *xs,
+     FLOAT *f, FLOAT *dfdrs, FLOAT *dfdz, FLOAT *dfdxt, FLOAT *dfdxs,
+     FLOAT *d2fdrs2, FLOAT *d2fdrsz, FLOAT *d2fdrsxt, FLOAT *d2fdrsxs, FLOAT *d2fdz2, 
+     FLOAT *d2fdzxt, FLOAT *d2fdzxs, FLOAT *d2fdxt2, FLOAT *d2fdxtxs, FLOAT *d2fdxs2)
 {
-  XC(gga_type) *p = (XC(gga_type) *)p_;
   gga_c_lyp_params *params;
 
-  static FLOAT ee = 36.462398978764767321; /* ee = 8*2^(2/3)*e */
-
-  FLOAT rho[2], sigma[2], rhot, sigmat;
-  FLOAT AA, BB, cc, dd; /* sortcuts for parameters */
-  FLOAT sfact, rhot13, rhot43, rho83[2], ZZ, delta, omega;
-  FLOAT dZZdr, ddeltadr, domegadr;
+  FLOAT AA, BB, cc, dd, Cf; /* sortcuts for parameters */
+  FLOAT cnst_rs, xt2, opz, omz, opz53, omz53, opz83, omz83, zeta2, opdrs;
+  FLOAT omega, delta, domega, ddelta;
+  FLOAT aux4, aux5, aux6;
   FLOAT t1, t2, t3, t4, t5, t6;
-  int is;
+  FLOAT dt1drs, dt2drs, dt3drs, dt4drs, dt5drs;
+  FLOAT dt1dz, dt2dz, dt3dz, dt4dz, dt5dz, dt6dz;
 
   assert(p->params != NULL);
   params = (gga_c_lyp_params *)(p->params);
 
+  cnst_rs = CBRT(4.0*M_PI/3.0);
+  Cf      = 3.0*POW(3.0*M_PI*M_PI, 2.0/3.0)/10.0;
+  xt2     = xt*xt;
+
   AA = params->A;
   BB = params->B;
-  cc = params->c;
-  dd = params->d;
+  cc = params->c*cnst_rs;
+  dd = params->d*cnst_rs;
 
-  /* convert input to spin-polarized case */
-  if(p->nspin == XC_POLARIZED){
-    sfact    = 1.0;
-    rho[0]   = rho_[0];
-    rho[1]   = rho_[1];
-    sigma[0] = sigma_[0];
-    sigma[1] = sigma_[2];
-    rhot     = rho_[0] + rho_[1];
-    sigmat   = sigma_[0] + 2.0*sigma_[1] + sigma_[2];
-  }else{
-    sfact    = 2.0;
-    rho[0]   = rho_[0]/2.0;
-    rho[1]   = rho_[0]/2.0;
-    sigma[0] = sigma_[0]/4.0;
-    sigma[1] = sigma_[0]/4.0;
-    rhot     = rho_[0];
-    sigmat   = sigma_[0];    
-  }
+  zeta2 = zeta*zeta;
+  opz = 1.0 + zeta;
+  omz = 1.0 - zeta;
+  opz53 = POW(opz, 5.0/3.0);
+  omz53 = POW(omz, 5.0/3.0);   
+  opz83 = opz*opz53;
+  omz83 = omz*omz53;
 
-  if(rhot < MIN_DENS) return;
+  opdrs = 1.0/(1.0 + dd*rs);
+  omega = BB*exp(-cc*rs)*opdrs;
+  delta = (cc + dd*opdrs)*rs;
 
-  /* some handy functions of the total density */
-  rhot13   = CBRT(rhot);
-  rhot43   = rhot*rhot13;
-  rho83[0] = POW(rho[0], 8.0/3.0);
-  rho83[1] = POW(rho[1], 8.0/3.0);
+  aux6 = 1.0/POW(2.0, 8.0/3.0);
+  aux4 = aux6/4.0;
+  aux5 = aux4/(9.0*2.0);
 
-  ZZ     = rhot13/(rhot13 + dd);
-  delta  = (cc + dd*ZZ)/rhot13;
-  omega  = exp(-cc/rhot13) * ZZ * POW(rhot, -11.0/3.0);
+  t1 = -(1.0 - zeta2)/(1.0 + dd*rs);
+  t2 = -xt2*((1.0 - zeta2)*(47.0 - 7.0*delta)/(4.0*18.0) - 2.0/3.0);
+  t3 = -Cf/2.0*(1.0 - zeta2)*(opz83 + omz83);
+  t4 =  aux4*(1.0 - zeta2)*(5.0/2.0 - delta/18.0)*(xs[0]*xs[0]*opz83 + xs[1]*xs[1]*omz83);
+  t5 =  aux5*(1.0 - zeta2)*(delta - 11.0)*(xs[0]*xs[0]*opz*opz83 + xs[1]*xs[1]*omz*omz83);
+  t6 = -aux6*(2.0/3.0*(xs[0]*xs[0]*opz83 + xs[1]*xs[1]*omz83) -
+	      opz*opz*xs[1]*xs[1]*omz83/4.0 - omz*omz*xs[0]*xs[0]*opz83/4.0);
 
-  /* and their derivatives */
-  dZZdr    = dd*ZZ*ZZ/(3.0*rhot43);
-  ddeltadr = -(cc + dd*ZZ - dd*dd*ZZ*ZZ/rhot13)/(3.0*rhot43);
-  domegadr = omega*ZZ*(cc*dd + (cc-10.0*dd)*rhot13 - 11.0*rhot13*rhot13)/(3.0*rhot43*rhot13);
+  *f  = AA*(t1 + omega*(t2 + t3 + t4 + t5 + t6));
 
-  /* we now add the terms of the xc energy one by one */
+  if(order < 1) return;
 
-  /* t1 */
-  {
-    FLOAT aux1;
+  domega = -omega*(cc + dd*opdrs);
+  ddelta = cc + dd*opdrs*(1.0 - dd*rs*opdrs);
 
-    aux1 = -4.0*AA/rhot;
-
-    t1 = aux1*rho[0]*rho[1]*ZZ;
-
-    if(vrho != NULL){
-      for(is=0; is<p->nspin; is++){
-	int js = (is==0) ? 1 : 0;
-
-	vrho[is] = aux1*(rho[js]*ZZ - rho[0]*rho[1]*(ZZ/rhot - dZZdr));
-      }
-    }
-  }
-
+  dt1drs = -dd*t1/(1.0 + dd*rs);
+  dt2drs =  xt2*(1.0 - zeta2)*7.0*ddelta/(4.0*18.0);
+  dt4drs = -aux4*(1.0 - zeta2)*ddelta/18.0*(xs[0]*xs[0]*opz83 + xs[1]*xs[1]*omz83);
+  dt5drs =  aux5*(1.0 - zeta2)*ddelta*(xs[0]*xs[0]*opz*opz83 + xs[1]*xs[1]*omz*omz83);
+  *dfdrs =  AA*(dt1drs + domega*(t2 + t3 + t4 + t5 + t6) + omega*(dt2drs + dt3drs + dt4drs + dt5drs));
   
-  /* t2 */
-  {
-    FLOAT aux1, aux2, aux3, aux4;
+  dt1dz =  2.0*zeta/(1.0 + dd*rs);
+  dt2dz =  xt2*2.0*zeta*(47.0 - 7.0*delta)/(4.0*18.0);
+  dt3dz = -Cf/2.0*(-2.0*zeta*(opz83 + omz83) + (1.0 - zeta2)*8.0/3.0*(opz53 - omz53));
+  dt4dz =  aux4*(5.0/2.0 - delta/18.0)*
+    (-2.0*zeta*(xs[0]*xs[0]*opz83 + xs[1]*xs[1]*omz83) + (1.0 - zeta2)*8.0/3.0*(xs[0]*xs[0]*opz53 - xs[1]*xs[1]*omz53));
+  dt5dz =  aux5*(delta - 11.0)*
+    (-2.0*zeta*(xs[0]*xs[0]*opz*opz83 + xs[1]*xs[1]*omz*omz83) + (1.0 - zeta2)*11/3.0*(xs[0]*xs[0]*opz83 - xs[1]*xs[1]*omz83));
+  dt6dz = -aux6*(2.0/3.0*8.0/3.0*(xs[0]*xs[0]*opz53 - xs[1]*xs[1]*omz53)
+		 - xs[1]*xs[1]/4.0*opz*( 2.0*omz83 - opz*8.0/3.0*omz53)
+		 - xs[0]*xs[0]/4.0*omz*(-2.0*opz83 + omz*8.0/3.0*opz53));
+  *dfdz =  AA*(dt1dz + omega*(dt2dz + dt3dz + dt4dz + dt5dz + dt6dz));
 
-    aux1 = -AA*BB;
-    aux2 = rho[0]*rho[1];
-    aux3 = (47.0 - 7.0*delta)/18.0;
-    aux4 = 2.0/3.0*rhot*rhot;
+  *dfdxt = -2.0*AA*omega*xt*((1.0 - zeta2)*(47.0 - 7.0*delta)/(4.0*18.0) - 2.0/3.0);
 
-    t2 = aux1*omega*sigmat*(aux2*aux3 - aux4);
+  dfdxs[0] = AA*omega*2.0*xs[0]*
+    (aux4*(1.0 - zeta2)*(5.0/2.0 - delta/18.0)*opz83 +
+     aux5*(1.0 - zeta2)*(delta - 11.0)*opz*opz83 -
+     aux6*(2.0/3.0*opz83 - omz*omz*opz83/4.0)
+     );
+  dfdxs[1] = AA*omega*2*xs[1]*
+    (aux4*(1.0 - zeta2)*(5.0/2.0 - delta/18.0)*omz83 +
+     aux5*(1.0 - zeta2)*(delta - 11.0)*omz*omz83 -
+     aux6*(2.0/3.0*omz83 - opz*opz*omz83/4.0)
+     );
 
-    if(vrho != NULL){
-      for(is=0; is<p->nspin; is++){
-	int js = (is==0) ? 1 : 0;
+  if(order < 2) return;
 
-	vrho[is] += aux1*
-	  (domegadr*sigmat*(aux2*aux3 - aux4) +
-	   omega*sigmat*(rho[js]*aux3 - aux2*7.0/18.0*ddeltadr - 4.0/3.0*rhot));
-      }
-
-      {
-	FLOAT aux5 = aux1*omega*(aux2*aux3 - aux4);
-
-	vsigma[0] += aux5/sfact;
-	if(p->nspin == XC_POLARIZED){
-	  vsigma[1] += 2.0*aux5;
-	  vsigma[2] +=     aux5;
-	}else{
-	  vsigma[0] += aux5/2.0;
-	}
-      }
-    }
-  }
-
-
-  /* t3 */
-  {
-    FLOAT aux1, aux2;
-    aux1 = -AA*BB;
-    aux2 = ee*(rho83[0] + rho83[1]);
-
-    t3 = aux1*omega*rho[0]*rho[1]*aux2;
-
-    if(vrho != NULL){
-      for(is=0; is<p->nspin; is++){
-	int js = (is==0) ? 1 : 0;
-
-	vrho[is] += aux1*domegadr*rho[0]*rho[1]*aux2;
-	vrho[is] += aux1* omega * ee
-	  * rho[js]*(11.0/3.0*rho83[is] + rho83[js]);
-      }
-    }
-  }
-
-
-  /* t4 */
-  {
-    FLOAT aux1, aux2, aux3, aux4;
-
-    aux1 = AA*BB;
-    aux2 = rho[0]*rho[1];
-    aux3 = 5.0/2.0 - delta/18.0;
-    aux4 = sigma[0] + sigma[1];
-
-    t4 = aux1*omega*aux2*aux3*aux4;
-
-    if(vrho != NULL){
-      for(is=0; is<p->nspin; is++){
-	int js = (is==0) ? 1 : 0;
-      
-	vrho[is] += aux1*aux4*
-	  (domegadr*aux2*aux3 + omega*rho[js]*aux3 -
-	   omega*aux2*ddeltadr/18.0);
-      }
-
-      {
-	FLOAT aux5 = aux1*omega*aux2*aux3;
-
-	vsigma[0] += aux5/sfact;
-	if(p->nspin == XC_POLARIZED)
-	  vsigma[2] += aux5;
-      }
-    }
-  }
-
-
-  /* t5 */
-  {
-    FLOAT aux1, aux2, aux3, aux4;
-
-    aux1 = AA*BB;
-    aux2 = rho[0]*rho[1]/(9.0*rhot);
-    aux3 = delta - 11.0;
-    aux4 = rho[0]*sigma[0] + rho[1]*sigma[1];
-
-    t5 = aux1*omega*aux2*aux3*aux4;
-
-    if(vrho != NULL){
-      for(is=0; is<p->nspin; is++){
-	int js = (is==0) ? 1 : 0;
-
-	vrho[is] += aux1*
-	  ((domegadr*aux2 + omega*(rho[js]*rhot - rho[0]*rho[1])/(9.0*rhot*rhot))*aux3*aux4 +
-	   omega*aux2*(ddeltadr*aux4 + aux3*sigma[is]));
-
-	vsigma[is==0 ? 0 : 2] += aux1*omega*aux2*aux3*rho[is]/sfact;
-      }
-    }
-  }
-
-  
-  /* t6 */
-  {
-    FLOAT aux1, aux2, aux3;
-
-    aux1 = -AA*BB;
-    aux2 = 2.0/3.0*rhot*rhot*(sigma[0] + sigma[1]);
-    aux3 = rho[0]*rho[0]*sigma[1] + rho[1]*rho[1]*sigma[0];
-
-    t6 = aux1*omega*(aux2 - aux3);
-
-    if(vrho != NULL){
-      for(is=0; is<p->nspin; is++){
-	int js = (is==0) ? 1 : 0;
-
-	vrho[is] += aux1*
-	  (domegadr*(aux2 - aux3) + 
-	   omega*(4.0/3.0*rhot*(sigma[0] + sigma[1]) - 2*rho[is]*sigma[js]));
-
-	vsigma[is==0 ? 0 : 2] += aux1*omega*(2.0/3.0*rhot*rhot - rho[js]*rho[js])/sfact;
-      }
-    }
-  }
-
-  /* we add all contributions to the total energy */
-  *e = (t1 + t2 + t3 + t4 + t5 + t6)/rhot;
+  /*
+  *d2fdrs2    = -2.0*(1.0 + d*cnst_rs*xt72)*(*dfdrs)/den;
+  *d2fdrsz    = 0.0;
+  *d2fdrsxt   = -7.0*d*cnst_rs*rs*xt52*(*dfdrs)/den - 7.0/2.0*d*cnst_rs*xt52*(*f)/den
+    - 2.0*b*xt*exp(-k*xt2)*(1.0 - k*xt2)*(1.0 + d*cnst_rs*xt72)/(den*den);
+  d2fdrsxs[0] = 0.0;
+  d2fdrsxs[1] = 0.0;
+  *d2fdz2     = 0.0;
+  *d2fdzxt    = 0.0;
+  d2fdzxs[0]  = 0.0;
+  d2fdzxs[1]  = 0.0;
+  *d2fdxt2    = (49.0/2.0*d*cnst_rs*rs*xt2*xt2/den - 35.0/4.0*xt0)*(*f)*d*cnst_rs*rs*xt/den
+    + (-7.0*d*cnst_rs*rs*xt72*(1.0 - k*xt2)/den + 1.0 - 5.0*k*xt2 + 2.0*k*k*xt2*xt2)*2.0*b*exp(-k*xt2)/den;
+  d2fdxtxs[0] = 0.0;
+  d2fdxtxs[1] = 0.0;
+  d2fdxs2[0]  = 0.0;
+  d2fdxs2[1]  = 0.0;
+  d2fdxs2[2]  = 0.0;
+  */
 }
 
-static void
-gga_c_lyp(const void *p_, int np, const FLOAT *rho, const FLOAT *sigma,
-	  FLOAT *zk, FLOAT *vrho, FLOAT *vsigma,
-	  FLOAT *v2rho2, FLOAT *v2rhosigma, FLOAT *v2sigma2)
-{
-  int ip;
-  const XC(gga_type) *p = p_;
-
-  for(ip=0; ip<np; ip++){
-    my_gga_c_lyp(p_, rho, sigma, zk, vrho, vsigma);
-
-    /* increment pointers */
-    rho   += p->n_rho;
-    sigma += p->n_sigma;
-    
-    if(zk != NULL)
-      zk += p->n_zk;
-    
-    if(vrho != NULL){
-      vrho   += p->n_vrho;
-      vsigma += p->n_vsigma;
-    }
-
-    if(v2rho2 != NULL){
-      v2rho2     += p->n_v2rho2;
-      v2rhosigma += p->n_v2rhosigma;
-      v2sigma2   += p->n_v2sigma2;
-    }
-  }
-}
+#include "work_gga_c.c"
 
 const XC(func_info_type) XC(func_info_gga_c_lyp) = {
   XC_GGA_C_LYP,
   XC_CORRELATION,
-  "Lee, Yang & Parr",
+  "Lee, Yang & Parr 2",
   XC_FAMILY_GGA,
   "C Lee, W Yang and RG Parr, Phys. Rev. B 37, 785 (1988)\n"
   "B Miehlich, A Savin, H Stoll and H Preuss, Chem. Phys. Lett. 157, 200 (1989)",
-  XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC,
+  XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC, // | XC_FLAGS_HAVE_FXC,
   XC(gga_c_lyp_init), 
   NULL,
   NULL,
-  gga_c_lyp
+  work_gga_c
 };

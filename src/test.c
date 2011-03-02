fc76bf1f05f60a6a5c79487a2c33318884c42ec9
Miguel Marques <marques@tddft.org>
2011-03-02 12:42:51 +0000

    I rewrote lyp to the new framework. Now the file is half the size as before, and it will be easy to get the second derivatives.

--- src/test.c
+++ src/test.c
@@ -46,7 +46,7 @@ void test_lda()
   XC(func_type) l1, l2, l3;
   int i;
   
-  XC(func_init)(&l1, XC_LDA_C_1D_CSC, XC_UNPOLARIZED);
+  XC(func_init)(&l1, XC_LDA_C_GOMBAS, XC_UNPOLARIZED);
   XC(func_init)(&l2, XC_LDA_C_PW, XC_POLARIZED);
   XC(func_init)(&l3, XC_LDA_X, XC_UNPOLARIZED);
 
@@ -76,7 +76,7 @@ void test_lda()
 
     //dens = rho[0] + rho[1];
 
-    XC(lda)(&l1, 1, rho, &ec1, vc1, NULL, NULL);
+    XC(lda)(&l1, 1, rho, &ec1, vc1, fxc1, kxc1);
     XC(lda)(&l2, 1, rho, &ec2, vc2, NULL, NULL);
     //XC(lda_fxc_fd)(&l2, rho, fxc2);
     //XC(lda_kxc_fd)(&l2, rho, kxc2);
@@ -87,9 +87,37 @@ void test_lda()
     // printf("%e\t%e\t%e\n", dens, (fxc1[0]+2.0*fxc1[1]+fxc1[2])/4.0, fxc3[0]);
     // printf("%e\t%e\t%e\n", dens, (kxc1[0]+3.0*kxc1[1]+3.0*kxc1[2]+kxc1[3])/8.0, kxc3[0]);
 
-    printf("%e\t%e\t%e\n", rho[0], rho[0]*ec1, vc1[0]);
+    printf("%e\t%e\t%e\n", rho[0], fxc1[0], kxc1[0]);
   }
 }
+
+void test_gga()
+{
+  XC(func_type) gga, gga2;
+  int i;
+
+  XC(func_init)(&gga, XC_GGA_C_LYP, XC_POLARIZED);
+  XC(func_init)(&gga2, XC_GGA_C_LYP2, XC_POLARIZED);
+  
+  for(i=0; i<=1000; i++){
+    double rho[2], sigma[3], tau[2], lapl[2];
+    double zk,   vrho[2],  vsigma[3];
+    double zk2, vrho2[2], vsigma2[3];
+    double v2rho2[3], v2sigma2[6], v2rhosigma[6];
+
+    rho[0]   = 0.1 + i/1000.0;
+    rho[1]   = 0.5;
+    sigma[0] = 0.1;
+    sigma[1] = 0.11;
+    sigma[2] = 0.7;
+
+    XC(gga)(&gga,  1, rho, sigma, &zk, vrho, vsigma, NULL, v2rhosigma, v2sigma2);
+    XC(gga)(&gga2, 1, rho, sigma, &zk2, vrho2, vsigma2, NULL, v2rhosigma, v2sigma2);
+
+    fprintf(stderr, "%16.10lf\t%16.10lf\t%16.10lf\n", rho[0], vsigma[0], vsigma2[0]);
+  }
+}
+
 
 //void 
 //XC(mgga_x_tpss_lara)(XC(mgga_type) *p, FLOAT *rho, FLOAT *sigma, FLOAT *tau,
@@ -137,11 +165,65 @@ void test_tpss()
   }
 }
 
+void test_neg_rho()
+{
+  xc_func_type func;
+  double rho[5][2] = { {9.03897273e-06,-1.00463992e-06}, 
+		       {8.48383564e-06,-3.51231267e-07}, 
+		       {1.45740621e-08,-2.94546705e-09}, 
+		       {2.62778445e-07, -1.00191745e-07}, 
+		       {2.55745103e-06, -1.54789964e-06} };
+  double sigma[5][3] = { {1.20122271e-08,4.83240746e-09,6.24774836e-09}, 
+			 {1.54146602e-07,1.41584609e-07,1.36663204e-07}, 
+			 {2.75312438e-08,2.75224049e-08,2.75135719e-08}, 
+			 {1.90251649e-07,1.91241798e-07,1.92240989e-07}, 
+			 {9.29562712e-09,7.83940082e-09, 8.05714636e-09} };
+  double vsigma[5][3];
+  double zk[5], vk[5][2];
+  int i, func_id;
+
+  for(func_id=1; func_id<1000; func_id++){
+    if(xc_func_init(&func, func_id, XC_POLARIZED) != 0) continue;
+    if(func_id == XC_LDA_C_2D_PRM || func_id == XC_GGA_XC_LB) goto end;
+
+    printf("\n%s:\n", func.info->name);
+
+    switch(func.info->family){
+    case XC_FAMILY_LDA:
+      xc_lda_exc_vxc(&func, 5, &rho[0][0], zk, &vk[0][0]);
+      break;
+    case XC_FAMILY_GGA:
+      xc_gga_exc_vxc(&func, 5, &rho[0][0], &sigma[0][0], zk, &vk[0][0], &vsigma[0][0]);
+      break;
+    }
+
+    switch(func.info->family){
+    case XC_FAMILY_LDA:
+      for(i=0; i<5; i+=1)
+	printf("%.8e %.8e %.8e %.8e %.8e\n", 
+	       rho[i][0], rho[i][1], zk[i], vk[i][0], vk[i][1]);
+      break;
+    case XC_FAMILY_GGA:
+      for(i=0; i<5; i+=1)
+	printf("%.8e %.8e %.8e %.8e %.8e %.8e %.8e %.8e %.8e %.8e %.8e\n", 
+	       rho[i][0], rho[i][1], sigma[i][0], sigma[i][1], sigma[i][2], 
+	       zk[i], vk[i][0], vk[i][1], vsigma[i][0], vsigma[i][1], vsigma[i][2]);
+      break;
+    }
+
+  end:
+    xc_func_end(&func);
+  }
+}
+
+
 int main()
 {
-  test_tpss();
   //test_integration();
+  //test_neg_rho();
+
   //test_lda();
+  test_gga();
 
   return 0;
 }

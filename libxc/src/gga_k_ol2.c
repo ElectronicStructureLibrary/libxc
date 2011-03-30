8342e1d2be33d58616600925bbb7850c23945319
Miguel Marques <marques@tddft.org>
2011-03-30 12:17:39 +0000

    Bug fix in K_OL2. The formula was not correct. Added the functional X_OL2, but I have very strong reasons to believe that the formula in the paper is wrong.

--- libxc/src/gga_k_ol2.c
+++ libxc/src/gga_k_ol2.c
@@ -22,29 +22,57 @@
 #include "util.h"
 
 #define XC_GGA_K_OL2          513 /* Ou-Yang and Levy v.2 */
+#define XC_GGA_X_OL2          183 /* Exchange form based on Ou-Yang and Levy v.2 */
+
+static void 
+gga_k_ol2_init(void *p_)
+{
+  XC(gga_type) *p = (XC(gga_type) *)p_;
+
+  switch(p->info->number){
+  case XC_GGA_K_OL2: p->func = 0; break;
+  case XC_GGA_X_OL2: p->func = 1; break;
+  }
+}
 
 static inline void 
 func(const XC(gga_type) *p, int order, FLOAT x, 
      FLOAT *f, FLOAT *dfdx, FLOAT *ldfdx, FLOAT *d2fdx2)
 {
-  const FLOAT c4 = 0.00887;
-  FLOAT ss, ss2, denom;
+  static const FLOAT aa[2] = {    1.0,            M_CBRT2*0.07064/X_FACTOR_C};
+  static const FLOAT bb[2] = {    1.0/K_FACTOR_C, M_CBRT2*0.07064/X_FACTOR_C};
+  static const FLOAT cc[2] = {0.00887/K_FACTOR_C, M_CBRT2*M_CBRT2*0.07064*34.0135/X_FACTOR_C};
+  FLOAT x2, denom;
 
-  ss    = x/M_CBRT2;
-  ss2   = ss*ss;
-  denom = 1.0 + 4.0*ss;
+  x2   = x*x;
+  denom = M_CBRT2 + 4.0*x;
 
-  *f = 1.0 + (ss2/72.0 + c4*ss/denom)/K_FACTOR_C;
+  *f = aa[p->func] + bb[p->func]*x*x/72.0 + cc[p->func]*x/denom;
 
   if(order < 1) return;
 
-  *dfdx = (2.0*ss/72.0 + c4/(denom*denom))/(K_FACTOR_C*M_CBRT2);
-  *ldfdx= 1.0/(72.0*K_FACTOR_C*M_CBRT2*M_CBRT2);
+  *dfdx = 2.0*bb[p->func]*x/72.0 + cc[p->func]*M_CBRT2/(denom*denom);
+  *ldfdx= bb[p->func]/72.0;
   
   if(order < 2) return;
 
-  *d2fdx2 = (2.0/72.0 - 8.0*c4/(denom*denom*denom))/(K_FACTOR_C*M_CBRT2*M_CBRT2);
+  *d2fdx2 = 2.0*bb[p->func]/72.0 - 8.0*cc[p->func]*M_CBRT2/(denom*denom*denom);
 }
+
+#include "work_gga_x.c"
+const XC(func_info_type) XC(func_info_gga_x_ol2) = {
+  XC_GGA_X_OL2,
+  XC_EXCHANGE,
+  "Exchange form based on Ou-Yang and Levy v.2",
+  XC_FAMILY_GGA,
+  "P Fuentealba and O Reyes, Chem. Phys. Lett. 232, 31-34 (1995)\n"
+  "H Ou-Yang, M Levy, Int. J. of Quant. Chem. 40, 379–388 (1991)",
+  XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC,
+  gga_k_ol2_init,
+  NULL, NULL,
+  work_gga_x
+};
+
 
 #define XC_KINETIC_FUNCTIONAL
 #include "work_gga_x.c"
@@ -56,6 +84,7 @@ const XC(func_info_type) XC(func_info_gga_k_ol2) = {
   XC_FAMILY_GGA,
   "H Ou-Yang, M Levy, Int. J. of Quant. Chem. 40, 379–388 (1991)",
   XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC,
-  NULL, NULL, NULL,
+  gga_k_ol2_init, 
+  NULL, NULL,
   work_gga_k
 };

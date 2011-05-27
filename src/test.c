1a9d4bbda87e5698cf4c7cf0b05d61e77c4636c6
Miguel Marques <marques@tddft.org>
2011-05-27 15:28:23 +0000

    Sopke too soon ;) v2sigma2 is fine, but v2rhosigma still has a bug for some spins...

--- src/test.c
+++ src/test.c
@@ -96,8 +96,8 @@ void test_gga()
   XC(func_type) gga, gga2;
   int i;
 
-  XC(func_init)(&gga, XC_GGA_C_AM05, XC_POLARIZED);
-  //XC(func_init)(&gga, XC_GGA_C_WL, XC_POLARIZED);
+  //XC(func_init)(&gga, XC_GGA_C_AM05, XC_POLARIZED);
+  XC(func_init)(&gga, XC_GGA_C_WL, XC_POLARIZED);
   //XC(func_init)(&gga2, XC_GGA_C_AM052, XC_UNPOLARIZED);
   
   for(i=0; i<=10000; i++){
@@ -107,16 +107,16 @@ void test_gga()
     double v2rho2[3], v2sigma2[6], v2rhosigma[6];
 
     rho[0]   = 0.1;
-    rho[1]   = 0.5;
+    rho[1]   = 0.5 + i/10000.0;
     sigma[0] = 0.1;
     sigma[1] = 0.11;
-    sigma[2] = 0.7 + i/10000.0;
+    sigma[2] = 0.7;
 
     XC(gga)(&gga,  1, rho, sigma, &zk, vrho, vsigma, v2rho2, v2rhosigma, v2sigma2);
     // XC(gga)(&gga2, 1, rho, sigma, &zk2, vrho2, vsigma2, v2rho2, v2rhosigma, v2sigma2);
     
     //fprintf(stderr, "%16.10lf\t%16.10lf\t%16.10lf\n", rho[0], (rho[0]+rho[1])*zk, vrho[0]);
-    fprintf(stderr, "%16.10lf\t%16.10lf\t%16.10lf\n", sigma[2], vsigma[0], v2sigma2[2]);
+    fprintf(stderr, "%16.10lf\t%16.10lf\t%16.10lf\n", rho[1], vsigma[2], v2rhosigma[5]);
     //fprintf(stderr, "%16.10lf\t%16.10lf\t%16.10lf\n", rho[1], vrho[1], v2rho2[2]);
   }
 }

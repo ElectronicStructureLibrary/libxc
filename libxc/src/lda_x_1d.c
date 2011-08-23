803ac174a89b7493c52947d48d9de2b7bcc8931f
David Strubbe <dstrubbe@ucmerced.edu>
2011-08-23 18:55:52 +0000

    Tobias Burnus' patch to libxc references.

--- libxc/src/lda_x_1d.c
+++ libxc/src/lda_x_1d.c
@@ -166,7 +166,7 @@ const XC(func_info_type) XC(func_info_lda_x_1d) = {
   XC_EXCHANGE,
   "Exchange in 1D",
   XC_FAMILY_LDA,
-  "Unpublished",
+  "N. Helbig, J. I. Fuks, M. Casula, M. J. Verstraete, M. A. L. Marques, I. V. Tokatly and A. Rubio, Phys. Rev. A 83, 032503 (2011)",
   XC_FLAGS_1D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC,
   lda_x_1d_init,    /* init */
   NULL,             /* end  */

1c29f8d590ae468e358d25a264e21a56cdc8da4d
Miguel Marques <marques@tddft.org>
2012-11-15 08:43:08 +0000

    Missing declaration. Thanks David.

--- libxc/src/util.h
+++ libxc/src/util.h
@@ -139,9 +139,10 @@ void XC(lda_kxc_fd)(const XC(func_type) *p, int np, const FLOAT *rho, FLOAT *kxc
 void XC(lda_x_attenuation_function)(int interaction, int order, FLOAT aa, FLOAT *f, FLOAT *df, FLOAT *d2f, FLOAT *d3f);
 
 /* direct access to the internal functions */
-void XC(lda_c_hl_func)(const XC(func_type) *p, XC(lda_rs_zeta) *r);
-void XC(lda_c_pw_func)(const XC(func_type) *p, XC(lda_rs_zeta) *r);
-void XC(lda_c_pz_func)(const XC(func_type) *p, XC(lda_rs_zeta) *r);
+void XC(lda_c_hl_func)  (const XC(func_type) *p, XC(lda_rs_zeta) *r);
+void XC(lda_c_pw_func)  (const XC(func_type) *p, XC(lda_rs_zeta) *r);
+void XC(lda_c_pz_func)  (const XC(func_type) *p, XC(lda_rs_zeta) *r);
+void XC(lda_c_rc04_func)(const XC(func_type) *p, XC(lda_rs_zeta) *r);
 
 /* GGAs */
 void work_gga_becke_init(XC(func_type) *p);

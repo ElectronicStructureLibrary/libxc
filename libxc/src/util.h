f86bee9411e9c362d5733be14073ec48449b80a1
Miguel Marques <marques@tddft.org>
2012-11-15 15:07:06 +0000

    Reorganize the M05/06 echange functionals and added the exchange part of m06, m06-hf, and m06-2x

--- libxc/src/util.h
+++ libxc/src/util.h
@@ -186,6 +186,7 @@ typedef struct XC(work_mgga_x_params) {
   FLOAT d2fdrsx, d2fdrst, d2fdrsu, d2fdxt, d2fdxu, d2fdtu;
 } XC(work_mgga_x_params);
 
+void XC(mgga_series_w)(int order, int n, const FLOAT *a, FLOAT t, FLOAT *fw, FLOAT *dfwdt);
 
 /* direct access to the internal functions */
 void XC(mgga_x_gvt4_func)(int order, FLOAT x, FLOAT z, FLOAT alpha, const FLOAT *d, 

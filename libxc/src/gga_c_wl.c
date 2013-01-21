561bb49951a1f3116eb1acf22b4f2cb991b95463
Miguel Marques <marques@tddft.org>
2013-01-21 10:23:45 +0000

    No more NaN in gga_c_wl

--- libxc/src/gga_c_wl.c
+++ libxc/src/gga_c_wl.c
@@ -31,37 +31,52 @@ func(const XC(func_type) *p, XC(gga_work_c_t) *r)
 
   FLOAT aux, num, den;
 
-  aux = SQRT(1.0 - r->zeta*r->zeta);
-  num = a + b*r->xt;
-  den = c + d*(r->xs[0] + r->xs[1]) + r->rs;
+  if(1.0 - ABS(r->zeta) >= p->info->min_zeta){
+    aux = SQRT(1.0 - r->zeta*r->zeta);
+    num = a + b*r->xt;
+    den = c + d*(r->xs[0] + r->xs[1]) + r->rs;
 
-  r->f  = aux * num/den;
+    r->f  = aux * num/den;
+  }else
+    r->f = 0.0;
 
   if(r->order < 1) return;
 
-  r->dfdrs    = -r->f/den;
-  r->dfdz     = -r->zeta/aux * num/den;
-  r->dfdxt    = b*aux/den;
-  r->dfdxs[0] = -d*r->f/den;
-  r->dfdxs[1] = r->dfdxs[0];
+  if(1.0 - ABS(r->zeta) >= p->info->min_zeta){
+    r->dfdrs    = -r->f/den;
+    r->dfdz     = -r->zeta/aux * num/den;
+    r->dfdxt    = b*aux/den;
+    r->dfdxs[0] = -d*r->f/den;
+    r->dfdxs[1] = r->dfdxs[0];
+  }else{
+    r->dfdrs = r->dfdz = r->dfdxt = 0.0;
+    r->dfdxs[0] = r->dfdxs[1] = 0.0;
+  }
 
   if(r->order < 2) return;
 
-  r->d2fdrs2     = -2.0*(r->dfdrs)/den;
-  r->d2fdrsz     = r->zeta/aux * num/(den*den);
-  r->d2fdrsxt    = -b*aux/(den*den);
-  r->d2fdrsxs[0] = -2.0*d*(r->dfdrs)/den;
-  r->d2fdrsxs[1] = r->d2fdrsxs[0];
-  r->d2fdz2      = -num/(den*aux*aux*aux);
-  r->d2fdzxt     = -r->zeta/aux * b/den;
-  r->d2fdzxs[0]  = r->zeta/aux * d*num/(den*den);
-  r->d2fdzxs[1]  = r->d2fdzxs[0];
-  r->d2fdxt2     = 0.0;
-  r->d2fdxtxs[0] = -b*d*aux/(den*den);
-  r->d2fdxtxs[1] = r->d2fdxtxs[0];
-  r->d2fdxs2[0]  = -2.0*d*r->dfdxs[0]/den;
-  r->d2fdxs2[1]  = r->d2fdxs2[0];
-  r->d2fdxs2[2]  = r->d2fdxs2[0];
+  if(1.0 - ABS(r->zeta) >= p->info->min_zeta){
+    r->d2fdrs2     = -2.0*(r->dfdrs)/den;
+    r->d2fdrsz     = r->zeta/aux * num/(den*den);
+    r->d2fdrsxt    = -b*aux/(den*den);
+    r->d2fdrsxs[0] = -2.0*d*(r->dfdrs)/den;
+    r->d2fdrsxs[1] = r->d2fdrsxs[0];
+    r->d2fdz2      = -num/(den*aux*aux*aux);
+    r->d2fdzxt     = -r->zeta/aux * b/den;
+    r->d2fdzxs[0]  = r->zeta/aux * d*num/(den*den);
+    r->d2fdzxs[1]  = r->d2fdzxs[0];
+    r->d2fdxt2     = 0.0;
+    r->d2fdxtxs[0] = -b*d*aux/(den*den);
+    r->d2fdxtxs[1] = r->d2fdxtxs[0];
+    r->d2fdxs2[0]  = -2.0*d*r->dfdxs[0]/den;
+    r->d2fdxs2[1]  = r->d2fdxs2[0];
+    r->d2fdxs2[2]  = r->d2fdxs2[0];
+  }else{
+    r->d2fdrs2 = r->d2fdrsz = r->d2fdrsxt = r->d2fdrsxs[0] = r->d2fdrsxs[1] = 0.0;
+    r->d2fdz2 = r->d2fdzxt = r->d2fdzxs[0] = r->d2fdzxs[1] = 0.0;
+    r->d2fdxt2 = r->d2fdxtxs[0] = r->d2fdxtxs[1] = 0.0;
+    r->d2fdxs2[0] = r->d2fdxs2[1] = r->d2fdxs2[2] = 0.0;
+  }
 }
 
 #include "work_gga_c.c"

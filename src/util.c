5dac9b069f89df3f09e22379e2570bb7ac49c492
Miguel Marques <marques@tddft.org>
2013-07-01 13:20:43 +0000

    Making sure we never divide by zero. Reported by Vittorio Zecca and David.

--- src/util.c
+++ src/util.c
@@ -32,10 +32,15 @@ XC(rho2dzeta)(int nspin, const FLOAT *rho, FLOAT *d, FLOAT *zeta)
     *d    = max(rho[0], 0.0);
     *zeta = 0.0;
   }else{
-    *d    = max(rho[0] + rho[1], 0.0);
-    *zeta = (rho[0] - rho[1])/(*d);
-    *zeta = min(*zeta,  1.0);
-    *zeta = max(*zeta, -1.0);
+    *d = rho[0] + rho[1];
+    if(*d > 0.0){
+      *zeta = (rho[0] - rho[1])/(*d);
+      *zeta = min(*zeta,  1.0);
+      *zeta = max(*zeta, -1.0);
+    }else{
+      *d    = 0.0;
+      *zeta = 0.0;
+    }
   }
 }
 

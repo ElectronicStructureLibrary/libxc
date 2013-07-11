da263fd5524d0477cbd8d512190fcaaab4424cd1
Micael Oliveira <micael.oliveira@mpsd.mpg.de>
2013-07-11 15:28:46 +0000

    Corrected the FSF address in the copyright.

--- libxc/src/libxc_master.F90
+++ libxc/src/libxc_master.F90
@@ -12,8 +12,8 @@
 !!
 !! You should have received a copy of the GNU Lesser General Public License
 !! along with this program; if not, write to the Free Software
-!! Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-!! 02111-1307, USA.
+!! Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+!! 02110-1301, USA.
 !!
 !! $Id: libxc.f90 3550 2007-11-19 14:32:49Z marques $
 

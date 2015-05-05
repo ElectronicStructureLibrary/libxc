a368e0c44e1463a79eff80dd44a215d93f7ab5e6
Susi Lehtola <susi.lehtola@alumni.helsinki.fi>
2015-05-05 00:43:03 +0000

    Increase density threshold for SOGGA11 correlation, since I caught NaNs in a calculation.

--- src/gga_c_sogga11.c
+++ src/gga_c_sogga11.c
@@ -183,7 +183,7 @@ const XC(func_info_type) XC(func_info_gga_c_sogga11) = {
   XC_FAMILY_GGA,
   {&xc_ref_Peverati2011_1991, NULL, NULL, NULL, NULL},
   XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC,
-  1e-27, 1e-32, 0.0, 1e-32,
+  1e-26, 1e-32, 0.0, 1e-32,
   gga_c_sogga11_init,
   NULL, NULL,
   work_gga_c,

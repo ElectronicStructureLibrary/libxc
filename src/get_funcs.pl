0ef682c64d711bf435e7b64b5e8435e5f7a6c119
David Strubbe <dstrubbe@ucmerced.edu>
2015-05-13 18:39:01 +0000

    * Better error-handling in get_funcs.pl. * An attempt at generating some documentation, via get_funcs.pl. I had this lying around for a little while, but since Susi just committed an effort in the same direction, I think I had better put mine into svn so we can see what the best way to proceed is.

--- src/get_funcs.pl
+++ src/get_funcs.pl
@@ -16,15 +16,23 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+if(@ARGV < 2) {
+    print STDERR "Usage: get_funcs.pl srcdir builddir\n";
+    exit(1);
+}
+
 $srcdir = shift;
 $builddir = shift;
 
 my @funcs = ("lda", "gga", "hyb_gga", "mgga", "hyb_mgga");
 
+open(DOCS, ">$builddir/libxc_docs.txt") or die("Could not open '$builddir/libxc_docs.txt.'\n");
+
 $s0 = ""; $s3 = ""; $s4 = ""; $s5 = "";
 foreach $func (@funcs){
   undef %deflist_f;
   undef %deflist_c;
+  undef %num;
 
   read_file($srcdir, $func);
 
@@ -46,7 +54,7 @@ foreach $func (@funcs){
     $s2 .= "  &XC(func_info_$t),\n";
   }
 
-  open(OUT, ">$builddir/funcs_$func.c");
+  open(OUT, ">$builddir/funcs_$func.c") or die("Could not open '$builddir/funcs_$func.c'.\n");
   print OUT <<EOF
 #include "util.h"
 
@@ -60,7 +68,9 @@ EOF
   close OUT;
 }
 
-  open(OUT, ">$builddir/funcs_key.c");
+close DOCS;
+
+open(OUT, ">$builddir/funcs_key.c") or die("Could not open '$builddir/funcs_key.c'.\n");
 print OUT <<EOF
 #include "util.h"
 
@@ -69,14 +79,14 @@ $s4,
 {"", -1}
 };
 EOF
-  ;
+;
 
-open(OUT, ">$builddir/xc_funcs.h");
+open(OUT, ">$builddir/xc_funcs.h") or die("Could not open '$builddir/xc_funcs.h'.\n");
 print OUT $s0;
 print $so;
 close OUT;
 
-open(OUT, ">$builddir/libxc_funcs.f90");
+open(OUT, ">$builddir/libxc_funcs.f90") or die("Could not open '$builddir/libxc_funcs.f90'.\n");
 print OUT <<EOF
 module libxc_funcs_m
   implicit none
@@ -97,17 +107,121 @@ sub read_file() {
   $TYPE =~ s/(.*)/\U$1/;
 
   # we remove the hyb from the filenames
+  $save_type = $type;
   $type =~ s/^hyb_//;
 
   opendir(DIR, "$dir/") || die "cannot opendir '$dir': $!";
   while($_ = readdir(DIR)){
     next if(!/^${type}_.*\.c$/ && !/^hyb_${type}_.*\.c$/ );
 
-    open(IN, "<$dir/$_");
+    $file=$_;
+    open(IN, "<$dir/$_") or die("Could not open '$dir/$_'.\n");
     while($_=<IN>){
       if(/#define\s+(XC_${TYPE}_\S+)\s+(\S+)\s+\/\*(.*)\*\//){
 	$deflist_f{$2} = $1;
 	$deflist_c{$2} = $3;
+	$num{$1} = $2;
+      }
+
+      if(/^(const |)XC\(func_info_type\) XC\(func_info_${save_type}/){
+	  $infostr = "";
+	  while($_=<IN>){
+	      if(/([^}])*};/) { 
+		  $infostr .= $1;
+		  last;
+	      }
+	      # remove C comments
+	      $_ =~ s|/\*[^\*]*\*/||;
+	      # remove braces
+	      $_ =~ s/[{}]//g;
+	      chomp($_);
+	      $infostr .= $_;
+	  }
+	  @infos = split('"', $infostr);
+	  @infos0 = split(',', $infos[0]);
+	  $infos0[0] =~ s/^\s*//;
+	  $infos0[1] =~ s/^\s*//;
+	  @infos2 = split(',', $infos[2]);
+
+	  for($ii = 0; $ii <= $#infos2; $ii++) {
+	      # remove leading spaces
+	      $infos2[$ii] =~ s/^\s*//;
+	  }
+
+	  print DOCS "Number         : $num{$infos0[0]}\n";
+	  print DOCS "File           : $file\n";
+	  print DOCS "Codename       : $infos0[0]\n";
+	  print DOCS "Kind           : $infos0[1]\n";
+	  print DOCS "Description 1  : $infos[1]\n";
+	  $deflist_c{$num{$infos0[0]}} =~ s/^\s*//;
+	  $deflist_c{$num{$infos0[0]}} =~ s/\s*$//;
+	  if($deflist_c{$num{$infos0[0]}} ne $infos[1]) {
+	      print DOCS "Description 2  : $deflist_c{$num{$infos0[0]}}\n";
+	  }
+	  #infos2[0] will be blank
+	  print DOCS "Family         : $infos2[1]\n";
+
+	  print DOCS "References     : ";
+
+	  if(-e "./xc-info" && -x "./xc-info") {
+	      $xc_info = `./xc-info $num{$infos0[0]}`;
+	      @refs = split('\n', $xc_info);
+	      print DOCS $refs[5] . "\n";
+	      for($ii = 6; $ii <= $#refs; $ii++) {
+		  print DOCS "                 " . $refs[$ii] . "\n";
+	      }
+	  } else {
+	      # print only the names of the variables in references.c
+	      for($ii = 2; $ii <= 6; $ii++) {
+		  if($infos2[$ii] ne "NULL") {
+		      $infos2[$ii] =~ s/&xc_ref_//;
+		      print DOCS " $infos2[$ii]";
+		  }
+	      }
+	      print DOCS "\n";
+	  }
+
+	  if(($infos2[7] =~ /XC_FLAGS_(.)D/) != 1) {
+	      print STDERR "Must set exactly one dimensionality flag.\n";
+	      exit(1);
+	  }
+          print DOCS "Dimensionality : $1\n";
+	   
+	  print DOCS "Quantities     : ";
+	  @quantities = ($infos2[7] =~ /XC_FLAGS_HAVE_(.XC)/g);
+	  print DOCS join(" ", @quantities) . "\n";
+
+	  $infos2[7] =~ s/XC_FLAGS_.D//;
+	  $infos2[7] =~ s/XC_FLAGS_HAVE_.XC//g;
+	  $infos2[7] =~ s/\|//g;
+	  $infos2[7] =~ s/^\s*//;
+	  $infos2[7] =~ s/^s*$//;
+	  
+	  print DOCS "Other flags    : $infos2[7]\n";
+	  print DOCS "min dens       : $infos2[8]\n";
+	  print DOCS "min grad       : $infos2[9]\n";
+	  print DOCS "min tau        : $infos2[10]\n";
+	  print DOCS "min zeta       : $infos2[11]\n";
+	  print DOCS "init           : $infos2[12]\n";
+	  # apparently, the end is always NULL
+	  print DOCS "end            : $infos2[13]\n";
+	  print DOCS "work lda       : $infos2[14]\n";
+	  print DOCS "work gga       : $infos2[15]\n";
+	  print DOCS "work mgga      : $infos2[16]\n";
+	  print DOCS "----------------------------\n";
+
+#	  print "$file $infos0[0] $infos2[12]\n";
+
+	  if($num{$infos0[0]} eq "") {
+	      print STDERR "ERROR: missing number\n";
+	      exit(1);
+	  }
+
+	  if($deflist_f{$num{$infos0[0]}} ne $infos0[0]) {
+	      print STDERR $deflist_f{$num{$infos0[0]}} . " " . $infos0[0] . "\n";
+	      print STDERR "Mismatch of names.\n";
+	      exit(1);
+	  }
       }
     }
     close(IN);

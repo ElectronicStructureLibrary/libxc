be1fb9d87858b339ed0892d7c59700a44db6e3b0
Susi Lehtola <susi.lehtola@alumni.helsinki.fi>
2015-05-13 18:47:39 +0000

    Sort functionals in alphabetical order in the wiki generation.

--- src/genwiki.c
+++ src/genwiki.c
@@ -23,13 +23,33 @@
 #include <string.h>
 #include "xc.h"
 
-#define FUNCMAX 1000
+#define FUNCMAX 2000
+  
+int compare(const void *f1, const void *f2) {
+  /* Functional IDs are */
+  int i1=*(int *)f1;
+  int i2=*(int *)f2;
+
+  /* Get functional names */
+  char *n1=XC(functional_get_name)(i1);
+  char *n2=XC(functional_get_name)(i2);
+
+  int val=strcmp(n1,n2);
+
+  free(n1);
+  free(n2);
+
+  return val;
+}
+
+void sort_funcs(int * list, int nfunc) {
+  qsort(list,nfunc,sizeof(int),compare);
+}
 
 int main(void) {
-  int func_id, error, i;
+  int func_id, error, i, ii;
   xc_func_type func;
   char *fname;
-  int typeprint;
 
   /* Families to print out */
   const int families[]={XC_FAMILY_LDA, XC_FAMILY_GGA, XC_FAMILY_HYB_GGA, XC_FAMILY_MGGA, XC_FAMILY_HYB_MGGA};
@@ -42,55 +62,70 @@ int main(void) {
   const int Ntype=sizeof(types)/sizeof(types[0]);
   int itype;
 
+  /* List of functionals of current type and family */
+  int funclist[FUNCMAX];
+  int nfunc;
+
   /* Loop over families, and then types */
   for(ifam=0;ifam<Nfam;ifam++) {
     /* Print wiki section header */
     printf("\n== %s functionals ==\n",famleg[ifam]);
     for(itype=0;itype<Ntype;itype++) {
-      typeprint=0;
       
-      /* Loop over functional ids */
+      /* Collect functional ids */
+      nfunc=0;
       for(func_id=0;func_id<FUNCMAX;func_id++) {
 	/* Initialize functional */
 	error = xc_func_init(&func, func_id, XC_UNPOLARIZED);
 	if(error) {
 	  /* Functional does not exist */
-	  // printf("Functional %i doesn't exist\n",i);
 	  continue;
 	}
 	
 	/* Check family and type */
 	if(func.info->family != families[ifam] || func.info->kind != types[itype]) {
-	  //printf("Functional %i type doesn't match\n",i);
 	  XC(func_end)(&func);
 	  continue;
 	}
 
-	/* Get functional keyword */
-	fname = XC(functional_get_name)(func_id);
-	/* Convert functional keyword to upper case */
-	for(i=0; i<strlen(fname); i++)
-	  fname[i]=toupper(fname[i]);
-	
-	/* Print wiki section header */
-	if(!typeprint) {
-	  typeprint=1;
-	  printf("\n===== %s %s =====\n",famleg[ifam],typeleg[itype]);
-	}
-	
-	/* Print out info */
-	printf("* '''%s''': %s\n",fname,func.info->name);
-	for(i=0; i<5; i++){
-	  if(func.info->refs[i]==NULL) break;
-	  if(strlen(func.info->refs[i]->doi) > 0)
-	    printf("** [http://dx.doi.org/%s %s] (doi: %s)\n", func.info->refs[i]->doi, func.info->refs[i]->ref, func.info->refs[i]->doi);
-	  else
-	    printf("** %s\n", func.info->refs[i]->ref);
-	}
+	/* Add to list */
+	funclist[nfunc++]=func_id;
 	
 	/* Free memory */
 	XC(func_end)(&func);
-	free(fname);
+      }
+
+      /* Sort list alphabetically */
+      sort_funcs(funclist,nfunc);
+      
+      /* Print list */
+      if(nfunc) {
+	printf("\n===== %s %s =====\n",famleg[ifam],typeleg[itype]);
+	for(ii=0;ii<nfunc;ii++) {
+	  /* Func id is */
+	  func_id=funclist[ii];
+
+	  /* Initialize functional */
+	  error = xc_func_init(&func, func_id, XC_UNPOLARIZED);
+	  /* Get functional keyword */
+	  fname = XC(functional_get_name)(func_id);
+	  /* Convert functional keyword to upper case */
+	  for(i=0; i<strlen(fname); i++)
+	    fname[i]=toupper(fname[i]);
+	  
+	  /* Print out info */
+	  printf("* '''%s''': %s\n",fname,func.info->name);
+	  for(i=0; i<5; i++){
+	    if(func.info->refs[i]==NULL) break;
+	    if(strlen(func.info->refs[i]->doi) > 0)
+	      printf("** [http://dx.doi.org/%s %s] (doi: %s)\n", func.info->refs[i]->doi, func.info->refs[i]->ref, func.info->refs[i]->doi);
+	    else
+	      printf("** %s\n", func.info->refs[i]->ref);
+	  }
+	  
+	  XC(func_end)(&func);
+	  free(fname);
+	}
       }
     }
   }

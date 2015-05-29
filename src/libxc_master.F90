b6ebb07cf2d62e06f42e61e9130d70a14aa09790
Miguel Marques <marques@tddft.org>
2015-05-29 15:10:29 +0000

    The Fortran module files are now dual licensed (BSD and lGPL) so that commercial codes can include the Fortran module internally.

--- src/libxc_master.F90
+++ src/libxc_master.F90
@@ -1,4 +1,9 @@
-!! Copyright (C) 2003-2006 M. Marques, A. Castro, A. Rubio, G. Bertsch
+!! Copyright (C) 2003-2015 Miguel Marques
+!! All rights reserved.
+!!
+!! This file is dual-licensed under a GPL and a BSD license
+!!
+!! GPL License:
 !!
 !! This program is free software; you can redistribute it and/or modify
 !! it under the terms of the GNU Lesser General Public License as published by
@@ -15,6 +20,37 @@
 !! Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 !! 02110-1301, USA.
 !!
+!! BSD License:
+!!
+!! Redistribution and use in source and binary forms, with or without
+!! modification, are permitted provided that the following conditions
+!! are met:
+!!
+!! 1. Redistributions of source code must retain the above copyright
+!! notice, this list of conditions and the following disclaimer.
+!!
+!! 2. Redistributions in binary form must reproduce the above
+!! copyright notice, this list of conditions and the following
+!! disclaimer in the documentation and/or other materials provided
+!! with the distribution.
+!!
+!! 3. Neither the name of the copyright holder nor the names of its
+!! contributors may be used to endorse or promote products derived
+!! from this software without specific prior written permission.
+!!
+!! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+!! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+!! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+!! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+!! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+!! INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+!! (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+!! SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+!! HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+!! STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+!! ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+!! OF THE POSSIBILITY OF SUCH DAMAGE.
+
 !! $Id: libxc.f90 3550 2007-11-19 14:32:49Z marques $
 
 #ifdef SINGLE_PRECISION

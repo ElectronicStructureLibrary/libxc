600e645c88ec34995431349c9844bfa9a1a37ad4
Susi Lehtola <susi.lehtola@alumni.helsinki.fi>
2016-02-13 03:44:38 +0000

    Make xc-info print out exact exchange.

--- src/xc-info.c
+++ src/xc-info.c
@@ -106,6 +106,32 @@ int main(int argc, char **argv) {
   printf("%10s: %-20s\t%10s: %-25s\n","family", get_family(&func), "kind", get_kind(&func));
   printf("%10s: %s\n","comment", func.info->name);
 
+  /* Print out hybrid exchange info */
+  if(func.info->family==XC_FAMILY_HYB_GGA || func.info->family==XC_FAMILY_HYB_MGGA) {
+    /* Range separation? */
+    int rangesep=0;
+    if(func.info->flags & XC_FLAGS_HYB_CAM)
+      rangesep++;
+    if(func.info->flags & XC_FLAGS_HYB_CAMY)
+      rangesep++;
+    if(func.info->flags & XC_FLAGS_HYB_LC)
+      rangesep++;
+    if(func.info->flags & XC_FLAGS_HYB_LCY)
+      rangesep++;
+
+    if(rangesep) {
+      double alpha, beta, omega;
+      XC(hyb_cam_coef(&func,&omega,&alpha,&beta));
+      printf("\nThis is a range separated hybrid functional with range separation constant % .3f,\n",omega);
+      printf("and %4.1f%% short-range and %4.1f%% long-range exact exchange.\n",(alpha+beta)*100,(alpha)*100);
+    } else {
+      double alpha=XC(hyb_exx_coef(&func));
+      printf("\nThis is a global hybrid functional with %4.1f%% of exact exchange.\n",alpha*100);
+    }
+  } else {
+    printf("\nThis is a pure functional with no exact exchange.\n");
+  }
+  
   printf("\nReference(s):\n");
   for(i=0; i<5; i++){
     if(func.info->refs[i]==NULL) break;

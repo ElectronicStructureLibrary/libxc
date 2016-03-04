9d030764b111d2b9f3dad3a436a74cc187717319
Susi Lehtola <susi.lehtola@alumni.helsinki.fi>
2016-03-04 02:08:09 +0000

    Reintroduce VV10 flag in wB97X-V that was dropped in revision 12240.

--- src/gga_xc_b97.c
+++ src/gga_xc_b97.c
@@ -791,7 +791,7 @@ const XC(func_info_type) XC(func_info_hyb_gga_xc_wb97x_v) = {
   "wB97X range-separated functional",
   XC_FAMILY_HYB_GGA,
   {&xc_ref_Mardirossian2014_9904, NULL, NULL, NULL, NULL},
-  XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC | XC_FLAGS_HYB_CAM,
+  XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC | XC_FLAGS_HYB_CAM | XC_FLAGS_VV10,
   1e-23, 1e-32, 0.0, 1e-32,
   gga_xc_b97_init, 
   NULL,

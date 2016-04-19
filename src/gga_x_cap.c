a3f5575af4a362ec925f9d81c9e1744dbf6345c4
Susi Lehtola <susi.lehtola@alumni.helsinki.fi>
2016-04-19 22:21:08 +0000

    Whoops, forgot to adapt copy-pasted license boilerplate

--- src/gga_x_cap.c
+++ src/gga_x_cap.c
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2006-2007 M.A.L. Marques
+ Copyright (C) 2016 Susi Lehtola
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by

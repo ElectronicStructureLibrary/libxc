17c79c7346125f9d648adc16933555ddc596fe63
Miguel Marques <marques@tddft.org>
2016-05-25 11:45:48 +0000

    Added HYB_GGA_XC_PBE_MOL0, HYB_GGA_XC_PBE_SOL0, HYB_GGA_XC_PBEB0, HYB_GGA_XC_PBE_MOLB0

--- src/hyb_gga_xc_pbeh.c
+++ src/hyb_gga_xc_pbeh.c
@@ -21,9 +21,13 @@
 #include <assert.h>
 #include "util.h"
 
-#define XC_HYB_GGA_XC_PBEH    406 /* aka PBE0 or PBE1PBE */
-#define XC_HYB_GGA_XC_PBE0_13 456 /* PBE0-1/3            */
-#define XC_HYB_GGA_XC_HPBEINT 472 /* hPBEint             */
+#define XC_HYB_GGA_XC_PBEH      406 /* aka PBE0 or PBE1PBE */
+#define XC_HYB_GGA_XC_PBE0_13   456 /* PBE0-1/3            */
+#define XC_HYB_GGA_XC_HPBEINT   472 /* hPBEint             */
+#define XC_HYB_GGA_XC_PBE_MOL0  169 /* PBEmol0             */
+#define XC_HYB_GGA_XC_PBE_SOL0  171 /* PBEsol0             */
+#define XC_HYB_GGA_XC_PBEB0     172 /* PBEbeta0            */
+#define XC_HYB_GGA_XC_PBE_MOLB0 176 /* PBEmolbeta0         */
 
 static void
 hyb_gga_xc_pbeh_init(XC(func_type) *p)
@@ -78,6 +82,7 @@ const XC(func_info_type) XC(func_info_hyb_gga_xc_pbe0_13) = {
   NULL, NULL, NULL, NULL /* this is taken care of by the generic routine */
 };
 
+
 static void
 hyb_gga_xc_hpbeint_init(XC(func_type) *p)
 {
@@ -101,3 +106,97 @@ const XC(func_info_type) XC(func_info_hyb_gga_xc_hpbeint) = {
 };
 
 
+static void
+hyb_gga_xc_pbemol0_init(XC(func_type) *p)
+{
+  static int   funcs_id  [2] = {XC_GGA_X_PBE_MOL, XC_GGA_C_PBE_MOL};
+  static FLOAT funcs_coef[2] = {1.0 - 0.25, 1.0};
+
+  XC(mix_init)(p, 2, funcs_id, funcs_coef);
+  p->cam_alpha = 0.25;
+}
+
+const XC(func_info_type) XC(func_info_hyb_gga_xc_pbe_mol0) = {
+  XC_HYB_GGA_XC_PBE_MOL0,
+  XC_EXCHANGE_CORRELATION,
+  "PBEmol0",
+  XC_FAMILY_HYB_GGA,
+  {&xc_ref_delCampo2012_104108, NULL, NULL, NULL, NULL},
+  XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC,
+  1e-32, 1e-32, 0.0, 1e-32,
+  hyb_gga_xc_pbemol0_init,
+  NULL, NULL, NULL, NULL /* this is taken care of by the generic routine */
+};
+
+
+static void
+hyb_gga_xc_pbesol0_init(XC(func_type) *p)
+{
+  static int   funcs_id  [2] = {XC_GGA_X_PBE_SOL, XC_GGA_C_PBE_SOL};
+  static FLOAT funcs_coef[2] = {1.0 - 0.25, 1.0};
+
+  XC(mix_init)(p, 2, funcs_id, funcs_coef);
+  p->cam_alpha = 0.25;
+}
+
+const XC(func_info_type) XC(func_info_hyb_gga_xc_pbe_sol0) = {
+  XC_HYB_GGA_XC_PBE_SOL0,
+  XC_EXCHANGE_CORRELATION,
+  "PBEsol0",
+  XC_FAMILY_HYB_GGA,
+  {&xc_ref_delCampo2012_104108, NULL, NULL, NULL, NULL},
+  XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC,
+  1e-32, 1e-32, 0.0, 1e-32,
+  hyb_gga_xc_pbesol0_init,
+  NULL, NULL, NULL, NULL /* this is taken care of by the generic routine */
+};
+
+
+static void
+hyb_gga_xc_pbeb0_init(XC(func_type) *p)
+{
+  static int   funcs_id  [2] = {XC_GGA_X_PBE, XC_GGA_C_PBE};
+  static FLOAT funcs_coef[2] = {1.0 - 0.25, 1.0};
+
+  XC(mix_init)(p, 2, funcs_id, funcs_coef);
+  /* 0.050044 ~ 3/4 beta_PBE */
+  XC(gga_c_pbe_set_params)(p->func_aux[1], 0.050044);
+  p->cam_alpha = 0.25;
+}
+
+const XC(func_info_type) XC(func_info_hyb_gga_xc_pbeb0) = {
+  XC_HYB_GGA_XC_PBEB0,
+  XC_EXCHANGE_CORRELATION,
+  "PBEbeta0",
+  XC_FAMILY_HYB_GGA,
+  {&xc_ref_delCampo2012_104108, NULL, NULL, NULL, NULL},
+  XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC,
+  1e-32, 1e-32, 0.0, 1e-32,
+  hyb_gga_xc_pbeb0_init,
+  NULL, NULL, NULL, NULL /* this is taken care of by the generic routine */
+};
+
+
+static void
+hyb_gga_xc_pbemolb0_init(XC(func_type) *p)
+{
+  static int   funcs_id  [2] = {XC_GGA_X_PBE_MOL, XC_GGA_C_PBE_MOL};
+  static FLOAT funcs_coef[2] = {1.0 - 0.25, 1.0};
+
+  XC(mix_init)(p, 2, funcs_id, funcs_coef);
+  /* 0.06288 ~ 3/4 beta_PBEmol */
+  XC(gga_c_pbe_set_params)(p->func_aux[1], 0.06288);
+  p->cam_alpha = 0.25;
+}
+
+const XC(func_info_type) XC(func_info_hyb_gga_xc_pbe_molb0) = {
+  XC_HYB_GGA_XC_PBE_MOLB0,
+  XC_EXCHANGE_CORRELATION,
+  "PBEmolbeta0",
+  XC_FAMILY_HYB_GGA,
+  {&xc_ref_delCampo2012_104108, NULL, NULL, NULL, NULL},
+  XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC,
+  1e-32, 1e-32, 0.0, 1e-32,
+  hyb_gga_xc_pbemolb0_init,
+  NULL, NULL, NULL, NULL /* this is taken care of by the generic routine */
+};

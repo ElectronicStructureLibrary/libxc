720927ee2c049876bb256cb18d08896c4b7147b9
Miguel Marques <marques@tddft.org>
2016-06-17 09:13:48 +0000

    Added the hybrid version of the exchange of tau_hcth

--- src/mgga_x_tau_hcth.c
+++ src/mgga_x_tau_hcth.c
@@ -24,12 +24,16 @@
 
 #define XC_MGGA_X_TAU_HCTH        205 /* tau-HCTH from Boese and Handy */
 #define XC_MGGA_X_BMK             179 /* Boese-Martin for kinetics     */
+#define XC_HYB_MGGA_X_TAU_HCTH    228 /* Hybrid version of tau-HCTH    */
 
 const FLOAT tHCTH_cx_local [4] = {1.10734, -1.0534, 6.3491, -2.5531};
 const FLOAT tHCTH_cx_nlocal[4] = {0.00110, -0.3041, 6.9543, -0.7235};
 
 const FLOAT BMK_cx_local [4] = { 0.474302, 2.77701, -11.4230, 11.7167};
 const FLOAT BMK_cx_nlocal[4] = {-0.192212, 4.73936, -26.6188, 22.4891};
+
+const FLOAT hyb_tHCTH_cx_local [4] = { 0.86735,  0.3008, 1.2208,   0.1574};
+const FLOAT hyb_tHCTH_cx_nlocal[4] = {-0.00230, -0.2849, 5.4146, -10.909};
 
 typedef struct{
   const FLOAT *cx_local;
@@ -56,6 +60,11 @@ mgga_x_tau_hcth_init(XC(func_type) *p)
   case XC_MGGA_X_BMK:
     params->cx_local  = BMK_cx_local;
     params->cx_nlocal = BMK_cx_nlocal;
+    break;
+  case XC_HYB_MGGA_X_TAU_HCTH:
+    p->cam_alpha = 0.15;
+    params->cx_local  = hyb_tHCTH_cx_local;
+    params->cx_nlocal = hyb_tHCTH_cx_nlocal;
     break;
   default:
     fprintf(stderr, "Internal error in mgga_tau_hcth\n");
@@ -137,7 +146,7 @@ func(const XC(func_type) *pt, XC(mgga_work_x_t) *r)
 const XC(func_info_type) XC(func_info_mgga_x_tau_hcth) = {
   XC_MGGA_X_TAU_HCTH,
   XC_EXCHANGE,
-  "tau-HCTH",
+  "tau-HCTH from Boese and Handy",
   XC_FAMILY_MGGA,
   {&xc_ref_Boese2002_9559, NULL, NULL, NULL, NULL},
   XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC,
@@ -153,6 +162,19 @@ const XC(func_info_type) XC(func_info_mgga_x_bmk) = {
   "Boese-Martin for kinetics",
   XC_FAMILY_MGGA,
   {&xc_ref_Boese2004_3405, NULL, NULL, NULL, NULL},
+  XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC,
+  MIN_DENS, MIN_GRAD, MIN_TAU, MIN_ZETA,
+  mgga_x_tau_hcth_init, 
+  NULL, NULL, NULL,
+  work_mgga_x,
+};
+
+const XC(func_info_type) XC(func_info_hyb_mgga_x_tau_hcth) = {
+  XC_HYB_MGGA_X_TAU_HCTH,
+  XC_EXCHANGE,
+  "Hybrid version of tau-HCTH",
+  XC_FAMILY_HYB_MGGA,
+  {&xc_ref_Boese2002_9559, NULL, NULL, NULL, NULL},
   XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC,
   MIN_DENS, MIN_GRAD, MIN_TAU, MIN_ZETA,
   mgga_x_tau_hcth_init, 

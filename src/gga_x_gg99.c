cacdce642f659851d1da337575104202c03b81db
Miguel Marques <marques@tddft.org>
2016-09-12 10:45:43 +0000

    The derivative seems to be correct, so I delete the DEVELOPMENT flag fromn the functional

--- src/gga_x_gg99.c
+++ src/gga_x_gg99.c
@@ -143,7 +143,7 @@ const XC(func_info_type) XC(func_info_gga_x_gg99) = {
   "Gilbert and Gill 1999",
   XC_FAMILY_GGA,
   {&xc_ref_Gilbert1999_511, NULL, NULL, NULL, NULL},
-  XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_DEVELOPMENT,
+  XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC,
   1e-32, 1e-32, 0.0, 1e-32,
   0, NULL, NULL,
   NULL, NULL, 

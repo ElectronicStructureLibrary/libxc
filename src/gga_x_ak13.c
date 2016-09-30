36ec4a473980e11c7024874aada47d5d9e22b7d6
Miguel Marques <marques@tddft.org>
2016-09-30 09:44:27 +0000

    Added ak13 to mathematica distcheck now works again in my machine

--- src/gga_x_ak13.c
+++ src/gga_x_ak13.c
@@ -39,38 +39,9 @@ FLOAT XC(gga_ak13_get_asymptotic) (FLOAT homo)
 }
 
 
-void XC(gga_x_ak13_enhance) 
-  (const XC(func_type) *p, int order, FLOAT x, 
-   FLOAT *f, FLOAT *dfdx, FLOAT *d2fdx2, FLOAT *d3fdx3)
-{
-  FLOAT ss, den, f1, f2;
+#include "hand_written/gga_x_ak13.c"
+#include "math2c/gga_x_ak13.c"
 
-  ss  = X2S*x;
-
-  f1 = LOG(1.0 + ss);
-  f2 = LOG(1.0 + f1);
-
-  *f = 1.0 + B1*ss*f1 + B2*ss*f2;
-
-  if(order < 1) return;
-
-  den = (1.0 + ss)*(1.0 + f1);
-
-  *dfdx  = B1*f1 + ss*(B1 + B2 + B1*f1)/den + B2*f2;
-  *dfdx *= X2S;
-
-  if(order < 2) return;
-  
-  *d2fdx2  = (2.0*B2 + B1*(2.0 + ss) + (2.0 + ss)*f1*(2.0*B1 + B2 + B1*f1))/(den*den);
-  *d2fdx2 *= X2S*X2S;
-
-  if(order < 3) return;
-
-  *d3fdx3  = (B2*(ss - 6.0) - B1*(ss + 3.0) - f1*(3.0*B1*(ss + 3.0) + B2*(2.0*ss + 9.0) + (ss + 3.0)*f1*(3.0*B1 + B2 + B1*f1)))/(den*den*den);
-  *d3fdx3 *= X2S*X2S*X2S;
-}
-
-#define func XC(gga_x_ak13_enhance) 
 #include "work_gga_x.c"
 
 const XC(func_info_type) XC(func_info_gga_x_ak13) = {

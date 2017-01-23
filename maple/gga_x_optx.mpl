f6f598c4bf6bfe59333f45289432d107e1982eb7
Miguel Marques <marques@tddft.org>
2017-01-23 08:16:11 +0000

    Added missing files. sogga11 is also handled by maple

---
+++ maple/gga_x_optx.mpl
@@ -0,0 +1,9 @@
+(* type: work_gga_x *)
+(* prefix:
+  gga_x_optx_params *params;
+ 
+  assert(p->params != NULL);
+  params = (gga_x_optx_params * )(p->params);
+*)
+
+f := x-> params_a_a + params_a_b*(params_a_gamma*x^2/(1.0 + params_a_gamma*x^2))^2:
--- src/gga_x_sogga11.c
+++ src/gga_x_sogga11.c
@@ -24,15 +24,38 @@
 #define XC_GGA_X_SOGGA11        151 /* Second-order generalized gradient approximation 2011 */
 #define XC_HYB_GGA_X_SOGGA11_X  426 /* Hybrid based on SOGGA11 form */
 
+typedef struct{
+  FLOAT kappa, mu, a[6], b[6];
+} gga_x_sogga11_params;
+
+static const gga_x_sogga11_params par_sogga11 = {
+  0.552, MU_GE, 
+  {0.50000, -2.95535,  15.7974, -91.1804,  96.2030, 0.18683},
+  {0.50000,  3.50743, -12.9523,  49.7870, -33.2545, -11.1396}
+};
+
+/* These coefficients include the factor (1-X) in the functional definition. */
+static const gga_x_sogga11_params par_sogga11_x = {
+  0.552, MU_GE, 
+  {0.29925,  3.21638, -3.55605,  7.65852, -11.2830, 5.25813},
+  {0.29925, -2.88595,  3.23617, -2.45393, -3.75495,  3.96613}
+};
+
 static void 
 gga_x_sogga11_init(XC(func_type) *p)
 {
+  gga_x_sogga11_params *params;
+
+  assert(p!=NULL && p->params == NULL);
+  p->params = malloc(sizeof(gga_x_sogga11_params));
+  params = (gga_x_sogga11_params *) (p->params);
+
   switch(p->info->number){
   case XC_GGA_X_SOGGA11:
-    p->func = 0;
+    memcpy(params, &par_sogga11, sizeof(gga_x_sogga11_params));
     break;
   case XC_HYB_GGA_X_SOGGA11_X:
-    p->func = 1;
+    memcpy(params, &par_sogga11_x, sizeof(gga_x_sogga11_params));
     p->cam_alpha = 0.4015;
     break;
   default:
@@ -41,71 +64,9 @@ gga_x_sogga11_init(XC(func_type) *p)
   }
 }
 
-void XC(gga_x_sogga11_enhance)
-  (const XC(func_type) *p, int order, FLOAT x, 
-   FLOAT *f, FLOAT *dfdx, FLOAT *d2fdx2, FLOAT *d3fdx3)
-{
-  const FLOAT kappa = 0.552;
-  const FLOAT mu = 10.0/81.0;
-  const FLOAT alpha = mu*X2S*X2S/kappa;
-  const FLOAT aa[][6] = {
-    {0.50000, -2.95535,  15.7974, -91.1804,  96.2030, 0.18683},   /* SOGGA11   */
-    {0.29925,  3.21638, -3.55605,  7.65852, -11.2830, 5.25813}    /* SOGGA11-X. These coefficients include the factor (1-X) in the functional definition. */
-  };
-  const FLOAT bb[][6] = {
-    {0.50000,  3.50743, -12.9523,  49.7870, -33.2545, -11.1396},  /* SOGGA11   */
-    {0.29925, -2.88595,  3.23617, -2.45393, -3.75495,  3.96613}   /* SOGGA11-X. These coefficients include the factor (1-X) in the functional definition. */
-  };
-    
-  FLOAT f0, df0, d2f0, d3f0, den0, den1, t0, dt0, d2t0, d3t0, t1, dt1, d2t1, d3t1, f1, df1, d2f1, d3f1;
+#include "maple2c/gga_x_sogga11.c"
 
-  den0 = -1.0/(1.0 + alpha*x*x);
-  f0   =  1.0 + den0;
-  den1 = -EXP(-alpha*x*x);
-  f1   =  1.0 + den1;
-
-  t0 = aa[p->func][0] + f0*(aa[p->func][1] + f0*(aa[p->func][2] + f0*(aa[p->func][3] + f0*(aa[p->func][4] + f0*aa[p->func][5]))));
-  t1 = bb[p->func][0] + f1*(bb[p->func][1] + f1*(bb[p->func][2] + f1*(bb[p->func][3] + f1*(bb[p->func][4] + f1*bb[p->func][5]))));
-
-  *f  = t0;
-  *f += t1;
-
-  if(order < 1) return;
-
-  df0 =  2.0*alpha*x*den0*den0;
-  df1 = -2.0*alpha*x*den1;
-
-  dt0  = aa[p->func][1] + f0*(2.0*aa[p->func][2] + f0*(3.0*aa[p->func][3] + f0*(4.0*aa[p->func][4] + f0*5.0*aa[p->func][5])));
-  dt1  = bb[p->func][1] + f1*(2.0*bb[p->func][2] + f1*(3.0*bb[p->func][3] + f1*(4.0*bb[p->func][4] + f1*5.0*bb[p->func][5])));
-
-  *dfdx  = dt0*df0;
-  *dfdx += dt1*df1;
-
-  if(order < 2) return;
-
-  d2f0 = 2.0*alpha*(3.0*alpha*x*x - 1.0)*den0*den0*den0;
-  d2f1 = 2.0*alpha*(2.0*alpha*x*x - 1.0)*den1;
-
-  d2t0 = 2.0*aa[p->func][2] + f0*(3.0*2.0*aa[p->func][3] + f0*(4.0*3.0*aa[p->func][4] + f0*5.0*4.0*aa[p->func][5]));
-  d2t1 = 2.0*bb[p->func][2] + f1*(3.0*2.0*bb[p->func][3] + f1*(4.0*3.0*bb[p->func][4] + f1*5.0*4.0*bb[p->func][5]));
-
-  *d2fdx2  = dt0*d2f0 + df0*df0*d2t0;
-  *d2fdx2 += dt1*d2f1 + df1*df1*d2t1;
-
-  if(order < 3) return;
-
-  d3f0 = 24.0*alpha*alpha*x*(alpha*x*x - 1.0)*den0*den0*den0*den0;
-  d3f1 = -4.0*alpha*alpha*x*(2.0*alpha*x*x - 3.0)*den1;
-
-  d3t0 = 3.0*2.0*aa[p->func][3] + f0*(4.0*3.0*2.0*aa[p->func][4] + f0*5.0*4.0*3.0*aa[p->func][5]);
-  d3t1 = 3.0*2.0*bb[p->func][3] + f1*(4.0*3.0*2.0*bb[p->func][4] + f1*5.0*4.0*3.0*bb[p->func][5]);
-
-  *d3fdx3  = 3.0*df0*d2f0*d2t0 + dt0*d3f0 + df0*df0*df0*d3t0;
-  *d3fdx3 += 3.0*df1*d2f1*d2t1 + dt1*d3f1 + df1*df1*df1*d3t1;
-}
-
-
-#define func XC(gga_x_sogga11_enhance)
+#define func maple2c_func
 #include "work_gga_x.c"
 
 

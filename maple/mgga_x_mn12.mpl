92df5cba260db0623484c83af73f6a7861e28908
Miguel Marques <marques@tddft.org>
2017-03-03 14:58:19 +0000

    Now mgga_x_mn12 is handled by maple

---
+++ maple/mgga_x_mn12.mpl
@@ -0,0 +1,52 @@
+(* type: work_mgga_c *)
+(* prefix:
+  mgga_x_mn12_params *params;
+
+  assert(p->params != NULL);
+  params = (mgga_x_mn12_params * ) (p->params);
+*)
+
+$define lda_x_params
+$include "lda_x.mpl"
+
+omega_x := 2.5:
+gamma_x := 0.004:
+
+vx := (rs, z) -> 1.0/(1.0 + rs/(omega_x*RS_FACTOR)*(2.0/(1.0 + z))^(1.0/3.0)):
+ux := x -> gamma_x*x^2/(1.0 + gamma_x*x^2):
+wx := t -> (K_FACTOR_C - t)/(K_FACTOR_C + t):
+
+pol1  := t-> params_a_c[ 1] + params_a_c[ 2]*wx(t) + params_a_c[ 3]*wx(t)^2 + params_a_c[ 4]*wx(t)^3
+  + params_a_c[ 5]*wx(t)^4 + params_a_c[ 6]*wx(t)^5:
+pol2  := t-> params_a_c[ 7] + params_a_c[ 8]*wx(t) + params_a_c[ 9]*wx(t)^2 + params_a_c[10]*wx(t)^3
+   + params_a_c[11]*wx(t)^4:
+pol3  := t-> params_a_c[12] + params_a_c[13]*wx(t) + params_a_c[14]*wx(t)^2 + params_a_c[15]*wx(t)^3:
+pol4  := t-> params_a_c[16] + params_a_c[17]*wx(t) + params_a_c[18]*wx(t)^2:
+pol5  := t-> params_a_c[19] + params_a_c[20]*wx(t) + params_a_c[21]*wx(t)^2 + params_a_c[22]*wx(t)^3
+   + params_a_c[23]*wx(t)^4:
+pol6  := t-> params_a_c[24] + params_a_c[25]*wx(t) + params_a_c[26]*wx(t)^2 + params_a_c[27]*wx(t)^3:
+pol7  := t-> params_a_c[28] + params_a_c[29]*wx(t) + params_a_c[30]*wx(t)^2:
+pol8  := t-> params_a_c[31] + params_a_c[32]*wx(t) + params_a_c[33]*wx(t)^2 + params_a_c[34]*wx(t)^3:
+pol9  := t-> params_a_c[35] + params_a_c[36]*wx(t) + params_a_c[37]*wx(t)^2:
+pol10 := t-> params_a_c[38] + params_a_c[39]*wx(t) + params_a_c[40]*wx(t)^2:
+
+FMN12 := (rs, z, x, t) ->
+  + pol1(t)
+  + pol2(t)*ux(x)
+  + pol3(t)*ux(x)^2
+  + pol4(t)*ux(x)^3
+  + pol5(t)*vx(rs, z)
+  + pol6(t)*ux(x)*vx(rs, z)
+  + pol7(t)*ux(x)^2*vx(rs, z)
+  + pol8(t)*vx(rs, z)^2
+  + pol9(t)*ux(x)*vx(rs, z)^2
+  + pol10(t)*vx(rs, z)^3:
+
+f_spin := (rs, z, x, t) ->
+  lda_x_ax*(1.0 + z)^(4.0/3.0)/rs*FMN12(rs, z, x, t):
+
+f_mn12 := (rs, z, xt, xs0, xs1, ts0, ts1) ->
+  f_spin(rs, z, xs0, ts0) + f_spin(rs, -z, xs1, ts1):
+
+f := (rs, z, xt, xs0, xs1, ts0, ts1, us0, us1) ->
+  f_mn12(rs, z, xt, xs0, xs1, ts0, ts1):
--- src/mgga_x_mn12.c
+++ src/mgga_x_mn12.c
@@ -24,6 +24,10 @@
 #define XC_MGGA_X_MN15_L        260 /* MN15-L functional from Minnesota  */
 #define XC_HYB_MGGA_X_MN15      268 /* MN15 functional from Minnesota  */
 
+typedef struct{
+  const FLOAT c[40];
+} mgga_x_mn12_params;
+
 /* the ordering is 
 CC000 [ 0], CC001 [ 1], CC002 [ 2], CC003 [ 3], CC004 [ 4], CC005 [ 5]
 CC010 [ 6], CC011 [ 7], CC012 [ 8], CC013 [ 9], CC014 [10],
@@ -36,192 +40,95 @@ CC200 [30], CC201 [31], CC202 [32], CC203 [33],
 CC210 [34], CC211 [35], CC212 [36],
 CC300 [37], CC301 [38], CC302 [39]
 */
-static const FLOAT CC_MN12_L[] =
-  { 6.735981e-01, -2.270598e+00, -2.613712e+00,  3.993609e+00,  4.635575e+00, 1.250676e+00,
-    8.444920e-01, -1.301173e+01, -1.777730e+01, -4.627211e+00,  5.976605e+00,
-    1.142897e+00, -2.040226e+01, -2.382843e+01,  7.119109e+00,
-   -2.335726e+01, -1.622633e+01,  1.482732e+01,
-    1.449285e+00,  1.020598e+01,  4.407450e+00, -2.008193e+01, -1.253561e+01,
-   -5.435031e+00,  1.656736e+01,  2.000229e+01, -2.513105e+00,
-    9.658436e+00, -3.825281e+00, -2.500000e+01,
-   -2.070080e+00, -9.951913e+00,  8.731211e-01,  2.210891e+01,
-    8.822633e+00,  2.499949e+01,  2.500000e+01,
-    6.851693e-01, -7.406948e-02, -6.788000e-01
-  };
+static const mgga_x_mn12_params par_mn12_l = { 
+   6.735981e-01, -2.270598e+00, -2.613712e+00,  3.993609e+00,  4.635575e+00, 1.250676e+00,
+   8.444920e-01, -1.301173e+01, -1.777730e+01, -4.627211e+00,  5.976605e+00,
+   1.142897e+00, -2.040226e+01, -2.382843e+01,  7.119109e+00,
+  -2.335726e+01, -1.622633e+01,  1.482732e+01,
+   1.449285e+00,  1.020598e+01,  4.407450e+00, -2.008193e+01, -1.253561e+01,
+  -5.435031e+00,  1.656736e+01,  2.000229e+01, -2.513105e+00,
+   9.658436e+00, -3.825281e+00, -2.500000e+01,
+  -2.070080e+00, -9.951913e+00,  8.731211e-01,  2.210891e+01,
+   8.822633e+00,  2.499949e+01,  2.500000e+01,
+   6.851693e-01, -7.406948e-02, -6.788000e-01
+};
 
-static const FLOAT CC_MN12_SX[] =
-  { 5.226556e-01, -2.681208e-01, -4.670705e+00,  3.067320e+00,  4.095370e+00,  2.653023e+00,
-    5.165969e-01, -2.035442e+01, -9.946472e+00,  2.938637e+00,  1.131100e+01,
-    4.752452e+00, -3.061331e+00, -2.523173e+01,  1.710903e+01,
-   -2.357480e+01, -2.727754e+01,  1.603291e+01,  
-    1.842503e+00,  1.927120e+00,  1.107987e+01, -1.182087e+01, -1.117768e+01,
-   -5.821000e+00,  2.266545e+01,  8.246708e+00, -4.778364e+00,
-    5.329122e-01, -6.666755e+00,  1.671429e+00,
-   -3.311409e+00,  3.415913e-01, -6.413076e+00,  1.038584e+01,
-    9.026277e+00,  1.929689e+01,  2.669232e+01,
-    1.517278e+00, -3.442503e+00,  1.100161e+00
-  };
+static const mgga_x_mn12_params par_mn12_sx = { 
+   5.226556e-01, -2.681208e-01, -4.670705e+00,  3.067320e+00,  4.095370e+00,  2.653023e+00,
+   5.165969e-01, -2.035442e+01, -9.946472e+00,  2.938637e+00,  1.131100e+01,
+   4.752452e+00, -3.061331e+00, -2.523173e+01,  1.710903e+01,
+  -2.357480e+01, -2.727754e+01,  1.603291e+01,  
+   1.842503e+00,  1.927120e+00,  1.107987e+01, -1.182087e+01, -1.117768e+01,
+  -5.821000e+00,  2.266545e+01,  8.246708e+00, -4.778364e+00,
+   5.329122e-01, -6.666755e+00,  1.671429e+00,
+  -3.311409e+00,  3.415913e-01, -6.413076e+00,  1.038584e+01,
+   9.026277e+00,  1.929689e+01,  2.669232e+01,
+   1.517278e+00, -3.442503e+00,  1.100161e+00
+};
 
-static const FLOAT CC_MN15_L[] =
-  { 0.670864162, -0.822003903, -1.022407046,  1.689460986, -0.00562032,  -0.110293849,
-    0.972245178, -6.697641991, -4.322814495, -6.786641376, -5.687461462,
-    9.419643818, 11.83939406,   5.086951311,  4.302369948,
-   -8.07344065,   2.429988978, 11.09485698,
-    1.247333909,  3.700485291,  0.867791614, -0.591190518, -0.295305435,
-   -5.825759145,  2.537532196,  3.143390933,  2.939126332,
-    0.599342114,  2.241702738,  2.035713838,
-   -1.525344043, -2.325875691,  1.141940663, -1.563165026,
-    7.882032871, 11.93400684,   9.852928303,
-    0.584030245, -0.720941131, -2.836037078
-  };
+static const mgga_x_mn12_params par_mn15_l = { 
+   0.670864162, -0.822003903, -1.022407046,  1.689460986, -0.00562032,  -0.110293849,
+   0.972245178, -6.697641991, -4.322814495, -6.786641376, -5.687461462,
+   9.419643818, 11.83939406,   5.086951311,  4.302369948,
+  -8.07344065,   2.429988978, 11.09485698,
+   1.247333909,  3.700485291,  0.867791614, -0.591190518, -0.295305435,
+  -5.825759145,  2.537532196,  3.143390933,  2.939126332,
+   0.599342114,  2.241702738,  2.035713838,
+  -1.525344043, -2.325875691,  1.141940663, -1.563165026,
+   7.882032871, 11.93400684,   9.852928303,
+   0.584030245, -0.720941131, -2.836037078
+};
 
-static const FLOAT CC_MN15[] =
-  {
-    0.073852235, -0.839976156, -3.082660125, -1.02881285, -0.811697255,   -0.063404387,
-    2.54805518,  -5.031578906,  0.31702159,  2.981868205, -0.749503735,
-    0.231825661,  1.261961411,  1.665920815, 7.483304941,
-   -2.544245723,  1.384720031,  6.902569885,
-    1.657399451, 2.98526709,    6.89391326,   2.489813993,  1.454724691,
-   -5.054324071, 2.35273334,    1.299104132,  1.203168217,
-    0.121595877,  8.048348238, 21.91203659,
-   -1.852335832, -3.4722735,   -1.564591493, -2.29578769,
-    3.666482991, 10.87074639,  9.696691388,
-    0.630701064, -0.505825216, -3.562354535
-  };
-
-static void
-hyb_mgga_x_mn12_sx_init(XC(func_type) *p)
-{
-  p->cam_alpha = 0.00;
-  p->cam_beta  = 0.25;
-  p->cam_omega = 0.11;
-}
+static const mgga_x_mn12_params par_mn15 = {
+   0.073852235, -0.839976156, -3.082660125, -1.02881285, -0.811697255,   -0.063404387,
+   2.54805518,  -5.031578906,  0.31702159,  2.981868205, -0.749503735,
+   0.231825661,  1.261961411,  1.665920815, 7.483304941,
+  -2.544245723,  1.384720031,  6.902569885,
+   1.657399451, 2.98526709,    6.89391326,   2.489813993,  1.454724691,
+  -5.054324071, 2.35273334,    1.299104132,  1.203168217,
+   0.121595877,  8.048348238, 21.91203659,
+  -1.852335832, -3.4722735,   -1.564591493, -2.29578769,
+   3.666482991, 10.87074639,  9.696691388,
+   0.630701064, -0.505825216, -3.562354535
+};
 
 static void
-hyb_mgga_x_mn15_init(XC(func_type) *p)
+mgga_x_mn12_init(XC(func_type) *p)
 {
-  p->cam_alpha = 0.44;
-  p->cam_beta  = 0.00;
-  p->cam_omega = 0.00;
-}
+  mgga_x_mn12_params *params;
 
-static void 
-func(const XC(func_type) *pt, XC(mgga_work_c_t) *r)
-{
-  int is;
-  const FLOAT *CC, sign[2] = {1.0, -1.0}, omega_x=2.5, gamma_x=0.004;
+  assert(p->params == NULL);
+  p->params = malloc(sizeof(mgga_x_mn12_params));
+  params = (mgga_x_mn12_params *) (p->params);
 
-  FLOAT cnst_rs, opz, opz13, rss, x2;
-  FLOAT vx, vx2, vx3, ux_d, ux, ux2, ux3, wx_d, wx, wx2, wx3, wx4, wx5;
-  FLOAT pol1, pol2, pol3, pol4, pol5, pol6, pol7, pol8, pol9, pol10;
-  FLOAT ex, FMN12;
-
-  FLOAT drssdrs, drssdz, dvxdrss, duxdxs, dwxdts;
-  FLOAT dpol1, dpol2, dpol3, dpol4, dpol5, dpol6, dpol7, dpol8, dpol9, dpol10;
-  FLOAT dexdz, dexdrss, dFMN12dwx, dFMN12dux, dFMN12dvx;
-
-  switch(pt->info->number) {
-  case(XC_MGGA_X_MN12_L):
-    CC = CC_MN12_L;
+  switch(p->info->number){
+  case XC_MGGA_X_MN12_L:
+    memcpy(params, &par_mn12_l, sizeof(mgga_x_mn12_params));
     break;
-
-  case(XC_MGGA_X_MN15_L):
-    CC = CC_MN15_L;
+  case XC_HYB_MGGA_X_MN12_SX:
+    memcpy(params, &par_mn12_sx, sizeof(mgga_x_mn12_params));
+    p->cam_alpha = 0.00;
+    p->cam_beta  = 0.25;
+    p->cam_omega = 0.11;
     break;
-
-  case(XC_HYB_MGGA_X_MN15):
-    CC = CC_MN15;
+  case XC_MGGA_X_MN15_L:
+    memcpy(params, &par_mn15_l, sizeof(mgga_x_mn12_params));
     break;
-
-  case(XC_HYB_MGGA_X_MN12_SX):
-    CC = CC_MN12_SX;
+  case XC_HYB_MGGA_X_MN15:
+    memcpy(params, &par_mn15, sizeof(mgga_x_mn12_params));
+    p->cam_alpha = 0.44;
+    p->cam_beta  = 0.00;
+    p->cam_omega = 0.00;
     break;
-
   default:
     fprintf(stderr, "Internal error in mgga_x_mn12\n");
     exit(1);
   }
-  
-  cnst_rs = CBRT(4.0*M_PI/3.0);
-
-  r->f = 0.0;
-  if(r->order >= 1)
-    r->dfdrs = r->dfdz = r->dfdxt = r->dfdxs[0] = r->dfdxs[1] = r->dfdts[0] = r->dfdts[1] = 0.0;
-
-  /* now the spin-resolved part */
-  for(is = 0; is < 2; is++){
-    opz   = 1.0 + sign[is]*r->z;
-    if(opz < pt->info->min_zeta) continue;
-
-    opz13 = CBRT(opz);
-    rss   = r->rs*M_CBRT2/opz13;
-    x2    = r->xs[is]*r->xs[is];
-
-    vx    = 1.0/(1.0 + (cnst_rs/omega_x)*rss);
-
-    ux_d  = 1.0/(1.0 + gamma_x*x2);
-    ux    = gamma_x*x2*ux_d;
-
-    wx_d  = 1.0/(K_FACTOR_C + r->ts[is]);
-    wx    = (K_FACTOR_C - r->ts[is])*wx_d;
-
-    vx2 = vx*vx; vx3 = vx2*vx;
-    ux2 = ux*ux; ux3 = ux2*ux;
-    wx2 = wx*wx; wx3 = wx2*wx; wx4 = wx3*wx; wx5 = wx4*wx;
-
-    pol1  = CC[ 0] + CC[ 1]*wx + CC[ 2]*wx2 + CC[ 3]*wx3 + CC[ 4]*wx4 + CC[ 5]*wx5;
-    pol2  = CC[ 6] + CC[ 7]*wx + CC[ 8]*wx2 + CC[ 9]*wx3 + CC[10]*wx4;
-    pol3  = CC[11] + CC[12]*wx + CC[13]*wx2 + CC[14]*wx3;
-    pol4  = CC[15] + CC[16]*wx + CC[17]*wx2;
-    pol5  = CC[18] + CC[19]*wx + CC[20]*wx2 + CC[21]*wx3 + CC[22]*wx4;
-    pol6  = CC[23] + CC[24]*wx + CC[25]*wx2 + CC[26]*wx3;
-    pol7  = CC[27] + CC[28]*wx + CC[29]*wx2;
-    pol8  = CC[30] + CC[31]*wx + CC[32]*wx2 + CC[33]*wx3;
-    pol9  = CC[34] + CC[35]*wx + CC[36]*wx2;
-    pol10 = CC[37] + CC[38]*wx + CC[39]*wx2;
-
-    FMN12 = pol1 + ux*pol2 + ux2*pol3 + ux3*pol4 +
-      vx*pol5 + ux*vx*pol6 + ux2*vx*pol7 + vx2*pol8 + ux*vx2*pol9 + vx3*pol10;
-
-    ex    = -X_FACTOR_C*opz/(2.0*cnst_rs*rss);
-    r->f += ex*FMN12;
-    
-    if(r->order < 1) continue;
-
-    drssdrs = M_CBRT2/opz13;
-    drssdz  = -sign[is]*rss/(3.0*opz);
-
-    dvxdrss = -(cnst_rs/omega_x)*vx*vx;
-    duxdxs  = 2.0*gamma_x*r->xs[is]*ux_d*ux_d;
-    dwxdts  = -2.0*K_FACTOR_C*wx_d*wx_d;
-
-    dpol1 =  CC[ 1] + 2.0*CC[ 2]*wx + 3.0*CC[ 3]*wx2 + 4.0*CC[ 4]*wx3 + 5.0*CC[ 5]*wx4;
-    dpol2  = CC[ 7] + 2.0*CC[ 8]*wx + 3.0*CC[ 9]*wx2 + 4.0*CC[10]*wx3;
-    dpol3  = CC[12] + 2.0*CC[13]*wx + 3.0*CC[14]*wx2;
-    dpol4  = CC[16] + 2.0*CC[17]*wx;
-    dpol5  = CC[19] + 2.0*CC[20]*wx + 3.0*CC[21]*wx2 + 4.0*CC[22]*wx3;
-    dpol6  = CC[24] + 2.0*CC[25]*wx + 3.0*CC[26]*wx2;
-    dpol7  = CC[28] + 2.0*CC[29]*wx;
-    dpol8  = CC[31] + 2.0*CC[32]*wx + 3.0*CC[33]*wx2;
-    dpol9  = CC[35] + 2.0*CC[36]*wx;
-    dpol10 = CC[38] + 2.0*CC[39]*wx;
-
-    dFMN12dwx = dpol1 + ux*dpol2 + ux2*dpol3 + ux3*dpol4 +
-      vx*dpol5 + ux*vx*dpol6 + ux2*vx*dpol7 + vx2*dpol8 + ux*vx2*dpol9 + vx3*dpol10;
-    dFMN12dux = pol2 + 2.0*ux*pol3 + 3.0*ux2*pol4 + vx*pol6 + 2.0*ux*vx*pol7 + vx2*pol9;
-    dFMN12dvx = pol5 + ux*pol6 + ux2*pol7 + 2.0*vx*pol8 + 2.0*ux*vx*pol9 + 3.0*vx2*pol10;
-
-    dexdrss = -ex/rss;
-    dexdz   = sign[is]*ex/opz;
-
-    r->dfdrs    += (dexdrss*FMN12 + ex*dFMN12dvx*dvxdrss)*drssdrs;
-    r->dfdz     += dexdz*FMN12 + (dexdrss*FMN12 + ex*dFMN12dvx*dvxdrss)*drssdz;
-    r->dfdxs[is] = ex*dFMN12dux*duxdxs;
-    r->dfdts[is] = ex*dFMN12dwx*dwxdts;
-  }
 }
 
+#include "maple2c/mgga_x_mn12.c"
 
+#define func maple2c_func
 #include "work_mgga_c.c"
 
 
@@ -234,9 +141,8 @@ const XC(func_info_type) XC(func_info_mgga_x_mn12_l) = {
   XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC,
   1e-32, 1e-32, 1e-32, 1e-32,
   0, NULL, NULL,
-  NULL,
-  NULL, NULL, NULL,
-  work_mgga_c,
+  mgga_x_mn12_init, NULL, 
+  NULL, NULL, work_mgga_c,
 };
 
 const XC(func_info_type) XC(func_info_hyb_mgga_x_mn12_sx) = {
@@ -248,9 +154,8 @@ const XC(func_info_type) XC(func_info_hyb_mgga_x_mn12_sx) = {
   XC_FLAGS_3D | XC_FLAGS_HYB_CAM | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC,
   1e-32, 1e-32, 1e-32, 1e-32,
   0, NULL, NULL,
-  hyb_mgga_x_mn12_sx_init, NULL,
-  NULL, NULL,
-  work_mgga_c
+  mgga_x_mn12_init, NULL,
+  NULL, NULL, work_mgga_c
 };
 
 const XC(func_info_type) XC(func_info_mgga_x_mn15_l) = {
@@ -262,9 +167,8 @@ const XC(func_info_type) XC(func_info_mgga_x_mn15_l) = {
   XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC,
   1e-32, 1e-32, 1e-32, 1e-32,
   0, NULL, NULL,
-  NULL,
-  NULL, NULL, NULL,
-  work_mgga_c,
+  mgga_x_mn12_init, NULL, 
+  NULL, NULL, work_mgga_c,
 };
 
 const XC(func_info_type) XC(func_info_hyb_mgga_x_mn15) = {
@@ -276,8 +180,7 @@ const XC(func_info_type) XC(func_info_hyb_mgga_x_mn15) = {
   XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC,
   1e-32, 1e-32, 1e-32, 1e-32,
   0, NULL, NULL,
-  hyb_mgga_x_mn15_init,
-  NULL, NULL, NULL,
-  work_mgga_c,
+  mgga_x_mn12_init, NULL, 
+  NULL, NULL, work_mgga_c,
 };
 

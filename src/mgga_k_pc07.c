55bd68cfddcb31aab208e0e27ff847e43a8a33dd
Micael Oliveira <micael.oliveira@mpsd.mpg.de>
2017-09-15 15:24:31 +0200

    XC_MGGA_K_PC07 was mislabeled as an exchange.

--- src/mgga_k_pc07.c
+++ src/mgga_k_pc07.c
@@ -28,7 +28,7 @@
 
 const xc_func_info_type xc_func_info_mgga_k_pc07 = {
   XC_MGGA_K_PC07,
-  XC_EXCHANGE,
+  XC_KINETIC,
   "Perdew and Constantin 2007",
   XC_FAMILY_MGGA,
   {&xc_ref_Perdew2007_155109, NULL, NULL, NULL, NULL},

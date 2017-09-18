cc88587e076cdf7c28cef4d7e18d1e5b5d734319
Micael Oliveira <micael.oliveira@mpsd.mpg.de>
2017-09-18 14:29:33 +0200

    Calculation of regression data is now done only for one spin option at a time.

--- testsuite/xc-regression.c
+++ testsuite/xc-regression.c
@@ -140,7 +140,7 @@ void free_memory(values_t val) {
 values_t read_data(const char *file, int nspin) {
   /* Format string */
   static const char fmt[]="%lf %lf %lf %lf %lf %lf %lf %lf %lf";
-  
+
   /* Data buffer */
   char buf[BUFSIZE];
   char *cp;
@@ -239,7 +239,7 @@ int main(int argc, char *argv[])
   static const char sfmt3[]=" %23s %23s %23s";
 
   if(argc != 5) {
-    fprintf(stderr, "Usage:\n%s funct input output.unpol output.pol\n", argv[0]);
+    fprintf(stderr, "Usage:\n%s funct nspin input output\n", argv[0]);
     exit(1);
   }
 
@@ -250,247 +250,248 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
-  for(nspin=1; nspin<=2; nspin++) {
-    /* Data array */
+  /* Spin-polarized or unpolarized ? */
+  nspin = atoi(argv[2]);
+
+  /* Data array */
     values_t d;
-    /* Functional evaluator */
-    xc_func_type func;
-    /* Flags for functional */
-    int flags;
-    /* Functional family */
-    int family;
-    /* Output file */
-    FILE *out;
-    /* Output file name */
-    char *fname;
+  /* Functional evaluator */
+  xc_func_type func;
+  /* Flags for functional */
+  int flags;
+  /* Functional family */
+  int family;
+  /* Output file */
+  FILE *out;
+  /* Output file name */
+  char *fname;
 
-    /* Helpers for properties that may not have been implemented */
-    double *zk, *vrho, *v2rho2, *v3rho3;
+  /* Helpers for properties that may not have been implemented */
+  double *zk, *vrho, *v2rho2, *v3rho3;
 
-    /* Read in data */
-    d=read_data(argv[2],nspin);
+  /* Read in data */
+  d=read_data(argv[3],nspin);
 
-    /* Initialize functional */
-    if(xc_func_init(&func, func_id, nspin)) {
-      fprintf(stderr, "Functional '%d' (%s) not found.\nPlease report a bug against functional_get_number.\n", func_id, argv[1]);
-      exit(1);
-    }
-    /* Get flags */
-    flags=func.info->flags;
-    family=func.info->family;
+  /* Initialize functional */
+  if(xc_func_init(&func, func_id, nspin)) {
+    fprintf(stderr, "Functional '%d' (%s) not found.\nPlease report a bug against functional_get_number.\n", func_id, argv[1]);
+    exit(1);
+  }
+  /* Get flags */
+  flags=func.info->flags;
+  family=func.info->family;
 
-    /* Set helpers */
-    zk     = (flags & XC_FLAGS_HAVE_EXC) ? d.zk     : NULL;
-    vrho   = (flags & XC_FLAGS_HAVE_VXC) ? d.vrho   : NULL;
-    v2rho2 = (flags & XC_FLAGS_HAVE_FXC) ? d.v2rho2 : NULL;
-    v3rho3 = (flags & XC_FLAGS_HAVE_KXC) ? d.v3rho3 : NULL;
+  /* Set helpers */
+  zk     = (flags & XC_FLAGS_HAVE_EXC) ? d.zk     : NULL;
+  vrho   = (flags & XC_FLAGS_HAVE_VXC) ? d.vrho   : NULL;
+  v2rho2 = (flags & XC_FLAGS_HAVE_FXC) ? d.v2rho2 : NULL;
+  v3rho3 = (flags & XC_FLAGS_HAVE_KXC) ? d.v3rho3 : NULL;
 
-    /* Evaluate xc functional */
-    switch(family) {
-    case XC_FAMILY_LDA:
-      xc_lda(&func, d.n, d.rho, zk, vrho, v2rho2, v3rho3);
-      break;
-    case XC_FAMILY_GGA:
-    case XC_FAMILY_HYB_GGA:
-      xc_gga(&func, d.n, d.rho, d.sigma, zk, vrho, d.vsigma,		\
-	     v2rho2, d.v2rhosigma, d.v2sigma2, NULL, NULL, NULL, NULL);
-      break;
-    case XC_FAMILY_MGGA:
-    case XC_FAMILY_HYB_MGGA:
-      xc_mgga(&func, d.n, d.rho, d.sigma, d.lapl, d.tau, zk, vrho, d.vsigma, d.vlapl, d.vtau, \
-	      v2rho2, d.v2sigma2, d.v2lapl2, d.v2tau2, d.v2rhosigma, d.v2rholapl, d.v2rhotau, \
-	      d.v2sigmalapl, d.v2sigmatau, d.v2lapltau);
-      break;
+  /* Evaluate xc functional */
+  switch(family) {
+  case XC_FAMILY_LDA:
+    xc_lda(&func, d.n, d.rho, zk, vrho, v2rho2, v3rho3);
+    break;
+  case XC_FAMILY_GGA:
+  case XC_FAMILY_HYB_GGA:
+    xc_gga(&func, d.n, d.rho, d.sigma, zk, vrho, d.vsigma,		\
+    v2rho2, d.v2rhosigma, d.v2sigma2, NULL, NULL, NULL, NULL);
+    break;
+  case XC_FAMILY_MGGA:
+  case XC_FAMILY_HYB_MGGA:
+    xc_mgga(&func, d.n, d.rho, d.sigma, d.lapl, d.tau, zk, vrho, d.vsigma, d.vlapl, d.vtau, \
+     v2rho2, d.v2sigma2, d.v2lapl2, d.v2tau2, d.v2rhosigma, d.v2rholapl, d.v2rhotau, \
+     d.v2sigmalapl, d.v2sigmatau, d.v2lapltau);
+    break;
 
-    default:
-      fprintf(stderr,"Support for family %i not implemented.\n",family);
-      free_memory(d);
-      exit(1);
-    }
+  default:
+    fprintf(stderr,"Support for family %i not implemented.\n",family);
+    free_memory(d);
+    exit(1);
+  }
 
     
-    /* Open output file */
-    fname = (nspin==XC_POLARIZED) ? argv[3] : argv[4];
-    out = fopen(fname,"w");
-    if(!out) {
-      fprintf(stderr,"Error opening output file %s.\n",fname);
-      free_memory(d);
-      exit(1);
+  /* Open output file */
+  fname = argv[4];
+  out = fopen(fname,"w");
+  if(!out) {
+    fprintf(stderr,"Error opening output file %s.\n",fname);
+    free_memory(d);
+    exit(1);
+  }
+
+  /* Functional id and amount of lines in output */
+  fprintf(out,"%i %i\n",func_id,d.n);
+
+  /* energy */
+  if(flags & XC_FLAGS_HAVE_EXC)
+    fprintf(out,sfmt,"zk");
+    
+  /* LDA part */
+  if(nspin==XC_POLARIZED) {
+    /* first order derivatives */
+    if(flags & XC_FLAGS_HAVE_VXC) {
+      fprintf(out,sfmt2,"vrho(a)","vrho(b)");
+	    if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA))
+	      fprintf(out,sfmt3,"vsigma(aa)","vsigma(ab)","vsigma(bb)");
+	    if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
+	      fprintf(out,sfmt2,"vlapl(a)","vlapl(b)");
+	      fprintf(out,sfmt2,"vtau(a)","vtau(b)");
+	    }
     }
+      
+    /* second-order derivatives */
+    if(flags & XC_FLAGS_HAVE_FXC) {
+	    fprintf(out,sfmt3,"v2rho(aa)","v2rho(ab)","v2rho(bb)");
+	    if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
+	      fprintf(out,sfmt3,"v2sigma2(aa-aa)","v2sigma2(aa-ab)","v2sigma2(aa-bb)");
+	      fprintf(out,sfmt3,"v2sigma2(ab-ab)","v2sigma2(ab-bb)","v2sigma2(bb-bb)");
+	      fprintf(out,sfmt3,"v2rho(a)sigma(aa)","v2rho(a)sigma(ab)","v2rho(a)sigma(bb)");
+	      fprintf(out,sfmt3,"v2rho(b)sigma(aa)","v2rho(b)sigma(ab)","v2rho(b)sigma(bb)");
+	    }
+	
+	    if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
+	      fprintf(out,sfmt3,"v2lapl2(aa)","v2lapl2(ab)","v2lapl2(bb)");
+	      fprintf(out,sfmt3,"v2tau2(aa)","v2tau2(ab)","v2tau2(bb)");
+	      fprintf(out,sfmt3,"v2rholapl(aa)","v2rholapl(ab)","v2rholapl(bb)");
+	      fprintf(out,sfmt3,"v2rhotau(aa)","v2rhotau(ab)","v2rhotau(bb)");
+	      fprintf(out,sfmt3,"v2lapltau(aa)","v2lapltau(ab)","v2lapltau(bb)");
+	      fprintf(out,sfmt3,"v2sigma(aa)tau(a)","v2sigma(aa)tau(b)","v2sigma(ab)tau(a)");
+	      fprintf(out,sfmt3,"v2sigma(ab)tau(b)","v2sigma(bb)tau(a)","v2sigma(bb)tau(b)");
+	      fprintf(out,sfmt3,"v2sigma(aa)lapl(a)","v2sigma(aa)lapl(b)","v2sigma(ab)lapl(a)");
+	      fprintf(out,sfmt3,"v2sigma(ab)lapl(b)","v2sigma(bb)lapl(a)","v2sigma(bb)lapl(b)");
+	    }
+    }
+     
+    /* third-order derivatives ... to be done */
 
-    /* Functional id and amount of lines in output */
-    fprintf(out,"%i %i\n",func_id,d.n);
+    fprintf(out,"\n");
 
+  } else {
+
+    /* first order derivatives */
+    if(flags & XC_FLAGS_HAVE_VXC) {
+	    fprintf(out,sfmt,"vrho");
+	    if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA))
+	      fprintf(out,sfmt,"vsigma");
+	    if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
+	      fprintf(out,sfmt,"vlapl");
+	      fprintf(out,sfmt,"vtau");
+	    }
+    }
+      
+    /* second-order derivatives */
+    if(flags & XC_FLAGS_HAVE_FXC) {
+	    fprintf(out,sfmt,"v2rho");
+	    if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
+	      fprintf(out,sfmt,"v2sigma2");
+	      fprintf(out,sfmt,"v2rhosigma");
+	    }
+	
+	    if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
+	      fprintf(out,sfmt,"v2lapl2");
+	      fprintf(out,sfmt,"v2tau2");
+	      fprintf(out,sfmt,"v2rholapl");
+	      fprintf(out,sfmt,"v2rhotau");
+	      fprintf(out,sfmt,"v2lapltau");
+	      fprintf(out,sfmt,"v2sigmatau");
+	      fprintf(out,sfmt,"v2sigmalapl");
+	    }
+    }
+      
+    /* third-order derivatives ... to be done */
+
+    fprintf(out,"\n");
+  }
+
+
+  /* Loop over data points */
+  for(i=0;i<d.n;i++) {
     /* energy */
     if(flags & XC_FLAGS_HAVE_EXC)
-      fprintf(out,sfmt,"zk");
-    
+	    fprintf(out,efmt,d.zk[i]);
+      
     /* LDA part */
     if(nspin==XC_POLARIZED) {
-      /* first order derivatives */
-      if(flags & XC_FLAGS_HAVE_VXC) {
-	fprintf(out,sfmt2,"vrho(a)","vrho(b)");
-	if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA))
-	  fprintf(out,sfmt3,"vsigma(aa)","vsigma(ab)","vsigma(bb)");
-	if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
-	  fprintf(out,sfmt2,"vlapl(a)","vlapl(b)");
-	  fprintf(out,sfmt2,"vtau(a)","vtau(b)");
-	}
-      }
+	    /* first order derivatives */
+	    if(flags & XC_FLAGS_HAVE_VXC) {
+	      fprintf(out,efmt2,d.vrho[2*i],d.vrho[2*i+1]);
+	      if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA))
+	        fprintf(out,efmt3,d.vsigma[3*i],d.vsigma[3*i+1],d.vsigma[3*i+2]);
+	      if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
+	        fprintf(out,efmt2,d.vlapl[2*i],d.vlapl[2*i+1]);
+	        fprintf(out,efmt2,d.vtau[2*i],d.vtau[2*i+1]);
+	      }
+	    }
       
-      /* second-order derivatives */
-      if(flags & XC_FLAGS_HAVE_FXC) {
-	fprintf(out,sfmt3,"v2rho(aa)","v2rho(ab)","v2rho(bb)");
-	if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
-	  fprintf(out,sfmt3,"v2sigma2(aa-aa)","v2sigma2(aa-ab)","v2sigma2(aa-bb)");
-	  fprintf(out,sfmt3,"v2sigma2(ab-ab)","v2sigma2(ab-bb)","v2sigma2(bb-bb)");
-	  fprintf(out,sfmt3,"v2rho(a)sigma(aa)","v2rho(a)sigma(ab)","v2rho(a)sigma(bb)");
-	  fprintf(out,sfmt3,"v2rho(b)sigma(aa)","v2rho(b)sigma(ab)","v2rho(b)sigma(bb)");
-	}
+	    /* second-order derivatives */
+	    if(flags & XC_FLAGS_HAVE_FXC) {
+	      fprintf(out,efmt3,d.v2rho2[3*i],d.v2rho2[3*i+1],d.v2rho2[3*i+2]);
+	      if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
+	        fprintf(out,efmt3,d.v2sigma2[6*i],d.v2sigma2[6*i+1],d.v2sigma2[6*i+2]);
+	        fprintf(out,efmt3,d.v2sigma2[6*i+3],d.v2sigma2[6*i+4],d.v2sigma2[6*i+5]);
+	        fprintf(out,efmt3,d.v2rhosigma[6*i],d.v2rhosigma[6*i+1],d.v2rhosigma[6*i+2]);
+	        fprintf(out,efmt3,d.v2rhosigma[6*i+3],d.v2rhosigma[6*i+4],d.v2rhosigma[6*i+5]);
+	      }
 	
-	if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
-	  fprintf(out,sfmt3,"v2lapl2(aa)","v2lapl2(ab)","v2lapl2(bb)");
-	  fprintf(out,sfmt3,"v2tau2(aa)","v2tau2(ab)","v2tau2(bb)");
-	  fprintf(out,sfmt3,"v2rholapl(aa)","v2rholapl(ab)","v2rholapl(bb)");
-	  fprintf(out,sfmt3,"v2rhotau(aa)","v2rhotau(ab)","v2rhotau(bb)");
-	  fprintf(out,sfmt3,"v2lapltau(aa)","v2lapltau(ab)","v2lapltau(bb)");
-	  fprintf(out,sfmt3,"v2sigma(aa)tau(a)","v2sigma(aa)tau(b)","v2sigma(ab)tau(a)");
-	  fprintf(out,sfmt3,"v2sigma(ab)tau(b)","v2sigma(bb)tau(a)","v2sigma(bb)tau(b)");
-	  fprintf(out,sfmt3,"v2sigma(aa)lapl(a)","v2sigma(aa)lapl(b)","v2sigma(ab)lapl(a)");
-	  fprintf(out,sfmt3,"v2sigma(ab)lapl(b)","v2sigma(bb)lapl(a)","v2sigma(bb)lapl(b)");
-	}
-      }      
+	      if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
+	        fprintf(out,efmt3,d.v2lapl2[3*i],d.v2lapl2[3*i+1],d.v2lapl2[3*i+2]);
+	        fprintf(out,efmt3,d.v2tau2[3*i],d.v2tau2[3*i+1],d.v2tau2[3*i+2]);
+	        fprintf(out,efmt3,d.v2rholapl[3*i],d.v2rholapl[3*i+1],d.v2rholapl[3*i+2]);
+	        fprintf(out,efmt3,d.v2rhotau[3*i],d.v2rhotau[3*i+1],d.v2rhotau[3*i+2]);
+	        fprintf(out,efmt3,d.v2lapltau[3*i],d.v2lapltau[3*i+1],d.v2lapltau[3*i+2]);
+	        fprintf(out,efmt3,d.v2sigmatau[3*i],d.v2sigmatau[3*i+1],d.v2sigmatau[3*i+2]);
+	        fprintf(out,efmt3,d.v2sigmatau[3*i+3],d.v2sigmatau[3*i+4],d.v2sigmatau[3*i+5]);
+	        fprintf(out,efmt3,d.v2sigmalapl[3*i],d.v2sigmalapl[3*i+1],d.v2sigmalapl[3*i+2]);
+	        fprintf(out,efmt3,d.v2sigmalapl[3*i+3],d.v2sigmalapl[3*i+4],d.v2sigmalapl[3*i+5]);
+	      }
+	    }
      
-      /* third-order derivatives ... to be done */
+	    /* third-order derivatives ... to be done */
 
-      fprintf(out,"\n");
+	    fprintf(out,"\n");
 
     } else {
 
-      /* first order derivatives */
-      if(flags & XC_FLAGS_HAVE_VXC) {
-	fprintf(out,sfmt,"vrho");
-	if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA))
-	  fprintf(out,sfmt,"vsigma");
-	if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
-	  fprintf(out,sfmt,"vlapl");
-	  fprintf(out,sfmt,"vtau");
-	}
-      }
+	    /* first order derivatives */
+	    if(flags & XC_FLAGS_HAVE_VXC) {
+	      fprintf(out,efmt,d.vrho[i]);
+	      if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA))
+	        fprintf(out,efmt,d.vsigma[i]);
+	      if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
+	        fprintf(out,efmt,d.vlapl[i]);
+	        fprintf(out,efmt,d.vtau[i]);
+	      }
+	    }
       
-      /* second-order derivatives */
-      if(flags & XC_FLAGS_HAVE_FXC) {
-	fprintf(out,sfmt,"v2rho");
-	if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
-	  fprintf(out,sfmt,"v2sigma2");
-	  fprintf(out,sfmt,"v2rhosigma");
-	}
+	    /* second-order derivatives */
+	    if(flags & XC_FLAGS_HAVE_FXC) {
+	      fprintf(out,efmt,d.v2rho2[i]);
+	      if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
+	        fprintf(out,efmt,d.v2sigma2[i]);
+	        fprintf(out,efmt,d.v2rhosigma[i]);
+	      }
 	
-	if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
-	  fprintf(out,sfmt,"v2lapl2");
-	  fprintf(out,sfmt,"v2tau2");
-	  fprintf(out,sfmt,"v2rholapl");
-	  fprintf(out,sfmt,"v2rhotau");
-	  fprintf(out,sfmt,"v2lapltau");
-	  fprintf(out,sfmt,"v2sigmatau");
-	  fprintf(out,sfmt,"v2sigmalapl");
-	}
-      }
+	      if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
+	        fprintf(out,efmt,d.v2lapl2[i]);
+	        fprintf(out,efmt,d.v2tau2[i]);
+	        fprintf(out,efmt,d.v2rholapl[i]);
+	        fprintf(out,efmt,d.v2rhotau[i]);
+	        fprintf(out,efmt,d.v2lapltau[i]);
+	        fprintf(out,efmt,d.v2sigmatau[i]);
+	        fprintf(out,efmt,d.v2sigmalapl[i]);
+	      }
+	    }
       
-      /* third-order derivatives ... to be done */
+	    /* third-order derivatives ... to be done */
 
       fprintf(out,"\n");
     }
-
-
-    /* Loop over data points */
-    for(i=0;i<d.n;i++) {
-      /* energy */
-      if(flags & XC_FLAGS_HAVE_EXC)
-	fprintf(out,efmt,d.zk[i]);
-      
-      /* LDA part */
-      if(nspin==XC_POLARIZED) {
-	/* first order derivatives */
-	if(flags & XC_FLAGS_HAVE_VXC) {
-	  fprintf(out,efmt2,d.vrho[2*i],d.vrho[2*i+1]);
-	  if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA))
-	    fprintf(out,efmt3,d.vsigma[3*i],d.vsigma[3*i+1],d.vsigma[3*i+2]);
-	  if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
-	    fprintf(out,efmt2,d.vlapl[2*i],d.vlapl[2*i+1]);
-	    fprintf(out,efmt2,d.vtau[2*i],d.vtau[2*i+1]);
-	  }
-	}
-      
-	/* second-order derivatives */
-	if(flags & XC_FLAGS_HAVE_FXC) {
-	  fprintf(out,efmt3,d.v2rho2[3*i],d.v2rho2[3*i+1],d.v2rho2[3*i+2]);
-	  if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
-	    fprintf(out,efmt3,d.v2sigma2[6*i],d.v2sigma2[6*i+1],d.v2sigma2[6*i+2]);
-	    fprintf(out,efmt3,d.v2sigma2[6*i+3],d.v2sigma2[6*i+4],d.v2sigma2[6*i+5]);
-	    fprintf(out,efmt3,d.v2rhosigma[6*i],d.v2rhosigma[6*i+1],d.v2rhosigma[6*i+2]);
-	    fprintf(out,efmt3,d.v2rhosigma[6*i+3],d.v2rhosigma[6*i+4],d.v2rhosigma[6*i+5]);
-	  }
-	
-	  if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
-	    fprintf(out,efmt3,d.v2lapl2[3*i],d.v2lapl2[3*i+1],d.v2lapl2[3*i+2]);
-	    fprintf(out,efmt3,d.v2tau2[3*i],d.v2tau2[3*i+1],d.v2tau2[3*i+2]);
-	    fprintf(out,efmt3,d.v2rholapl[3*i],d.v2rholapl[3*i+1],d.v2rholapl[3*i+2]);
-	    fprintf(out,efmt3,d.v2rhotau[3*i],d.v2rhotau[3*i+1],d.v2rhotau[3*i+2]);
-	    fprintf(out,efmt3,d.v2lapltau[3*i],d.v2lapltau[3*i+1],d.v2lapltau[3*i+2]);
-	    fprintf(out,efmt3,d.v2sigmatau[3*i],d.v2sigmatau[3*i+1],d.v2sigmatau[3*i+2]);
-	    fprintf(out,efmt3,d.v2sigmatau[3*i+3],d.v2sigmatau[3*i+4],d.v2sigmatau[3*i+5]);
-	    fprintf(out,efmt3,d.v2sigmalapl[3*i],d.v2sigmalapl[3*i+1],d.v2sigmalapl[3*i+2]);
-	    fprintf(out,efmt3,d.v2sigmalapl[3*i+3],d.v2sigmalapl[3*i+4],d.v2sigmalapl[3*i+5]);
-	  }
-	}      
-     
-	/* third-order derivatives ... to be done */
-
-	fprintf(out,"\n");
-
-      } else {
-
-	/* first order derivatives */
-	if(flags & XC_FLAGS_HAVE_VXC) {
-	  fprintf(out,efmt,d.vrho[i]);
-	  if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA))
-	    fprintf(out,efmt,d.vsigma[i]);
-	  if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
-	    fprintf(out,efmt,d.vlapl[i]);
-	    fprintf(out,efmt,d.vtau[i]);
-	  }
-	}
-      
-	/* second-order derivatives */
-	if(flags & XC_FLAGS_HAVE_FXC) {
-	  fprintf(out,efmt,d.v2rho2[i]);
-	  if(family & (XC_FAMILY_GGA | XC_FAMILY_HYB_GGA | XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
-	    fprintf(out,efmt,d.v2sigma2[i]);
-	    fprintf(out,efmt,d.v2rhosigma[i]);
-	  }
-	
-	  if(family & (XC_FAMILY_MGGA | XC_FAMILY_HYB_MGGA)) {
-	    fprintf(out,efmt,d.v2lapl2[i]);
-	    fprintf(out,efmt,d.v2tau2[i]);
-	    fprintf(out,efmt,d.v2rholapl[i]);
-	    fprintf(out,efmt,d.v2rhotau[i]);
-	    fprintf(out,efmt,d.v2lapltau[i]);
-	    fprintf(out,efmt,d.v2sigmatau[i]);
-	    fprintf(out,efmt,d.v2sigmalapl[i]);
-	  }
-	}
-      
-	/* third-order derivatives ... to be done */
-
-	fprintf(out,"\n");
-      }
-    }
-   
-    xc_func_end(&func);
-    free_memory(d);
-    fclose(out);
   }
-  
+   
+  xc_func_end(&func);
+  free_memory(d);
+  fclose(out);
+
   return 0;
 }

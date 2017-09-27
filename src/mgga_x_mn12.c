3242c3ce51b0371bfdd6505ef6fcb2aae25d8191
Susi Lehtola <susi.lehtola@gmail.com>
2017-09-27 08:16:12 -0700

    Fix initialization - need two sets of braces for struct that holds array.

--- src/mgga_x_mn12.c
+++ src/mgga_x_mn12.c
@@ -5,12 +5,12 @@
  it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation; either version 3 of the License, or
  (at your option) any later version.
-  
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Lesser General Public License for more details.
-  
+
  You should have received a copy of the GNU Lesser General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -28,7 +28,7 @@ typedef struct{
   const double c[40];
 } mgga_x_mn12_params;
 
-/* the ordering is 
+/* the ordering is
 CC000 [ 0], CC001 [ 1], CC002 [ 2], CC003 [ 3], CC004 [ 4], CC005 [ 5]
 CC010 [ 6], CC011 [ 7], CC012 [ 8], CC013 [ 9], CC014 [10],
 CC020 [11], CC021 [12], CC022 [13], CC023 [14],
@@ -40,33 +40,33 @@ CC200 [30], CC201 [31], CC202 [32], CC203 [33],
 CC210 [34], CC211 [35], CC212 [36],
 CC300 [37], CC301 [38], CC302 [39]
 */
-static const mgga_x_mn12_params par_mn12_l = { 
-   6.735981e-01, -2.270598e+00, -2.613712e+00,  3.993609e+00,  4.635575e+00, 1.250676e+00,
-   8.444920e-01, -1.301173e+01, -1.777730e+01, -4.627211e+00,  5.976605e+00,
-   1.142897e+00, -2.040226e+01, -2.382843e+01,  7.119109e+00,
-  -2.335726e+01, -1.622633e+01,  1.482732e+01,
-   1.449285e+00,  1.020598e+01,  4.407450e+00, -2.008193e+01, -1.253561e+01,
-  -5.435031e+00,  1.656736e+01,  2.000229e+01, -2.513105e+00,
-   9.658436e+00, -3.825281e+00, -2.500000e+01,
-  -2.070080e+00, -9.951913e+00,  8.731211e-01,  2.210891e+01,
-   8.822633e+00,  2.499949e+01,  2.500000e+01,
-   6.851693e-01, -7.406948e-02, -6.788000e-01
-};
+static const mgga_x_mn12_params par_mn12_l = {{
+    6.735981e-01, -2.270598e+00, -2.613712e+00,  3.993609e+00,  4.635575e+00, 1.250676e+00,
+    8.444920e-01, -1.301173e+01, -1.777730e+01, -4.627211e+00,  5.976605e+00,
+    1.142897e+00, -2.040226e+01, -2.382843e+01,  7.119109e+00,
+    -2.335726e+01, -1.622633e+01,  1.482732e+01,
+    1.449285e+00,  1.020598e+01,  4.407450e+00, -2.008193e+01, -1.253561e+01,
+    -5.435031e+00,  1.656736e+01,  2.000229e+01, -2.513105e+00,
+    9.658436e+00, -3.825281e+00, -2.500000e+01,
+    -2.070080e+00, -9.951913e+00,  8.731211e-01,  2.210891e+01,
+    8.822633e+00,  2.499949e+01,  2.500000e+01,
+    6.851693e-01, -7.406948e-02, -6.788000e-01
+  }};
 
-static const mgga_x_mn12_params par_mn12_sx = { 
+static const mgga_x_mn12_params par_mn12_sx = {{
    5.226556e-01, -2.681208e-01, -4.670705e+00,  3.067320e+00,  4.095370e+00,  2.653023e+00,
    5.165969e-01, -2.035442e+01, -9.946472e+00,  2.938637e+00,  1.131100e+01,
    4.752452e+00, -3.061331e+00, -2.523173e+01,  1.710903e+01,
-  -2.357480e+01, -2.727754e+01,  1.603291e+01,  
+  -2.357480e+01, -2.727754e+01,  1.603291e+01,
    1.842503e+00,  1.927120e+00,  1.107987e+01, -1.182087e+01, -1.117768e+01,
   -5.821000e+00,  2.266545e+01,  8.246708e+00, -4.778364e+00,
    5.329122e-01, -6.666755e+00,  1.671429e+00,
   -3.311409e+00,  3.415913e-01, -6.413076e+00,  1.038584e+01,
    9.026277e+00,  1.929689e+01,  2.669232e+01,
    1.517278e+00, -3.442503e+00,  1.100161e+00
-};
+  }};
 
-static const mgga_x_mn12_params par_mn15_l = { 
+static const mgga_x_mn12_params par_mn15_l = {{
    0.670864162, -0.822003903, -1.022407046,  1.689460986, -0.00562032,  -0.110293849,
    0.972245178, -6.697641991, -4.322814495, -6.786641376, -5.687461462,
    9.419643818, 11.83939406,   5.086951311,  4.302369948,
@@ -77,9 +77,9 @@ static const mgga_x_mn12_params par_mn15_l = {
   -1.525344043, -2.325875691,  1.141940663, -1.563165026,
    7.882032871, 11.93400684,   9.852928303,
    0.584030245, -0.720941131, -2.836037078
-};
+  }};
 
-static const mgga_x_mn12_params par_mn15 = {
+static const mgga_x_mn12_params par_mn15 = {{
    0.073852235, -0.839976156, -3.082660125, -1.02881285, -0.811697255,   -0.063404387,
    2.54805518,  -5.031578906,  0.31702159,  2.981868205, -0.749503735,
    0.231825661,  1.261961411,  1.665920815, 7.483304941,
@@ -90,7 +90,7 @@ static const mgga_x_mn12_params par_mn15 = {
   -1.852335832, -3.4722735,   -1.564591493, -2.29578769,
    3.666482991, 10.87074639,  9.696691388,
    0.630701064, -0.505825216, -3.562354535
-};
+  }};
 
 static void
 mgga_x_mn12_init(xc_func_type *p)
@@ -141,7 +141,7 @@ const xc_func_info_type xc_func_info_mgga_x_mn12_l = {
   XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC,
   1e-23,
   0, NULL, NULL,
-  mgga_x_mn12_init, NULL, 
+  mgga_x_mn12_init, NULL,
   NULL, NULL, work_mgga_c,
 };
 
@@ -167,7 +167,7 @@ const xc_func_info_type xc_func_info_mgga_x_mn15_l = {
   XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC,
   1e-23,
   0, NULL, NULL,
-  mgga_x_mn12_init, NULL, 
+  mgga_x_mn12_init, NULL,
   NULL, NULL, work_mgga_c,
 };
 
@@ -180,7 +180,6 @@ const xc_func_info_type xc_func_info_hyb_mgga_x_mn15 = {
   XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC,
   1e-32,
   0, NULL, NULL,
-  mgga_x_mn12_init, NULL, 
+  mgga_x_mn12_init, NULL,
   NULL, NULL, work_mgga_c,
 };
-

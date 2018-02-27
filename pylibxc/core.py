b6b3af58ef9e4c797858ea9904783a881f5a1037
Daniel Smith <malorian@me.com>
2018-02-27 14:55:49 -0500

    PY: Handles loading via NumPy extensions

--- pylibxc/core.py
+++ pylibxc/core.py
@@ -8,27 +8,24 @@ import numpy as np
 import os
 
 # Attempt to load the compiled C code
-__libxc_path = None
 core = None
 
 # First check the local folder
-__abs_path = os.path.abspath(os.path.dirname(__file__))
-for module_file in os.listdir(__abs_path):
-    if module_file.startswith("libxc."):
-        __libxc_path = os.path.join(__abs_path, module_file)
+try:
+    dirpath = os.path.abspath(os.path.dirname(__file__))
+    core = np.ctypeslib.load_library("libxc", dirpath)
+except OSError:
+    # If no libxc is local, check LD_LIBRARY_PATHS's
+    libxc_path = ctypes.util.find_library("libxc")
 
-# If no libxc is local, check LD_LIBRARY_PATHS's
-if __libxc_path is None:
-    __libxc_path = ctypes.util.find_library("libxc")
-
-# If we still havent found it, give up and throw an error
-if __libxc_path is None:
-    raise ImportError(
-        "LibXC Shared object not found, searched Python module local directory and library paths"
-    )
-
-# Load the C object
-core = ctypes.CDLL(__libxc_path)
+    # If we still havent found it, give up and throw an error
+    if libxc_path is None:
+        raise ImportError(
+            "LibXC Shared object not found, searched Python module local directory and library paths"
+        )
+    
+    # Load the C object
+    core = ctypes.CDLL(__libxc_path)
 
 
 def get_core_path():

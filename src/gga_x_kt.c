b81a1c1e35cdb499d9d3bd3a5db8fbf3b0e896e8
Miguel Marques <marques@tddft.org>
2018-05-08 16:53:33 +0200

    Added KT3 xc functional.

--- src/gga_x_kt.c
+++ src/gga_x_kt.c
@@ -11,6 +11,7 @@
 #define XC_GGA_X_KT1          145 /* Exchange part of Keal and Tozer version 1 */
 #define XC_GGA_XC_KT1         167 /* Keal and Tozer version 1                  */
 #define XC_GGA_XC_KT2         146 /* Keal and Tozer version 2                  */
+#define XC_GGA_XC_KT3         587 /* Keal and Tozer version 3                  */
 
 typedef struct{
   double gamma, delta;
@@ -79,6 +80,7 @@ const xc_func_info_type xc_func_info_gga_xc_kt1 = {
   NULL, NULL, NULL
 };
 
+
 static void
 gga_xc_kt2_init(xc_func_type *p)
 {
@@ -98,5 +100,44 @@ const xc_func_info_type xc_func_info_gga_xc_kt2 = {
   1e-24,
   0, NULL, NULL,
   gga_xc_kt2_init, NULL, 
+  NULL, NULL, NULL
+};
+
+
+static void
+gga_xc_kt3_init(xc_func_type *p)
+{
+  static int   funcs_id  [4] = {XC_LDA_X, XC_GGA_C_LYP, XC_GGA_X_KT1, XC_GGA_X_OPTX};
+  double funcs_coef[4];
+
+  /* Equation (2) */
+  static const double
+    alpha = 1.092,
+    beta  = 0.864409,
+    par_kt[2] = {-0.004, 0.1},
+    eps   = -0.925452,
+    a1    = 1.05151, /* these are the OPTX coefficients */
+    a2    = 1.43169;
+
+  funcs_coef[0] = alpha - eps*a1/a2;
+  funcs_coef[1] = beta;
+  funcs_coef[2] = 1.0;
+  funcs_coef[3] = eps/a2;
+
+  xc_mix_init(p, 4, funcs_id, funcs_coef);
+  /* set_ext_params(p->func_aux[2], par_kt); */
+  xc_gga_x_kt_set_params(p->func_aux[2], par_kt[0], par_kt[1]);
+}
+
+const xc_func_info_type xc_func_info_gga_xc_kt3 = {
+  XC_GGA_XC_KT3,
+  XC_EXCHANGE_CORRELATION,
+  "Keal and Tozer, version 3",
+  XC_FAMILY_GGA,
+  {&xc_ref_Keal2004_5654, NULL, NULL, NULL, NULL},
+  XC_FLAGS_3D | XC_FLAGS_HAVE_EXC | XC_FLAGS_HAVE_VXC | XC_FLAGS_HAVE_FXC | XC_FLAGS_HAVE_KXC,
+  1e-24,
+  0, NULL, NULL,
+  gga_xc_kt3_init, NULL, 
   NULL, NULL, NULL
 };

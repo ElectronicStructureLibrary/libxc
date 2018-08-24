7ef7889f465596b104557846bf9d0baba5951f73
Miguel Marques <marques@tddft.org>
2018-08-24 09:32:57 +0200

    Now also GGAs can be written in the new framework. As a first case I converted gga_c_wi

--- scripts/maple2c_new.pl
+++ scripts/maple2c_new.pl
@@ -42,7 +42,7 @@ close($in);
 my %commands = (
   "lda_exc"     => \&work_lda_exc,
   "lda_vxc"     => \&work_lda_vxc,
-#  "work_gga_x"   => \&work_gga_x,
+  "gga_exc"     => \&work_gga_exc,
 #  "work_gga_c"   => \&work_gga_c,
 #  "work_mgga_x"  => \&work_mgga_x,
 #  "work_mgga_c"  => \&work_mgga_c,
@@ -208,4 +208,105 @@ C([$maple_vrho0, $maple_vrho1, $out_c_pol], optimize, deducetypes=false):
       );
 
   maple2c_run(\@variables, \@derivatives, \@variants, 1);
+}
+
+sub work_gga_exc {
+  # these are the variables that the functional depends on
+  my @variables = ("rho_0_", "rho_1_", "sigma_0_", "sigma_1_", "sigma_2_");
+
+  # the definition of the derivatives that libxc transmits to the calling program
+  my @derivatives = (
+    [
+     [[0,0,0,0,0], "_s_zk"]
+    ], [
+      [[1,0,0,0,0], "vrho_0_"],   [[0,1,0,0,0], "vrho_1_"],
+      [[0,0,1,0,0], "vsigma_0_"], [[0,0,0,1,0], "vsigma_1_"], [[0,0,0,0,1], "vsigma_2_"]
+    ], [
+      [[2,0,0,0,0], "v2rho2_0_"], [[1,1,0,0,0], "v2rho2_1_"], [[0,2,0,0,0], "v2rho2_2_"],
+      [[1,0,1,0,0], "v2rhosigma_0_"], [[1,0,0,1,0], "v2rhosigma_1_"],  [[1,0,0,0,1], "v2rhosigma_2_"],
+      [[0,1,1,0,0], "v2rhosigma_3_"], [[0,1,0,1,0], "v2rhosigma_4_"],  [[0,1,0,0,1], "v2rhosigma_5_"],
+      [[0,0,2,0,0], "v2sigma2_0_"], [[0,0,1,1,0], "v2sigma2_1_"], [[0,0,1,0,1], "v2sigma2_2_"],
+      [[0,0,0,2,0], "v2sigma2_3_"], [[0,0,0,1,1], "v2sigma2_4_"], [[0,0,0,0,2], "v2sigma2_5_"], 
+    ], [
+      [[3,0,0,0,0], "v3rho3_0_"], [[2,1,0,0,0], "v3rho3_1_"], 
+      [[1,2,0,0,0], "v3rho3_2_"], [[0,3,0,0,0], "v3rho3_3_"],
+      [[2,0,1,0,0], "v3rho2sigma_0_"], [[2,0,0,1,0], "v3rho2sigma_1_"],  [[2,0,0,0,1], "v3rho2sigma_2_"],
+      [[1,1,1,0,0], "v3rho2sigma_3_"], [[1,1,0,1,0], "v3rho2sigma_4_"],  [[1,1,0,0,1], "v3rho2sigma_5_"],
+      [[0,2,1,0,0], "v3rho2sigma_6_"], [[0,2,0,1,0], "v3rho2sigma_7_"],  [[0,2,0,0,1], "v3rho2sigma_8_"],
+      [[1,0,2,0,0], "v3rhosigma2_0_"], [[1,0,1,1,0], "v3rhosigma2_1_"],  [[1,0,1,0,1], "v3rhosigma2_2_"],
+      [[1,0,0,2,0], "v3rhosigma2_3_"], [[1,0,0,1,1], "v3rhosigma2_4_"],  [[1,0,0,0,2], "v3rhosigma2_5_"],
+      [[0,1,2,0,0], "v3rhosigma2_6_"], [[0,1,1,1,0], "v3rhosigma2_7_"],  [[0,1,1,0,1], "v3rhosigma2_8_"],
+      [[0,1,0,2,0], "v3rhosigma2_9_"], [[0,1,0,1,1], "v3rhosigma2_10_"], [[0,1,0,0,2], "v3rhosigma2_11_"],
+      [[0,0,3,0,0], "v3sigma3_0_"], [[0,0,2,1,0], "v3sigma3_1_"], [[0,0,2,0,1], "v3sigma3_2_"],
+      [[0,0,1,2,0], "v3sigma3_3_"], [[0,0,1,1,1], "v3sigma3_4_"], [[0,0,1,0,2], "v3sigma3_5_"],
+      [[0,0,0,3,0], "v3sigma3_6_"], [[0,0,0,2,1], "v3sigma3_7_"], [[0,0,0,1,2], "v3sigma3_8_"],
+      [[0,0,0,0,3], "v3sigma3_9_"],
+    ],
+    );
+  
+  my ($der_def_unpol, @out_c_unpol) = 
+      maple2c_create_derivatives(\@variables, \@derivatives, "mf", "unpol");
+  my $out_c_unpol = join(", ", @out_c_unpol);
+  my ($der_def_pol, @out_c_pol) = 
+      maple2c_create_derivatives(\@variables, \@derivatives, "mf", "pol");
+  my $out_c_pol = join(", ", @out_c_pol);
+
+  # we join all the pieces
+  my $maple_code = "
+# zk is energy per unit particle
+mzk  := (r0, r1, s0, s1, s2) -> \\
+  $config{'simplify_begin'} \\
+    f(r_ws(dens(r0, r1)), zeta(r0, r1), xt(r0, r1, s0, s1, s2), xs0(r0, r1, s0, s1, s2), xs1(r0, r1, s0, s1, s2)) \\
+  $config{'simplify_end'}:
+
+(* mf is energy per unit volume *)
+mf   := (r0, r1, s0, s1, s2) -> eval(dens(r0, r1)*mzk(r0, r1, s0, s1, s2)):
+
+\$include <util.mpl>
+";
+  my $maple_zk = " _s_zk = mzk(".join(", ", @variables).")";
+
+  # we build 3 variants of the functional, for unpolarized, ferromagnetic, and polarized densities
+  @variants = (
+    "unpol", "
+dens := (r0, r1) -> r0:
+zeta := (r0, r1) -> 0:
+xs0  := (r0, r1, sigma0, sigma2) -> sqrt(sigma0)/(4*r0^(4/3)):
+xs1  := (r0, r1, sigma0, sigma2) -> sqrt(sigma0)/(4*r0^(4/3)):
+xt   := (r0, r1, sigma0, sigma1, sigma2) -> sqrt(sigma0)/r0^(4/3):
+
+$der_def_unpol
+
+$maple_code
+C([$maple_zk, $out_c_unpol], optimize, deducetypes=false):
+",
+
+    "ferr", "
+dens := (r0, r1) -> r0:
+zeta := (r0, r1) -> 1:
+xs0  := (r0, r1, sigma0, sigma2) -> sqrt(sigma0)/r0^(4/3):
+xs1  := (r0, r1, sigma0, sigma2) -> 0:
+xt   := (r0, r1, sigma0, sigma1, sigma2) -> sqrt(sigma0)/r0^(4/3):
+
+$der_def_unpol
+
+$maple_code
+C([$maple_zk, $out_c_unpol], optimize, deducetypes=false):
+\n",
+
+    "pol", "
+dens := (r0, r1) -> r0 + r1:
+zeta := (r0, r1) -> (r0 - r1)/(r0 + r1):
+xs0  := (r0, r1, sigma0, sigma2) -> sqrt(sigma0)/r0^(4/3):
+xs1  := (r0, r1, sigma0, sigma2) -> sqrt(sigma2)/r1^(4/3):
+xt   := (r0, r1, sigma0, sigma1, sigma2) -> sqrt(sigma0 + 2*sigma1 + sigma2)/(r0 + r1)^(4/3):
+
+$der_def_pol
+
+$maple_code
+C([$maple_zk, $out_c_pol], optimize, deducetypes=false):
+\n",
+      );
+
+  maple2c_run(\@variables, \@derivatives, \@variants, 0);
 }
---
+++ src/work_gga_new.c
@@ -0,0 +1,71 @@
+/*
+ Copyright (C) 2006-2018 M.A.L. Marques
+
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+/**
+ * @file work_gga.c
+ * @brief This file is to be included in GGA functionals.
+ */
+
+
+#define OUT_PARAMS_NO_EXC vrho, vsigma, v2rho2, v2rhosigma, v2sigma2, v3rho3, v3rho2sigma, v3rhosigma2, v3sigma3
+#ifdef XC_NO_EXC
+#define OUT_PARAMS OUT_PARAMS_NO_EXC
+#else
+#define OUT_PARAMS zk, OUT_PARAMS_NO_EXC
+#endif
+
+/**
+ * @param[in,out] func_type: pointer to functional structure
+ */
+static void 
+work_gga(const XC(func_type) *p, int np,
+         const double *rho, const double *sigma,
+         double *zk, double *vrho, double *vsigma,
+         double *v2rho2, double *v2rhosigma, double *v2sigma2,
+         double *v3rho3, double *v3rho2sigma, double *v3rhosigma2, double *v3sigma3)
+{
+  int ip, order;
+  double dens, zeta;
+
+  order = -1;
+  if(zk     != NULL) order = 0;
+  if(vrho   != NULL) order = 1;
+  if(v2rho2 != NULL) order = 2;
+  if(v3rho3 != NULL) order = 3;
+
+  if(order < 0) return;
+
+  for(ip = 0; ip < np; ip++){
+    xc_rho2dzeta(p->nspin, rho, &dens, &zeta);
+
+    if(dens > p->dens_threshold){
+      if(p->nspin == XC_UNPOLARIZED){             /* unpolarized case */
+        func_unpol(p, order, rho, sigma, OUT_PARAMS);
+      
+      }else if(zeta >  1.0 - 1e-10){              /* ferromagnetic case - spin 0 */
+        func_ferr(p, order, rho, sigma, OUT_PARAMS);
+        
+      }else if(zeta < -1.0 + 1e-10){              /* ferromagnetic case - spin 1 */
+        internal_counters_gga_next
+          (&(p->dim), -1, &rho, &sigma, &zk, &vrho, &vsigma,
+           &v2rho2, &v2rhosigma, &v2sigma2, &v3rho3, &v3rho2sigma, &v3rhosigma2, &v3sigma3);
+        func_ferr(p, order, rho, sigma, OUT_PARAMS);
+        internal_counters_gga_prev
+          (&(p->dim), -1, &rho, &sigma, &zk, &vrho, &vsigma,
+           &v2rho2, &v2rhosigma, &v2sigma2, &v3rho3, &v3rho2sigma, &v3rhosigma2, &v3sigma3);
+
+      }else{                                      /* polarized (general) case */
+        func_pol(p, order, rho, sigma, OUT_PARAMS);
+      } /* polarization */
+    }
+    
+    internal_counters_gga_next
+      (&(p->dim), 0, &rho, &sigma, &zk, &vrho, &vsigma,
+       &v2rho2, &v2rhosigma, &v2sigma2, &v3rho3, &v3rho2sigma, &v3rhosigma2, &v3sigma3);
+  }   /* for(ip) */
+}

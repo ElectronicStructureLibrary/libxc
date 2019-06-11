0f3711356cf5d47252e882cf06449fdfae9cb57a
Susi Lehtola <susi.lehtola@gmail.com>
2019-06-11 18:17:56 +0300

    Improve description of mgga_x_tlda.

--- src/mgga_x_tlda.c
+++ src/mgga_x_tlda.c
@@ -8,9 +8,7 @@
 
 #include "util.h"
 
-/* Local tau approximation */
-
-#define XC_MGGA_X_TLDA          685 /* tau LDA energy per particle */
+#define XC_MGGA_X_TLDA          685 /* LDA-type exchange with tau-dependent potential */
 
 #include "maple2c/mgga_exc/mgga_x_tlda.c"
 #include "work_mgga_new.c"
@@ -18,7 +16,7 @@
 const xc_func_info_type xc_func_info_mgga_x_tlda = {
   XC_MGGA_X_TLDA,
   XC_EXCHANGE,
-  "tau LDA energy per particle",
+  "LDA-type exchange with tau-dependent potential",
   XC_FAMILY_MGGA,
   {&xc_ref_Eich2014_224107, NULL, NULL, NULL, NULL},
   XC_FLAGS_3D | XC_FLAGS_I_HAVE_ALL,

3f460dfd47871a2d6d94e51b21e423e392c18f6d
Susi Lehtola <susi.lehtola@gmail.com>
2019-08-23 13:14:31 +0300

    Complete JS18 functional implementation.

--- src/hyb_mgga_x_js18.c
+++ src/hyb_mgga_x_js18.c
@@ -1,5 +1,6 @@
 /*
  Copyright (C) 2006-2007 M.A.L. Marques
+               2019 Susi Lehtola
 
  This Source Code Form is subject to the terms of the Mozilla Public
  License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,21 +9,24 @@
 
 #include "util.h"
 
-#define XC_HYB_GGA_XC_JS18       705 /* a screened version of TM */
+#define XC_HYB_MGGA_X_JS18       705 /* a screened version of TM */
 
 static void
 hyb_mgga_x_js18_init(xc_func_type *p)
 {
   p->cam_omega =  0.33;
-  p->cam_alpha =  0.1;
+  p->cam_beta  =  0.1;
 }
 
-const xc_func_info_type xc_func_info_hyb_gga_xc_js18 = {
-  XC_HYB_GGA_XC_JS18,
+#include "maple2c/mgga_exc/hyb_mgga_x_js18.c"
+#include "work_mgga_new.c"
+
+const xc_func_info_type xc_func_info_hyb_mgga_x_js18 = {
+  XC_HYB_MGGA_X_JS18,
   XC_EXCHANGE,
   "JS18",
   XC_FAMILY_HYB_MGGA,
-  {&xc_ref_Heyd2003_8207, NULL, NULL, NULL, NULL},
+  {&xc_ref_Jana2018_8999, NULL, NULL, NULL, NULL},
   XC_FLAGS_3D | XC_FLAGS_HYB_CAM | XC_FLAGS_I_HAVE_ALL,
   1e-32,
   0, NULL, NULL,

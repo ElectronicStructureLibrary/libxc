3678b4c97fe1c9fc3f23119c0a751b2cc4b7871d
Susi Lehtola <susi.lehtola@gmail.com>
2020-08-18 18:07:13 +0300

    Increase thresholds for functionals that fail tests

--- src/gga_x_pbepow.c
+++ src/gga_x_pbepow.c
@@ -24,7 +24,7 @@ const xc_func_info_type xc_func_info_gga_x_pbepow = {
   XC_FAMILY_GGA,
   {&xc_ref_Bremond2016_244102, NULL, NULL, NULL, NULL},
   XC_FLAGS_3D | MAPLE2C_FLAGS,
-  1e-15, /* Gives NaN for densities as small as 1e-3 */
+  1e-12,
   {0, NULL, NULL, NULL, NULL},
   NULL, NULL, 
   NULL, work_gga, NULL

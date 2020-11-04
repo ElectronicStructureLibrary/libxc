ElectronicStructureLibrary/libxc

commit b1fbc5bea87f1eeb7a0cf29672d78ced80225225

Removed CAM/CAMY flags. This of course breaks backward compatibility (like everything we have been doing to hybrids now ;). Took the opportunity to resolve some inconsistencies caught by xc-sanity.

src/xc-info.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char **argv)
 {
-  int i, func_id, error, npar;
+  int i, func_id, error, npar, hybrid_type;
   xc_func_type func;
   char *fname;
 
@@ -43,35 +43,53 @@ int main(int argc, char **argv)
   printf("%10s: %s\n", "comment", func.info->name);
 
   /* Print out hybrid exchange info */
-  if(func.info->family==XC_FAMILY_HYB_LDA || func.info->family==XC_FAMILY_HYB_GGA || func.info->family==XC_FAMILY_HYB_MGGA) {
-    /* Range separation? */
-    int rangesep=0;
-    if(func.info->flags & XC_FLAGS_HYB_CAM)
-      rangesep++;
-    if(func.info->flags & XC_FLAGS_HYB_CAMY)
-      rangesep++;
-    if(func.info->flags & XC_FLAGS_HYB_LC)
-      rangesep++;
-    if(func.info->flags & XC_FLAGS_HYB_LCY)
-      rangesep++;
+  hybrid_type = xc_hyb_type(&func);
+  switch(hybrid_type){
+  case XC_HYB_SEMILOCAL:
+    printf("\nThis is a semi-local functional.\n");
+    break;
+  case XC_HYB_HYBRID:
+    printf("\nThis is a hybrid functional.\n");
+    break;
+  case XC_HYB_CAM:
+    printf("\nThis is a CAM functional.\n");
+    break;
+  case XC_HYB_CAMY:
+    printf("\nThis is a CAMY functional.\n");
+    break;
+  case XC_HYB_CAMG:
+    printf("\nThis is a CAMG functional.\n");
+    break;
+  case XC_HYB_DOUBLE:
+    printf("\nThis is a double hybrid functional.\n");
+    break;
+  case XC_HYB_MIXTURE:
+    printf("\nThis is a complicated functional ;)\n");
+    break;
+  }
 
-    if(rangesep) {
-      double alpha, beta, omega;
-      xc_hyb_cam_coef(&func,&omega,&alpha,&beta);
-      printf("\nThis is a range-separated hybrid functional with range-separation constant % .3f,\n",omega);
-      printf("and %4.1f%% short-range and %4.1f%% long-range exact exchange,\n",(alpha+beta)*100,(alpha)*100);
+  if(hybrid_type != XC_HYB_NONE) {
+    printf("The calling program must add the following terms:\n");
+    for(i=0; i<func.hyb_number_terms; i++){
+      switch(func.hyb_type[i]){
+      case XC_HYB_FOCK:
+        printf("  *) Fock integral: mixing = %.3f\n", func.hyb_coeff[i]);
+        break;
+      case XC_HYB_ERF_SR:
+        printf("  *) Short range Fock (erf): mixing = %.3f; mu = %.3f\n", func.hyb_coeff[i], func.hyb_omega[i]);
+        break;
+      case XC_HYB_YUKAWA_SR:
+        printf("  *) Short range Fock (Yukawa): mixing = %.3f; mu = %.3f\n", func.hyb_coeff[i], func.hyb_omega[i]);
+        break;
+      case XC_HYB_GAUSSIAN_SR:
+        printf("  *) Short range Fock (gaussian): mixing = %.3f; mu = %.3f\n", func.hyb_coeff[i], func.hyb_omega[i]);
+        break;
+      case XC_HYB_PT2:
+        printf("  *) Second order perturbation theory: mixing = %.3f", func.hyb_coeff[i]);
+        break;
 
-      if(func.info->flags & XC_FLAGS_HYB_CAM || func.info->flags & XC_FLAGS_HYB_LC)
-        printf("using the error function kernel.\n");
-      else if(func.info->flags & XC_FLAGS_HYB_CAMY || func.info->flags & XC_FLAGS_HYB_LCY)
-        printf("using the Yukawa kernel.\n");
-    } else {
-      double alpha=xc_hyb_exx_coef(&func);
-      printf("\nThis is a global hybrid functional with %4.1f%% of exact exchange.\n",alpha*100);
+      }
     }
-  } else {
-    if(func.info->kind == XC_EXCHANGE || func.info->kind == XC_EXCHANGE_CORRELATION)
-      printf("\nThis is a pure functional with no exact exchange.\n");
   }
 
   printf("\nReference(s):\n");
@@ -83,7 +101,7 @@ int main(int argc, char **argv)
     }
   }
 
-  printf("\nImplementation has support for:\n");
+  printf("\nCompilation has support for:\n");
   if(func.info->flags & XC_FLAGS_HAVE_EXC)
     printf("  *) energy\n");
   if(func.info->flags & XC_FLAGS_HAVE_VXC)

commit daf882d4f04895fb94ae4a2eb8209d1dd93a5636

Restore functionality of xc-info broken by commit b1fbc5bea87f1eeb7a0cf29672d78ced80225225

src/xc-info.c
@@ -96,6 +96,18 @@ int main(int argc, char **argv)
     }
   }
 
+  if(hybrid_type == XC_HYB_CAM || hybrid_type == XC_HYB_CAMY) {
+    /* Determine amount of sr and lr exchange */
+    double alpha, beta, omega;
+    assert(func.hyb_type[0] == XC_HYB_ERF_SR || func.hyb_type[0] == XC_HYB_YUKAWA_SR);
+    omega=func.hyb_omega[0];
+    beta=func.hyb_coeff[0];
+    assert(func.hyb_type[1] == XC_HYB_FOCK);
+    alpha=func.hyb_coeff[1];
+    printf("\nFunctional has a range-separation constant % .3f,\n",omega);
+    printf("and %4.1f%% short-range and %4.1f%% long-range exact exchange,\n",(alpha+beta)*100,(alpha)*100);
+  }
+
   printf("\nReference(s):\n");
   for(i = 0; i < 5; i++){
     if(func.info->refs[i] == NULL) break;
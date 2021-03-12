4a4a626a32e27dd7c23d3e46f8bd8b532f801464
Susi Lehtola <susi.lehtola@gmail.com>
2021-03-12 12:45:32 -0500

    Implemented lda_xc_corrksdt, a corrected version of lda_xc_ksdt.

--- src/lda_xc_ksdt.c
+++ src/lda_xc_ksdt.c
@@ -16,8 +16,9 @@
   Ported to C and libxc by Lazaro Calderin and Miguel Marques
 ************************************************************************/
 
-#define XC_LDA_XC_KSDT    259    /* Karasiev et al. parametrization */
-#define XC_LDA_XC_GDSMFB  577    /* Groth et al. parametrization */
+#define XC_LDA_XC_KSDT     259    /* Karasiev et al. parametrization */
+#define XC_LDA_XC_GDSMFB   577    /* Groth et al. parametrization */
+#define XC_LDA_XC_CORRKSDT 318    /* Karasiev et al. corrected parametrization */
 
 typedef struct{
   double T;            /* In units of k_B */
@@ -40,6 +41,24 @@ static const lda_xc_ksdt_params par_ksdt = {
     {0.5513300, 180.2131590, 134.4862310, 103.8616950, 17.7507100}
   }, { /* e */
     {0.2120360, 16.7312490, 28.4857920,  34.0288760, 17.2355150},
+    {0.1531240, 19.5439450, 43.4003370, 120.2551450, 15.6628360}
+  }
+};
+
+static const lda_xc_ksdt_params par_corrksdt = {
+  0.0,  /* T */
+  0.0,  /* thetaParam */
+  {     /* b5 = Sqrt[3/2]/(lambda)*b3 */
+    {0.342554, 9.141315, 0.448483, 18.553096, 1.05414999729322402165649834296},
+    {0.3290010, 111.5983080, 0.5370530,105.0866630, 1.26233194679913807935662124247}
+  }, {  /* c */
+    {0.875130, -0.256320,  0.953988},
+    {0.8489300, 0.1679520, 0.0888200}
+  }, {  /* d */
+    {0.725917,    2.237347,    0.280748,    4.185911,   0.692183},
+    {0.5513300, 180.2131590, 134.4862310, 103.8616950, 17.7507100}
+  }, { /* e */
+    {0.255415,   0.931933,   0.115398,   17.234117,   0.451437},
     {0.1531240, 19.5439450, 43.4003370, 120.2551450, 15.6628360}
   }
 };
@@ -78,6 +97,9 @@ lda_xc_ksdt_init(xc_func_type *p)
     break;
   case XC_LDA_XC_GDSMFB:
     libxc_memcpy(params, &par_gdsmfb, sizeof(lda_xc_ksdt_params));
+    break;
+  case XC_LDA_XC_CORRKSDT:
+    libxc_memcpy(params, &par_corrksdt, sizeof(lda_xc_ksdt_params));
     break;
   default:
     fprintf(stderr, "Internal error in lda_xc_ksdt\n");
@@ -131,6 +153,22 @@ const xc_func_info_type xc_func_info_lda_xc_gdsmfb = {
   "Groth, Dornheim, Sjostrom, Malone, Foulkes, Bonitz",
   XC_FAMILY_LDA,
   {&xc_ref_Groth2017_135001, NULL, NULL, NULL, NULL},
+  XC_FLAGS_3D | MAPLE2C_FLAGS,
+  1e-15,
+  {1, T_names, T_desc, T_values, T_set_ext_params},
+  lda_xc_ksdt_init, NULL,
+  work_lda, NULL, NULL
+};
+
+#ifdef __cplusplus
+extern "C"
+#endif
+const xc_func_info_type xc_func_info_lda_xc_corrksdt = {
+  XC_LDA_XC_CORRKSDT,
+  XC_EXCHANGE_CORRELATION,
+  "Corrected KSDT by Karasiev, Dufty and Trickey",
+  XC_FAMILY_LDA,
+  {&xc_ref_Karasiev2018_076401, &xc_ref_Karasiev2014_076403, &xc_ref_lda_xc_corrksdt_note, NULL, NULL},
   XC_FLAGS_3D | MAPLE2C_FLAGS,
   1e-15,
   {1, T_names, T_desc, T_values, T_set_ext_params},

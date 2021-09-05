3720abbaa6bc4020ccdbd4632f53c2d385d94489
Susi Lehtola <susi.lehtola@gmail.com>
2021-09-05 11:49:55 +0300

    Actually need to handle tau, lapl, tau&lapl separately

--- src/mix_func.c
+++ src/mix_func.c
@@ -1,6 +1,6 @@
 /*
-  Copyright (C) 2006-2007 M.A.L. Marques
-                2018-2019 Susi Lehtola
+  Copyright (C) 2006-2021 M.A.L. Marques
+                2018-2021 Susi Lehtola
                 2019 X. Andrade
 
   This Source Code Form is subject to the terms of the Mozilla Public
@@ -205,12 +205,14 @@ xc_mix_func(const xc_func_type *func, size_t np,
           sum_var(v2rholapl);
           sum_var(v2sigmalapl);
           sum_var(v2lapl2);
-          sum_var(v2lapltau);
         }
         if(aux->info->flags & XC_FLAGS_NEEDS_TAU) {
           sum_var(v2rhotau);
           sum_var(v2sigmatau);
           sum_var(v2tau2);
+        }
+        if(aux->info->flags & (XC_FLAGS_NEEDS_LAPLACIAN | XC_FLAGS_NEEDS_TAU)) {
+          sum_var(v2lapltau);
         }
       }
     }
@@ -230,13 +232,9 @@ xc_mix_func(const xc_func_type *func, size_t np,
           sum_var(v3rho2lapl);
           sum_var(v3rhosigmalapl);
           sum_var(v3rholapl2);
-          sum_var(v3rholapltau);
           sum_var(v3sigma2lapl);
           sum_var(v3sigmalapl2);
-          sum_var(v3sigmalapltau);
           sum_var(v3lapl3);
-          sum_var(v3lapl2tau);
-          sum_var(v3lapltau2);
         }
         if(aux->info->flags & XC_FLAGS_NEEDS_TAU) {
           sum_var(v3rho2tau);
@@ -245,6 +243,12 @@ xc_mix_func(const xc_func_type *func, size_t np,
           sum_var(v3sigma2tau);
           sum_var(v3sigmatau2);
           sum_var(v3tau3);
+        }
+        if(aux->info->flags & (XC_FLAGS_NEEDS_LAPLACIAN | XC_FLAGS_NEEDS_TAU)) {
+          sum_var(v3rholapltau);
+          sum_var(v3sigmalapltau);
+          sum_var(v3lapl2tau);
+          sum_var(v3lapltau2);
         }
       }
     }
@@ -264,23 +268,13 @@ xc_mix_func(const xc_func_type *func, size_t np,
           sum_var(v4rho3lapl);
           sum_var(v4rho2sigmalapl);
           sum_var(v4rho2lapl2);
-          sum_var(v4rho2lapltau);
           sum_var(v4rhosigma2lapl);
           sum_var(v4rhosigmalapl2);
-          sum_var(v4rhosigmalapltau);
           sum_var(v4rholapl3);
-          sum_var(v4rholapl2tau);
-          sum_var(v4rholapltau2);
           sum_var(v4sigma3lapl);
           sum_var(v4sigma2lapl2);
-          sum_var(v4sigma2lapltau);
           sum_var(v4sigmalapl3);
-          sum_var(v4sigmalapl2tau);
-          sum_var(v4sigmalapltau2);
           sum_var(v4lapl4);
-          sum_var(v4lapl3tau);
-          sum_var(v4lapl2tau2);
-          sum_var(v4lapltau3);
         }
         if(aux->info->flags & XC_FLAGS_NEEDS_TAU) {
           sum_var(v4rho3tau);
@@ -294,6 +288,18 @@ xc_mix_func(const xc_func_type *func, size_t np,
           sum_var(v4sigmatau3);
           sum_var(v4tau4);
         }
+        if(aux->info->flags & (XC_FLAGS_NEEDS_LAPLACIAN | XC_FLAGS_NEEDS_TAU)) {
+          sum_var(v4rho2lapltau);
+          sum_var(v4rhosigmalapltau);
+          sum_var(v4rholapl2tau);
+          sum_var(v4rholapltau2);
+          sum_var(v4sigma2lapltau);
+          sum_var(v4sigmalapl2tau);
+          sum_var(v4sigmalapltau2);
+          sum_var(v4lapl3tau);
+          sum_var(v4lapl2tau2);
+          sum_var(v4lapltau3);
+        }
       }
     }
 #endif
@@ -305,4 +311,3 @@ xc_mix_func(const xc_func_type *func, size_t np,
   /* deallocate internal buffers */
   xc_mgga_vars_free_all(xzk MGGA_OUT_PARAMS_NO_EXC(XC_COMMA, x));
 }
-

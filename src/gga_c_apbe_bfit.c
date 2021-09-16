6619b2e24c64fae2a7f515337a2a040a43d65d5f
Miguel Marques <marques@tddft.org>
2021-09-16 08:59:47 +0200

    Added the first 3 LC hybrids from the paper. Susi, care to try them out?

--- src/gga_c_apbe_bfit.c
+++ src/gga_c_apbe_bfit.c
@@ -8,10 +8,14 @@
 
 #include "util.h"
 
-#define XC_GGA_C_APBE_BFIT    720 /* APBE with a fit of beta */
-#define XC_GGA_C_PBEINT_BFIT  721 /* PBEINT with a fit of beta */
+#define XC_GGA_C_APBE_BFIT        720 /* APBE with a fit of beta */
+#define XC_GGA_C_PBEINT_BFIT      721 /* PBEINT with a fit of beta */
+#define XC_HYB_GGA_XC_LC_OAPBE    722 /* LC hybrid based on APBE */
+#define XC_HYB_GGA_XC_LC_OPBEINT  723 /* LC hybrid based on PBEINT */
+#define XC_HYB_GGA_XC_LC_OSG4     724 /* LC hybrid based on SG4 */
 
 typedef struct{
+  double a1, a2, a3, a4, a5;
   double mux, omega;
 } gga_c_apbe_bfit_params;
 
@@ -22,15 +26,15 @@ static void gga_c_apbe_bfit_init(xc_func_type *p)
   p->params = libxc_malloc(sizeof(gga_c_apbe_bfit_params));
 }
 
-#define APBE_BFIT_N_PAR 2
+#define APBE_BFIT_N_PAR 7
 static const char  *apbe_bfit_names[APBE_BFIT_N_PAR]  = {"_mux", "_omega"};
 static const char  *apbe_bfit_desc[APBE_BFIT_N_PAR]   = {
   "mu of the corresponding exchange functional",
   "range separation parameter"};
 static const double apbe_bfit_values[APBE_BFIT_N_PAR] = 
-  {0.26, 0.37};
+  {0.06929609, 0.02090877, 73.63025684, 3.84513730, 0.00000049, 0.26, 0.37};
 static const double pbeint_bfit_values[APBE_BFIT_N_PAR] = 
-  {10./81., 0.52};
+  {0.00000000, 0.06413244, 27.06803466, 3.61233368, 0.00005694, 10./81., 0.52};
 
 #include "decl_gga.h"
 #include "maple2c/gga_exc/gga_c_apbe_bfit.c"
@@ -66,4 +70,89 @@ const xc_func_info_type xc_func_info_gga_c_pbeint_bfit = {
   {APBE_BFIT_N_PAR, apbe_bfit_names, apbe_bfit_desc, pbeint_bfit_values, set_ext_params_cpy},
   gga_c_apbe_bfit_init, NULL, 
   NULL, work_gga, NULL
+};
+
+
+static void
+hyb_gga_xc_lc_oapbe_init(xc_func_type *p)
+{
+  static int   funcs_id  [2] = {XC_GGA_X_HJS_APBE, XC_GGA_C_APBE};
+  static double funcs_coef[2] = {1.0, 1.0};
+
+  xc_mix_init(p, 2, funcs_id, funcs_coef);
+  xc_hyb_init_cam(p, 0.37, 1.0, -1.0);
+
+  xc_func_set_ext_params_name(p->func_aux[0], "_omega", p->hyb_omega[0]);
+}
+
+#ifdef __cplusplus
+extern "C"
+#endif
+const xc_func_info_type xc_func_info_hyb_gga_xc_lc_oapbe = {
+  XC_HYB_GGA_XC_LC_OAPBE,
+  XC_EXCHANGE_CORRELATION,
+  "LC hybrid based on APBE",
+  XC_FAMILY_GGA,
+  {&xc_ref_Jana2019_042515, NULL, NULL, NULL, NULL},
+  XC_FLAGS_3D | MAPLE2C_FLAGS,
+  1e-12,
+  {0, NULL, NULL, NULL, NULL},
+  hyb_gga_xc_lc_oapbe_init, NULL, 
+  NULL, NULL, NULL
+};
+
+static void
+hyb_gga_xc_lc_opbeint_init(xc_func_type *p)
+{
+  static int   funcs_id  [2] = {XC_GGA_X_HJS_PBEINT, XC_GGA_C_PBEINT};
+  static double funcs_coef[2] = {1.0, 1.0};
+
+  xc_mix_init(p, 2, funcs_id, funcs_coef);
+  xc_hyb_init_cam(p, 0.52, 1.0, -1.0);
+
+  xc_func_set_ext_params_name(p->func_aux[0], "_omega", p->hyb_omega[0]);
+}
+
+#ifdef __cplusplus
+extern "C"
+#endif
+const xc_func_info_type xc_func_info_hyb_gga_xc_lc_opbeint = {
+  XC_HYB_GGA_XC_LC_OPBEINT,
+  XC_EXCHANGE_CORRELATION,
+  "LC hybrid based on PBEINT",
+  XC_FAMILY_GGA,
+  {&xc_ref_Jana2019_042515, NULL, NULL, NULL, NULL},
+  XC_FLAGS_3D | MAPLE2C_FLAGS,
+  1e-12,
+  {0, NULL, NULL, NULL, NULL},
+  hyb_gga_xc_lc_opbeint_init, NULL, 
+  NULL, NULL, NULL
+};
+
+static void
+hyb_gga_xc_lc_osg4_init(xc_func_type *p)
+{
+  static int   funcs_id  [2] = {XC_GGA_X_HJS_SG4, XC_GGA_C_SG4};
+  static double funcs_coef[2] = {1.0, 1.0};
+
+  xc_mix_init(p, 2, funcs_id, funcs_coef);
+  xc_hyb_init_cam(p, 0.50, 1.0, -1.0);
+
+  xc_func_set_ext_params_name(p->func_aux[0], "_omega", p->hyb_omega[0]);
+}
+
+#ifdef __cplusplus
+extern "C"
+#endif
+const xc_func_info_type xc_func_info_hyb_gga_xc_lc_osg4 = {
+  XC_HYB_GGA_XC_LC_OSG4,
+  XC_EXCHANGE_CORRELATION,
+  "LC hybrid based on PBEINT",
+  XC_FAMILY_GGA,
+  {&xc_ref_Jana2019_042515, NULL, NULL, NULL, NULL},
+  XC_FLAGS_3D | MAPLE2C_FLAGS,
+  1e-12,
+  {0, NULL, NULL, NULL, NULL},
+  hyb_gga_xc_lc_osg4_init, NULL, 
+  NULL, NULL, NULL
 };

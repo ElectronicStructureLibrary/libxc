48ecbecccff7ee8746cb2b86afcb41cfa743adef
Susi Lehtola <susi.lehtola@gmail.com>
2021-09-18 12:28:21 +0300

    Denote omega by w not o

--- src/gga_c_apbe_bfit.c
+++ src/gga_c_apbe_bfit.c
@@ -1,5 +1,6 @@
 /*
- Copyright (C) 2006-2007 M.A.L. Marques
+ Copyright (C) 2006-2021 M.A.L. Marques
+                    2021 Susi Lehtola
 
  This Source Code Form is subject to the terms of the Mozilla Public
  License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -10,9 +11,9 @@
 
 #define XC_GGA_C_APBE_BFIT        830 /* APBE with a fit of beta */
 #define XC_GGA_C_PBEINT_BFIT      831 /* PBEINT with a fit of beta */
-#define XC_HYB_GGA_XC_LC_OAPBE    832 /* LC hybrid based on APBE */
-#define XC_HYB_GGA_XC_LC_OPBEINT  833 /* LC hybrid based on PBEINT */
-#define XC_HYB_GGA_XC_LC_OSG4     834 /* LC hybrid based on SG4 */
+#define XC_HYB_GGA_XC_LC_WAPBE    832 /* LC hybrid based on APBE */
+#define XC_HYB_GGA_XC_LC_WPBEINT  833 /* LC hybrid based on PBEINT */
+#define XC_HYB_GGA_XC_LC_WSG4     834 /* LC hybrid based on SG4 */
 
 typedef struct{
   double bfit_a1, bfit_a2, bfit_a3, bfit_a4, bfit_a5;
@@ -38,9 +39,9 @@ static const char  *apbe_bfit_desc[APBE_BFIT_N_PAR]   =
    "mu of the corresponding exchange functional",
    "range separation parameter"
   };
-static const double apbe_bfit_values[APBE_BFIT_N_PAR] = 
+static const double apbe_bfit_values[APBE_BFIT_N_PAR] =
   {0.06929609, 0.02090877, 73.63025684, 3.84513730, 0.00000049, 0.26, 0.37};
-static const double pbeint_bfit_values[APBE_BFIT_N_PAR] = 
+static const double pbeint_bfit_values[APBE_BFIT_N_PAR] =
   {0.00000000, 0.06413244, 27.06803466, 3.61233368, 0.00005694, 10./81., 0.52};
 
 #include "decl_gga.h"
@@ -59,7 +60,7 @@ const xc_func_info_type xc_func_info_gga_c_apbe_bfit = {
   XC_FLAGS_3D | MAPLE2C_FLAGS,
   1e-12,
   {APBE_BFIT_N_PAR, apbe_bfit_names, apbe_bfit_desc, apbe_bfit_values, set_ext_params_cpy},
-  gga_c_apbe_bfit_init, NULL, 
+  gga_c_apbe_bfit_init, NULL,
   NULL, work_gga, NULL
 };
 
@@ -75,13 +76,13 @@ const xc_func_info_type xc_func_info_gga_c_pbeint_bfit = {
   XC_FLAGS_3D | MAPLE2C_FLAGS,
   1e-12,
   {APBE_BFIT_N_PAR, apbe_bfit_names, apbe_bfit_desc, pbeint_bfit_values, set_ext_params_cpy},
-  gga_c_apbe_bfit_init, NULL, 
+  gga_c_apbe_bfit_init, NULL,
   NULL, work_gga, NULL
 };
 
 
 static void
-hyb_gga_xc_lc_oapbe_init(xc_func_type *p)
+hyb_gga_xc_lc_wapbe_init(xc_func_type *p)
 {
   static int   funcs_id  [2] = {XC_GGA_X_HJS_APBE, XC_GGA_C_APBE};
   static double funcs_coef[2] = {1.0, 1.0};
@@ -95,8 +96,8 @@ hyb_gga_xc_lc_oapbe_init(xc_func_type *p)
 #ifdef __cplusplus
 extern "C"
 #endif
-const xc_func_info_type xc_func_info_hyb_gga_xc_lc_oapbe = {
-  XC_HYB_GGA_XC_LC_OAPBE,
+const xc_func_info_type xc_func_info_hyb_gga_xc_lc_wapbe = {
+  XC_HYB_GGA_XC_LC_WAPBE,
   XC_EXCHANGE_CORRELATION,
   "LC hybrid based on APBE",
   XC_FAMILY_GGA,
@@ -104,12 +105,12 @@ const xc_func_info_type xc_func_info_hyb_gga_xc_lc_oapbe = {
   XC_FLAGS_3D | MAPLE2C_FLAGS,
   1e-12,
   {0, NULL, NULL, NULL, NULL},
-  hyb_gga_xc_lc_oapbe_init, NULL, 
+  hyb_gga_xc_lc_wapbe_init, NULL,
   NULL, NULL, NULL
 };
 
 static void
-hyb_gga_xc_lc_opbeint_init(xc_func_type *p)
+hyb_gga_xc_lc_wpbeint_init(xc_func_type *p)
 {
   static int   funcs_id  [2] = {XC_GGA_X_HJS_PBEINT, XC_GGA_C_PBEINT};
   static double funcs_coef[2] = {1.0, 1.0};
@@ -123,8 +124,8 @@ hyb_gga_xc_lc_opbeint_init(xc_func_type *p)
 #ifdef __cplusplus
 extern "C"
 #endif
-const xc_func_info_type xc_func_info_hyb_gga_xc_lc_opbeint = {
-  XC_HYB_GGA_XC_LC_OPBEINT,
+const xc_func_info_type xc_func_info_hyb_gga_xc_lc_wpbeint = {
+  XC_HYB_GGA_XC_LC_WPBEINT,
   XC_EXCHANGE_CORRELATION,
   "LC hybrid based on PBEINT",
   XC_FAMILY_GGA,
@@ -132,12 +133,12 @@ const xc_func_info_type xc_func_info_hyb_gga_xc_lc_opbeint = {
   XC_FLAGS_3D | MAPLE2C_FLAGS,
   1e-12,
   {0, NULL, NULL, NULL, NULL},
-  hyb_gga_xc_lc_opbeint_init, NULL, 
+  hyb_gga_xc_lc_wpbeint_init, NULL,
   NULL, NULL, NULL
 };
 
 static void
-hyb_gga_xc_lc_osg4_init(xc_func_type *p)
+hyb_gga_xc_lc_wsg4_init(xc_func_type *p)
 {
   static int   funcs_id  [2] = {XC_GGA_X_HJS_SG4, XC_GGA_C_SG4};
   static double funcs_coef[2] = {1.0, 1.0};
@@ -151,8 +152,8 @@ hyb_gga_xc_lc_osg4_init(xc_func_type *p)
 #ifdef __cplusplus
 extern "C"
 #endif
-const xc_func_info_type xc_func_info_hyb_gga_xc_lc_osg4 = {
-  XC_HYB_GGA_XC_LC_OSG4,
+const xc_func_info_type xc_func_info_hyb_gga_xc_lc_wsg4 = {
+  XC_HYB_GGA_XC_LC_WSG4,
   XC_EXCHANGE_CORRELATION,
   "LC hybrid based on PBEINT",
   XC_FAMILY_GGA,
@@ -160,6 +161,6 @@ const xc_func_info_type xc_func_info_hyb_gga_xc_lc_osg4 = {
   XC_FLAGS_3D | MAPLE2C_FLAGS,
   1e-12,
   {0, NULL, NULL, NULL, NULL},
-  hyb_gga_xc_lc_osg4_init, NULL, 
+  hyb_gga_xc_lc_wsg4_init, NULL,
   NULL, NULL, NULL
 };

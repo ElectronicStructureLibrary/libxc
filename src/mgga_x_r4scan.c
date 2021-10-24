7747e87d98d96414580cf588391a9abdf2749645
Susi Lehtola <susi.lehtola@gmail.com>
2021-10-24 16:11:06 -0400

    Remove flag from mgga_x_r4scan which doesn't exist in 5.1.x

--- src/mgga_x_r4scan.c
+++ src/mgga_x_r4scan.c
@@ -43,7 +43,7 @@ const xc_func_info_type xc_func_info_mgga_x_r4scan = {
   "r^4SCAN, a functional that satisfies the same exact constraints that SCAN does",
   XC_FAMILY_MGGA,
   {&xc_ref_Furness2021, NULL, NULL, NULL, NULL},
-  XC_FLAGS_3D | XC_FLAGS_NEEDS_TAU | MAPLE2C_FLAGS,
+  XC_FLAGS_3D | MAPLE2C_FLAGS,
   1e-11,
   {N_PAR, names, desc, par_r4scan, set_ext_params_cpy},
   mgga_x_r4scan_init, NULL,

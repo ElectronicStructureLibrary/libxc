3f9df341d2ee05bf9d0bb7f025a0723cc0ae8f71
Susi Lehtola <susi.lehtola@gmail.com>
2022-01-24 14:13:15 -0500

    Final literature reference for r++SCAN and r4SCAN.

--- src/mgga_c_rppscan.c
+++ src/mgga_c_rppscan.c
@@ -42,7 +42,7 @@ const xc_func_info_type xc_func_info_mgga_c_rppscan = {
   XC_CORRELATION,
   "r++SCAN: rSCAN with uniform density limit and coordinate scaling behavior",
   XC_FAMILY_MGGA,
-  {&xc_ref_Furness2021, NULL, NULL, NULL, NULL},
+  {&xc_ref_Furness2022_034109, NULL, NULL, NULL, NULL},
   XC_FLAGS_3D | XC_FLAGS_NEEDS_TAU | MAPLE2C_FLAGS,
   1e-15,
   {N_PAR, names, desc, rppscan_values, set_ext_params_cpy},

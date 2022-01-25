cb1af9b97295819096850341ec277bad02edf64a
Susi Lehtola <susi.lehtola@gmail.com>
2022-01-25 14:33:56 -0500

    Implemented hyb_mgga_xc_br3p86. The paper doesn't say which VWN the functional is based upon. However, Aron Cohen tells me CADPAC uses VWN5, so it must be this version that was also used in BR3P86. The functional apparently isn't available in the official CADPAC release, full verification is not possible. The data in the original paper is likely not converged to the grid limit - the atomization energies differ - but bond lengths for first-row homoatomics appear to be reproduced to 0.002 Å.

--- src/xc_funcs.h
+++ src/xc_funcs.h
@@ -317,6 +317,7 @@
 #define  XC_LDA_C_W20                      317 /* Xie| Wu| and Zhao interpolation ansatz without fitting parameters */
 #define  XC_LDA_XC_CORRKSDT                318 /* Corrected KSDT by Karasiev| Dufty and Trickey */
 #define  XC_MGGA_X_FT98                    319 /* Filatov and Thiel 1998 meta-GGA exchange */
+#define  XC_HYB_MGGA_XC_BR3P86             389 /* BR3P86 hybrid meta-GGA from Neumann and Handy */
 #define  XC_HYB_GGA_XC_CASE21              390 /* CASE21: Constrained And Smoothed semi-Empirical 2021 functional */
 #define  XC_MGGA_C_RREGTM                  391 /* Revised regTM correlation by Jana et al */
 #define  XC_HYB_GGA_XC_PBE_2X              392 /* PBE-2X: PBE0 with 56% exact exchange */

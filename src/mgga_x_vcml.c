42cf7a7f4b16adf073b586e47e7dd32a99e79d2f
Susi Lehtola <susi.lehtola@gmail.com>
2022-03-02 12:39:38 -0500

    adopt mgga_x_vcml to new code

--- src/mgga_x_vcml.c
+++ src/mgga_x_vcml.c
@@ -13,7 +13,6 @@
 #define XC_MGGA_X_VCML            651 /* VCML exchange, used in VCML-rVV10 by Trepte and Voss */
 #define XC_MGGA_XC_VCML_RVV10     652 /* VCML-rVV10 exchange-correlation by Trepte and Voss */
 
-#include "decl_mgga.h"
 #include "maple2c/mgga_exc/mgga_x_vcml.c"
 #include "work_mgga.c"
 

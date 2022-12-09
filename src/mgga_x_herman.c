b92f10cffb572e1a10485e431d16bf141fcc103a
Susi Lehtola <susi.lehtola@gmail.com>
2022-12-09 16:01:03 +0200

    Flags missing

--- src/mgga_x_herman.c
+++ src/mgga_x_herman.c
@@ -22,7 +22,7 @@ const xc_func_info_type xc_func_info_mgga_x_herman = {
   "Herman Xalphabeta mGGA",
   XC_FAMILY_MGGA,
   {&xc_ref_Herman1969_807, &xc_ref_Herman1970_827, NULL, NULL, NULL},
-  XC_FLAGS_3D | MAPLE2C_FLAGS,
+  XC_FLAGS_3D | MAPLE2C_FLAGS | XC_FLAGS_NEEDS_TAU | XC_FLAGS_NEEDS_LAPLACIAN,
   1e-15,
   {0, NULL, NULL, NULL, NULL},
   NULL, NULL,
